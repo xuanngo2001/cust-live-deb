9b92d8348bda73cee117715fd0d703014c382395
limelime <limelime@users.noreply.github.com>
2015-05-11 15:06:26 +0000

    Append log info.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 chmod +x  *.sh
 
 # Execute base scripts
-sh install-00-00.sh
+sh install-00-00.sh 2>>&1 | tee install.log
 
 # Execute script with install-<letter>*.sh
 for scriptfile in $( ls install-[aA-zZ]*.sh ); do
   echo ">>>>>>>>> Running ${scriptfile}..."
-  sh ${scriptfile} 2>&1 | tee install.log
+  sh ${scriptfile} 2>>&1 | tee install.log
 done
