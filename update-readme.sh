9374703eeec896de6f6c5b1345d0f6a1696e242c
limelime <limelime@users.noreply.github.com>
2015-05-12 17:29:07 +0000

    Update.

--- update-readme.sh
+++ update-readme.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Description: Extract chroot/root/scripts/install.log.ran and put logged info in README.md
+#   Script name will be the root level list.
+#   Logged line in script will be nested to the root level ist.
 
 README=README.md
 cp ${README} ${README}.bck
@@ -8,8 +11,9 @@ sed "/${PLACEHOLDER}/,/ISO/d" ${README}.bck > ${README}
 
 echo ${PLACEHOLDER} >> ${README}
 grep "CLDS:" chroot/root/scripts/install.log.ran | \
+   sed 's/^CLDS: \*/CLDS:    */' |
    sed 's/^CLDS://' | \
-   sed 's/^>/## /' >> ${README}
+   sed 's/^>/  * /' >> ${README}
    
 rm -f ${README}.bck
 
