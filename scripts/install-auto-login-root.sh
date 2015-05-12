9c6d636db86fadc988f354295ffee086c195c44a
limelime <limelime@users.noreply.github.com>
2015-05-12 18:25:31 +0000

    Add reference.

--- scripts/install-auto-login-root.sh
+++ scripts/install-auto-login-root.sh
@@ -9,3 +9,8 @@ cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf<<EOF
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root %I
 EOF
+
+
+# Reference:
+#  http://forums.debian.net/viewtopic.php?f=5&t=119348#p563096
+#  http://www.slicesofcomputer.com/2014/10/systemd-hate-console-autologin-on-jessie.html
