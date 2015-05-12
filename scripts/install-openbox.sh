818774e2a449210778dde9357b90b3ad49cb72f7
limelime <limelime@users.noreply.github.com>
2015-05-12 16:39:41 +0000

    Install openbox, set keyboard, auto login.

---
+++ scripts/install-openbox.sh
@@ -0,0 +1,19 @@
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "CLDS:>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+SETTINGS_DIR=settings
+
+#export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true DEBCONF_DEBUG=developer
+export DEBCONF_DEBUG=developer
+
+# Install X-Windows=xorg, Window Manager=openbox
+debconf-set-selections ${SETTINGS_DIR}/keyboard.seed
+apt-get -y --force-yes install xorg openbox
+
+# Auto startx
+cat ${SETTINGS_DIR}/bash_profile_auto_startx >> /root/.bash_profile
+
+
+# Log
+echo "CLDS: * Setup keyboard to use US international."
+echo "CLDS: * Install xorg and openbox."
