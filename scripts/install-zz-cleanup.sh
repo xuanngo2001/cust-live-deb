b8f049a432efa53fbda447f97e70ab81f2ae4452
limelime <limelime@users.noreply.github.com>
2015-05-12 16:42:37 +0000

    Improve log.

--- scripts/install-zz-cleanup.sh
+++ scripts/install-zz-cleanup.sh
@@ -16,7 +16,7 @@ echo "deb ${DEB_REPO_URL} jessie main" > /etc/apt/sources.list
 # Log
 echo "CLDS: * Clean apt-get cache."
 echo "CLDS: * rm -rf /tmp/*."
-echo "CLDS: * Umount /sys /dev/pts /proc."
+echo "CLDS: * Umount /sys, /dev/pts and /proc."
 echo "CLDS: * Add ${DEB_REPO_URL} in sources.list."
 
 
