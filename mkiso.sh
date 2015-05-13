c4a333a758e68074f3606b275755ad006dfaa1a4
limelime <limelime@users.noreply.github.com>
2015-05-13 13:32:15 +0000

    Parameterized ISO creation.

--- mkiso.sh
+++ mkiso.sh
@@ -1,8 +1,10 @@
 rm -f binary/live/filesystem.squashfs
 mksquashfs chroot binary/live/filesystem.squashfs -comp xz -e boot
 
+CD_LABEL=cust-live-deb
 DATE_STRING=`date +"%Y-%m-%d_%0k.%M.%S"`
-genisoimage  -r -V cust-debdog -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o cust-live-deb-64_${DATE_STRING}.iso binary/
+ISO_FILENAME="${CD_LABEL}-64_${DATE_STRING}.iso"
+genisoimage  -r -V ${CD_LABEL} -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ${ISO_FILENAME} binary/
 
 #TODO:
 # Auto architecture.
