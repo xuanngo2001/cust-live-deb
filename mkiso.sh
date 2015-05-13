9c6fcbec3433a6f9209d98039ab7d9049dda5b05
limelime <limelime@users.noreply.github.com>
2015-05-13 15:42:10 +0000

    Log sizes.

--- mkiso.sh
+++ mkiso.sh
@@ -6,6 +6,14 @@ DATE_STRING=`date +"%Y-%m-%d_%0k.%M.%S"`
 ISO_FILENAME="${CD_LABEL}-64_${DATE_STRING}.iso"
 genisoimage  -r -V ${CD_LABEL} -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ${ISO_FILENAME} binary/
 
+
+# Log sizes
+SIZE_LOG=sizes.log
+echo "${ISO_FILENAME}" >> ${SIZE_LOG}
+du -h -c binary | sed 's/^/   /' >> ${SIZE_LOG}
+du -h -c -d 1 chroot | sed 's/^/   /' >> ${SIZE_LOG}
+echo "" >> ${SIZE_LOG}
+
 #TODO:
 # Auto architecture.
 # Add distribution name.
