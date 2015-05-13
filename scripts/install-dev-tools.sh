581ca908d724c0e057a2dbc359618c2b64f2eeb1
limelime <limelime@users.noreply.github.com>
2015-05-13 14:01:22 +0000

    Add developer tools.

---
+++ scripts/install-dev-tools.sh
@@ -0,0 +1,9 @@
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# To use debconf-set-selections
+apt-get -y --force-yes install debconf-utils
