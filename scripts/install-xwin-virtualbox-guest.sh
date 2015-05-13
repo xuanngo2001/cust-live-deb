ee818e1efabe23df03e882d1b868b15af15ad665
limelime <limelime@users.noreply.github.com>
2015-05-13 18:48:31 +0000

    Add virtualbox guest.

---
+++ scripts/install-xwin-virtualbox-guest.sh
@@ -0,0 +1,9 @@
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# These packages are in contrib repository.
+apt-get -y --force-yes install virtualbox-guest-dkms virtualbox-guest-x11
