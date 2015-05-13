4a6910ad3a01cde099b1aa633bdb77dc6462fcaa
limelime <limelime@users.noreply.github.com>
2015-05-13 14:09:44 +0000

    Skip installing recommend package.

--- scripts/install-00-base.sh
+++ scripts/install-00-base.sh
@@ -14,7 +14,11 @@ apt-get -y --force-yes install dialog dbus
 dbus-uuidgen > /var/lib/dbus/machine-id
 apt-get -y --force-yes install linux-image-amd64 live-boot
 
+# Set to skip installing recommended packages
+APT_CONF_SKIP_REC_PKG=/etc/apt/apt.conf.d/skip-recommended-packages
+echo 'APT::Install-Recommends "false"; APT::Install-Suggests "false";' >> ${APT_CONF_SKIP_REC_PKG}
 
 echo "${GV_LOG} * Mount /proc, /sys and /dev/pts."
 echo "${GV_LOG} * Generate dbus-uuidgen to /var/lib/dbus/machine-id."
 echo "${GV_LOG} * Install Linux image."
+echo "${GV_LOG} * Set ${APT_CONF_SKIP_REC_PKG} to skip installing recommended packages."
