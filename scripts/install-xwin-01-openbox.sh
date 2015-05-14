7b7755a7cd528c3cfd61a076ab7d5389820cb4de
limelime <limelime@users.noreply.github.com>
2015-05-14 12:40:03 +0000

    Fix log.

--- scripts/install-xwin-01-openbox.sh
+++ scripts/install-xwin-01-openbox.sh
@@ -9,6 +9,4 @@ apt-get -y --force-yes install openbox
 cat ${SETTINGS_DIR}/bash_profile_auto_startx >> /root/.bash_profile
 
 # Log
-echo "${GV_LOG} * Set keyboard to use US international."
-echo "${GV_LOG} * Install xorg and openbox."
-echo "${GV_LOG} * Add auto start openbox on boot."
+echo "${GV_LOG} * Install Openbox."
