5c9f50e85d4642e47b792401beefbb8daf5201db
limelime <limelime@users.noreply.github.com>
2015-05-14 20:02:36 +0000

    Manually log total size.

--- scripts/install-00-00.sh
+++ scripts/install-00-00.sh
@@ -4,7 +4,8 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Log default total size.
-GF_LOG_TOTAL_SIZE
+# Don't know why GF_LOG_TOTAL_SIZE doesn't work.
+echo "${GV_LOG} * Total size = $(du -s -BK --exclude=/proc / | head -n 1 | cut -f1)."
 
 # Execute base scripts in the following order:
 ./install-00-base.sh
