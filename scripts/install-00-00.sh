625031052dd3c7f394f4d93404726ded3428d576
limelime <limelime@users.noreply.github.com>
2015-05-14 14:48:29 +0000

    Log default total size.

--- scripts/install-00-00.sh
+++ scripts/install-00-00.sh
@@ -3,6 +3,8 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Log default total size.
+GF_LOG_TOTAL_SIZE
 
 # Execute base scripts in the following order:
 ./install-00-base.sh
