82495791387ac73adb5ecd522f8031752c8cb7db
limelime <limelime@users.noreply.github.com>
2015-05-15 16:32:13 +0000

    Implemented "Used space".

--- scripts/install-00-00.sh
+++ scripts/install-00-00.sh
@@ -6,6 +6,8 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Log default total size.
 # Don't know why GF_LOG_TOTAL_SIZE doesn't work.
 echo "${GV_LOG} * Total size = $(du -s -BK --exclude=/proc / | head -n 1 | cut -f1)."
+  # Update previous total size.
+  echo "PREV_TOTAL_SIZE=${CURRENT_TOTAL_SIZE}" > ${G_VALUES_FILE}
 
 # Execute base scripts in the following order:
 ./install-00-base.sh
--- scripts/load-global-vars-funcs.sh
+++ scripts/load-global-vars-funcs.sh
@@ -9,11 +9,27 @@
 # Prefix this variable in echo to log echoed string. CLDS stands for Custom Live Debian System.
 export GV_LOG="CLDS:"
 
+# Use to exchange value variables between different script.
+export G_VALUES_FILE=load-global-values.txt
 
 # Log total size
 GF_LOG_TOTAL_SIZE ()
 {
-  echo "${GV_LOG} * Total size = $(du -s -BK --exclude=/proc / | head -n 1 | cut -f1)."
+  # Load previous total size.
+  . ${G_VALUES_FILE}
+  
+  # Get current total size.
+  CURRENT_TOTAL_SIZE=$(du -s --exclude=/proc / | head -n 1 | cut -f1)
+  
+  # Calculate space used.
+  if [[ -z "${PREV_TOTAL_SIZE}" ]]; then PREV_TOTAL_SIZE=0; fi
+  SPACE_USED=$((${CURRENT_TOTAL_SIZE}-${PREV_TOTAL_SIZE}))
+  
+  # Log total size.
+  echo "${GV_LOG} * Total size = ${CURRENT_TOTAL_SIZE}K. Space Used = ${SPACE_USED}K."
+  
+  # Update previous total size.
+  echo "PREV_TOTAL_SIZE=${CURRENT_TOTAL_SIZE}" > ${G_VALUES_FILE}
 }
 
 export -f GF_LOG_TOTAL_SIZE
