5fa54750b4a6efc19b4b458f71343b96c633b8b4
limelime <limelime@users.noreply.github.com>
2015-05-15 16:49:14 +0000

    Log default size.

--- scripts/install-00-00.sh
+++ scripts/install-00-00.sh
@@ -5,9 +5,24 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Log default total size.
 # Don't know why GF_LOG_TOTAL_SIZE doesn't work.
-echo "${GV_LOG} * Total size = $(du -s -BK --exclude=/proc / | head -n 1 | cut -f1)."
-  # Update previous total size.
-  echo "PREV_TOTAL_SIZE=${CURRENT_TOTAL_SIZE}" > ${G_VALUES_FILE}
+	  # Load previous total size.
+	  . ${G_VALUES_FILE}
+	  
+	  # Get current total size.
+	  CURRENT_TOTAL_SIZE=$(du -s --exclude=/proc / | head -n 1 | cut -f1)
+	  
+	  # Calculate space used.
+	  if [[ -z "${PREV_TOTAL_SIZE}" ]]; then PREV_TOTAL_SIZE=0; fi
+	  SPACE_USED=$((${CURRENT_TOTAL_SIZE}-${PREV_TOTAL_SIZE}))
+	  
+	  # Log total size.
+	  echo "${GV_LOG} * Total size = ${CURRENT_TOTAL_SIZE}K. Space Used = ${SPACE_USED}K."
+	  
+	  # Update previous total size.
+	  echo "PREV_TOTAL_SIZE=${CURRENT_TOTAL_SIZE}" > ${G_VALUES_FILE}
+
+
+
 
 # Execute base scripts in the following order:
 ./install-00-base.sh
