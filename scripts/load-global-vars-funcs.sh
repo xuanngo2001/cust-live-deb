7da4b26706bcb717786c02252649a3d4913324f1
limelime <limelime@users.noreply.github.com>
2015-05-18 20:22:47 +0000

    Add global function: size of

--- scripts/load-global-vars-funcs.sh
+++ scripts/load-global-vars-funcs.sh
@@ -6,8 +6,12 @@
 # Reference: http://stackoverflow.com/a/1158231
 #########################################################
 
+###### VARIABLES ######
 # Folder containing settings data.
 export GV_SETTINGS_DIR=./settings
+
+# Folder containing settings binary data.
+export GV_BINARY_DIR=${GV_SETTINGS_DIR}/binary
 
 # Prefix this variable in echo to log echoed string. CLDS stands for Custom Live Debian System.
 export GV_LOG="CLDS:"
@@ -15,14 +19,29 @@ export GV_LOG="CLDS:"
 # Use to exchange value variables between different script.
 export G_VALUES_FILE=load-global-values.txt
 
-# Log total size
+
+###### FUNCTIONS ######
+
+# Size of file in kilobytes.
+GF_SIZE_OF()
+{
+  if [ -f $1 ] || [ -d $1 ]
+  then
+    echo $(du -s --exclude=/proc $1 | head -n 1 | cut -f1)
+  else
+    echo "ERROR: $1 not found."
+  fi
+}
+export -f GF_SIZE_OF
+
+# Log total size in kilobytes.
 GF_LOG_TOTAL_SIZE ()
 {
   # Load previous total size.
   . ./${G_VALUES_FILE}
   
   # Get current total size.
-  CURRENT_TOTAL_SIZE=$(du -s --exclude=/proc / | head -n 1 | cut -f1)
+  CURRENT_TOTAL_SIZE=$(GF_SIZE_OF /)
   
   # Calculate space used.
   if [ -z "${PREV_TOTAL_SIZE}" ]; then PREV_TOTAL_SIZE=0; fi
@@ -34,6 +53,5 @@ GF_LOG_TOTAL_SIZE ()
   # Update previous total size.
   echo "PREV_TOTAL_SIZE=${CURRENT_TOTAL_SIZE}" > ${G_VALUES_FILE}
 }
-
 export -f GF_LOG_TOTAL_SIZE
 
