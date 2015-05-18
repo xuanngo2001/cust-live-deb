0a69bd9ff71634528650cfb1bd91583260a0ce45
limelime <limelime@users.noreply.github.com>
2015-05-18 21:00:51 +0000

    Add accessory applications.

---
+++ scripts/inst-std-accessories-galculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+apt-get -y --force-yes install galculator
+
+# Log
+echo "${GV_LOG} * Install Calculator: galculator."
