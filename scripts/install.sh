1cf21fd8fb597ca4524fb019b0bd7c8f4522c13d
limelime <limelime@users.noreply.github.com>
2015-05-18 20:03:45 +0000

    Don't use 'sh'. Instead, use './script.sh'

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,11 +12,12 @@ INSTALL_LOG=/root/scripts/install.log
 
 # Log default size.
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..." 2>&1 | tee -a ${INSTALL_LOG}
+echo "${GV_LOG} * Running on SHELL=$SHELL VER=$BASH_VERSION" 2>&1 | tee -a ${INSTALL_LOG}
 GF_LOG_TOTAL_SIZE 2>&1 | tee -a ${INSTALL_LOG}
 
 # Execute script with install-<letter>*.sh
 for scriptfile in $( ls inst-*.sh ); do
-  sh ${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
+  ./${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
   # Log total size at the end of script.
   GF_LOG_TOTAL_SIZE 2>&1 | tee -a ${INSTALL_LOG}
 done
