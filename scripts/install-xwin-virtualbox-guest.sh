4a24fa1a5f08f2ce3b19a8157c1df8d6d58707b9
limelime <limelime@users.noreply.github.com>
2015-05-18 15:39:34 +0000

    Add package list variable.

--- scripts/install-xwin-virtualbox-guest.sh
+++ scripts/install-xwin-virtualbox-guest.sh
@@ -3,16 +3,15 @@
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 
+
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-# These packages are in contrib repository.
-#apt-get -y --force-yes install virtualbox-guest-dkms virtualbox-guest-x11
-#apt-get -y --force-yes install virtualbox-guest-dkms
-
 # Install required packages to compile VirtualBox guest additions.
-apt-get -y --force-yes install dkms build-essential linux-headers-$(uname -r)
+PKGS_LIST="dkms build-essential linux-headers-$(uname -r)"
+apt-get -y --force-yes install ${PKGS_LIST}
+
 
 # Install VBoxGuestAdditions
 VBOX_GUEST_ADD_ISO=${GV_SETTINGS_DIR}/VBoxGuestAdditions.iso
@@ -31,10 +30,16 @@ else
   echo "${GV_LOG} * ERROR: ${VBOX_GUEST_ADD_ISO} is missing. Get it from virtualbox.org."
 fi
 
+
+# Clean up
+apt-get -y --force-yes purge ${PKGS_LIST}
+apt-get -y --force-yes autoremove
+
+
 # Log
-echo "${GV_LOG} * Install VirtualBox guest additions to Shared folder, Shared Clipboard, Auto window scaling, etc."
+echo "${GV_LOG} * Install VirtualBox guest additions to Shared folder/clipboard, auto window scaling, etc."
 echo "${GV_LOG} * Assumed packages installed: bzip2 & Xserver installed."
-echo "${GV_LOG} * Install dkms, build-essential and linux-headers to compile VirtualBox guest additions."
+echo "${GV_LOG} * Install ${PKGS_LIST} to compile VirtualBox guest additions."
 VBOX_GUEST_ADD_VER=$(lsmod | grep -io vboxguest | xargs modinfo | grep -iw version)
 echo "${GV_LOG} * VirtualBox guest additions ${VBOX_GUEST_ADD_VER} installed."
 echo "${GV_LOG} * Delete ${VBOX_GUEST_ADD_ISO}. Space used could be negative due to this deletion."
