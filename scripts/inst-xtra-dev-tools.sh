9fdae502d9a7b6b59cc6877e053c0f4bc2904b55
limelime <limelime@users.noreply.github.com>
2015-05-19 13:36:19 +0000

    xorriso so you can create isohybrid

--- scripts/inst-xtra-dev-tools.sh
+++ scripts/inst-xtra-dev-tools.sh
@@ -7,9 +7,12 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-# To use debconf-set-selections
+# To use debconf-set-selections.
 apt-get -y --force-yes install debconf-utils
 
+# To create isohybrid.
+apt-get -y --force-yes install xorriso
 
 # Log
 echo "${GV_LOG} * Install debconf-utils so you can use debconf-set-selections."
+echo "${GV_LOG} * Install xorriso so you can create isohybrid."
