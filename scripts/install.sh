0859ea2009ea1d40dc2b630eb74668144ed59143
limelime <limelime@users.noreply.github.com>
2015-05-19 13:40:45 +0000

    Give different for total size so there will be no name clash.

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,15 +16,15 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..." 2>&1 | tee -a ${INSTALL_LOG
 echo "${GV_LOG} * Running on SHELL=$SHELL VER=$BASH_VERSION" 2>&1 | tee -a ${INSTALL_LOG}
 TOTAL_SIZE=$(GF_LOG_TOTAL_SIZE)
 echo ${TOTAL_SIZE} 2>&1 | tee -a ${INSTALL_LOG}
-echo "${SCRIPT_NAME}: ${DATE_STRING}: ${TOTAL_SIZE}" >> ${INSTALL_LOG_SIZE}
+echo "${SCRIPT_NAME}: ${DATE_STRING}: ${TOTAL_SIZE}" > ${INSTALL_LOG_SIZE}
 
 # Execute script with install-<letter>*.sh
 for scriptfile in $( ls inst-*.sh ); do
   ./${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
   # Log total size at the end of script.
-  local TOTAL_SIZE=$(GF_LOG_TOTAL_SIZE)
-  echo ${TOTAL_SIZE} 2>&1 | tee -a ${INSTALL_LOG}
-  echo "${SCRIPT_NAME}: ${DATE_STRING}: ${TOTAL_SIZE}" >> ${INSTALL_LOG_SIZE}
+  local TOTAL_SIZE_TMP=$(GF_LOG_TOTAL_SIZE)
+  echo ${TOTAL_SIZE_TMP} 2>&1 | tee -a ${INSTALL_LOG}
+  echo "${SCRIPT_NAME}: ${DATE_STRING}: ${TOTAL_SIZE_TMP}" >> ${INSTALL_LOG_SIZE}
 done
 
 # Retried the log.
