c2b7e5e3adeca8f8606152e47aa884ddc100218a
limelime <limelime@users.noreply.github.com>
2015-05-19 14:51:01 +0000

    Add comment about boot.ca.

--- scripts/inst-xtra-dev-tools.sh
+++ scripts/inst-xtra-dev-tools.sh
@@ -10,7 +10,8 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # To use debconf-set-selections.
 apt-get -y --force-yes install debconf-utils
 
-# To get isohdpfx.bin, isolinux.bin, boot.cat in order to create isohybrid.
+# To get isohdpfx.bin and isolinux.bin in order to create isohybrid.
+# Note: boot.cat is automatically created
 apt-get -y --force-yes install isolinux 
 
 # To create isohybrid.
@@ -19,4 +20,5 @@ apt-get -y --force-yes install xorriso
 # Log
 echo "${GV_LOG} * Install debconf-utils so you can use debconf-set-selections."
 echo "${GV_LOG} * Install xorriso so you can create isohybrid."
-echo "${GV_LOG} * Install isolinux to get isohdpfx.bin, isolinux.bin in order to create isohybrid."
+echo "${GV_LOG} * Install isolinux to get isohdpfx.bin and isolinux.bin in order to create isohybrid."
+echo "${GV_LOG} * Note: boot.cat is automatically created."
