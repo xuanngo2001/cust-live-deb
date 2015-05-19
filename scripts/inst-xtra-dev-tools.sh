abebda62883ac59f344833927185fa94bdef1bcd
limelime <limelime@users.noreply.github.com>
2015-05-19 14:20:34 +0000

    Add isolinux.

--- scripts/inst-xtra-dev-tools.sh
+++ scripts/inst-xtra-dev-tools.sh
@@ -10,7 +10,7 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # To use debconf-set-selections.
 apt-get -y --force-yes install debconf-utils
 
-# To get isohdpfx.bin in order to create isohybrid.
+# To get isohdpfx.bin, isolinux.bin, boot.cat in order to create isohybrid.
 apt-get -y --force-yes install isolinux 
 
 # To create isohybrid.
@@ -19,4 +19,4 @@ apt-get -y --force-yes install xorriso
 # Log
 echo "${GV_LOG} * Install debconf-utils so you can use debconf-set-selections."
 echo "${GV_LOG} * Install xorriso so you can create isohybrid."
-echo "${GV_LOG} * Install isolinux to get isohdpfx.bin in order to create isohybrid."
+echo "${GV_LOG} * Install isolinux to get isohdpfx.bin, isolinux.bin in order to create isohybrid."
