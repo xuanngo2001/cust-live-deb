8c5018cd3977c5c2c24a2eea2ba54941e9692628
limelime <limelime@users.noreply.github.com>
2015-05-20 14:15:57 +0000

    Change scite configuration file.

--- scripts/inst-std-accessories-scite.sh
+++ scripts/inst-std-accessories-scite.sh
@@ -10,5 +10,21 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install scite
 
+# Modify Scite configurations
+SCITE_CONFIG_FILE=/etc/scite/SciTEGlobal.properties
+if [ ! -f ${SCITE_CONFIG_FILE}.bck ]; then
+  cp -n ${SCITE_CONFIG_FILE} ${SCITE_CONFIG_FILE}.bck
+  cat ${GV_SETTINGS_DIR}/scite-config.txt >> ${SCITE_CONFIG_FILE}
+  
+  # Add the $(all.files)\ right after open.filter so that I can see all the files when I open the file dialog
+  # Debug: sed "s/open.filter=\\\/open.filter=\\\xuan\\n\$(all.files)\\\xuan\\nxuan/" /etc/scite/SciTEGlobal.properties | grep xuan
+  sed -i "s/open.filter=\\\/open.filter=\\\\\n\$(all.files)\\\/" /etc/scite/SciTEGlobal.properties
+
+else
+  echo "Error: ${SCRIPT_NAME} failed!"
+  echo "${SCITE_CONFIG_FILE}.bck already exists."
+fi
+
+
 # Log
 echo "${GV_LOG} * Install Text Editor: SCite."
