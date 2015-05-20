92ebbe455745832f0c4c93a00a6b84c0981e0268
limelime <limelime@users.noreply.github.com>
2015-05-20 19:36:14 +0000

    Add rejection reason.

--- scripts/inst-std-accessories-xscreensaver.sh
+++ scripts/inst-std-accessories-xscreensaver.sh
@@ -12,3 +12,9 @@ apt-get -y --force-yes install xscreensaver
 
 # Log
 echo "${GV_LOG} * Install Screensaver: xscreensaver."
+
+# REJECTION
+#  * Running inst-std-accessories-xscreensaver.sh ...
+#    * After this operation, 9,363 kB of additional disk space will be used.
+#    * Install Screensaver: xscreensaver.
+#    * Total size = 1077628K. Space Used = 13680K.
