78c02c5c6fc69d97887d6fd555a9babf42c7362d
limelime <limelime@users.noreply.github.com>
2015-05-20 17:44:08 +0000

    Add install.log in ISO.

--- mkiso.sh
+++ mkiso.sh
@@ -1,14 +1,16 @@
-# Create ISO
+# Create squashfs
 rm -f binary/live/filesystem.squashfs
 mksquashfs chroot binary/live/filesystem.squashfs -comp xz -e boot
 
-CD_LABEL=cust-live-deb
-DATE_STRING=`date +"%Y-%m-%d_%0k.%M.%S"`
-ISO_FILENAME="${CD_LABEL}-64_${DATE_STRING}.iso"
-#genisoimage  -r -V ${CD_LABEL} -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ${ISO_FILENAME} binary/
+# Copy install.log in ISOHYBRID
+INSTALL_LOG=chroot/root/scripts/install.log
+yes | cp ${INSTALL_LOG} binary/
 
 # Create ISOHYBRID.
 # Note: boot.cat is automatically created
+CD_LABEL=cust-live-deb
+DATE_STRING=`date +"%Y-%m-%d_%0k.%M.%S"`
+ISO_FILENAME="${CD_LABEL}-64_${DATE_STRING}.iso"
 xorriso -as mkisofs -r -J -joliet-long -l -cache-inodes \
 				-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16 \
 				-A "${CD_LABEL}"  -b isolinux/isolinux.bin \
