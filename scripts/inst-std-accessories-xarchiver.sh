ddacb96c92a4b7f9ecf390ae3b185bb2cc4ff345
limelime <limelime@users.noreply.github.com>
2015-05-20 20:38:17 +0000

    Add reason for rejection.

--- scripts/inst-std-accessories-xarchiver.sh
+++ scripts/inst-std-accessories-xarchiver.sh
@@ -12,3 +12,14 @@ apt-get -y --force-yes install xarchiver
 
 # Log
 echo "${GV_LOG} * Install Archive Manager: xarchiver."
+
+# REJECTION
+# Small but not intuitive: How to add multiple files in archive? Tried for 3 minutes.
+#  * Running inst-std-accessories-xarchiver.sh ...
+#    * After this operation, 1,211 kB of additional disk space will be used.
+#    * Install Archive Manager: xarchiver.
+#    * Total size = 967708K. Space Used = 1248K.
+#  * Running inst-std-accessories-fileroller.sh ...
+#    * After this operation, 103 MB of additional disk space will be used.
+#    * Install Archive Manager: File Roller.
+#    * Total size = 912564K. Space Used = 90184K.
