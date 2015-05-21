677db1c86f3f8ba1e46d498eaa57bf831b215f3f
limelime <limelime@users.noreply.github.com>
2015-05-21 16:35:56 +0000

    Add rejections.

--- scripts/inst-std-system-screenlets.sh
+++ scripts/inst-std-system-screenlets.sh
@@ -12,3 +12,17 @@ apt-get -y --force-yes install screenlets
 
 # Log
 echo "${GV_LOG} * Install System Monitor: screenlets."
+
+# REJECTION
+#  * Running inst-std-system-conky.sh ...
+#    * After this operation, 967 kB of additional disk space will be used.
+#    * Install System Monitor: conky.
+#    * Total size = 1609364K. Space Used = 1308K.
+#  * Running inst-std-system-gkrellm.sh ...
+#    * After this operation, 2,319 kB of additional disk space will be used.
+#    * Install System Monitor: gkrellm.
+#    * Total size = 1666160K. Space Used = 2124K.
+#  * Running inst-std-system-screenlets.sh ...
+#    * After this operation, 75.3 MB of additional disk space will be used.
+#    * Install System Monitor: screenlets.
+#    * Total size = 1779700K. Space Used = 77848K.
