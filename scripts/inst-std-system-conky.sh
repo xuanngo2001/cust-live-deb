950ffc4c668b15d6cb23572f8cc69712b7cb8c6f
limelime <limelime@users.noreply.github.com>
2015-05-21 16:36:34 +0000

    Remove rejection.

--- scripts/inst-std-system-conky.sh
+++ scripts/inst-std-system-conky.sh
@@ -16,13 +16,3 @@ echo "${GV_LOG} * Install System Monitor: conky."
 # Reference
 # http://www.techdrivein.com/2010/12/13-breathtaking-conky-configurations.html
 
-# REJECTION
-# Small but ugly.
-#  * Running inst-std-system-conky.sh ...
-#    * After this operation, 967 kB of additional disk space will be used.
-#    * Install System Monitor: conky.
-#    * Total size = 1609272K. Space Used = 1308K.
-#  * Running inst-std-system-gkrellm.sh ...
-#    * After this operation, 2,319 kB of additional disk space will be used.
-#    * Install System Monitor: gkrellm.
-#    * Total size = 1666072K. Space Used = 2124K.
