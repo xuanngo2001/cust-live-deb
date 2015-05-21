3be1a9753299b255271dcc5e99f5bc02d7e1cbe0
limelime <limelime@users.noreply.github.com>
2015-05-21 17:28:59 +0000

    Echo steps.

--- dd-usb.sh
+++ dd-usb.sh
@@ -33,46 +33,55 @@ fi
 
 # Delete all partitions on USD device.
 ####################################
+echo ">>>>>>>>>> Delete all partitions of ${USB_DEVICE}."
 dd if=/dev/zero of=${USB_DEVICE} bs=1k count=2048
-sync; partprobe
+sync; partprobe ${USB_DEVICE}
 
 # Transfer iso-hybrid to USD device.
 ####################################
+echo ">>>>>>>>>> Transfer iso-hybrid to ${USB_DEVICE}."
 dd if="${ISOHYBRID}" of="${USB_DEVICE}" bs=4M
 
 # Flush write operations.
-sync; partprobe
+sync; partprobe ${USB_DEVICE}
 
 # Create a new partition in the USB.
 ####################################
-# Create extended partition for the remaining USB.
+# Create an extended partition for the remaining USB.
+echo ">>>>>>>>>> Create an extended partition for the remaining ${USB_DEVICE}."
 (echo m; echo n; echo e; echo; echo; echo; echo w) | sudo fdisk "${USB_DEVICE}"
 
 # Refresh partition table. 
-sync; partprobe
+sync; partprobe ${USB_DEVICE}
 
 # Create a logical partition of X MiB.
 LOGICAL_PARTITION_SIZE_MB=200
+echo ">>>>>>>>>> Create a logical partition of ${LOGICAL_PARTITION_SIZE_MB}MB in ${USB_DEVICE}."
 (echo m; echo n; echo l; echo; echo "+${LOGICAL_PARTITION_SIZE_MB}M"; echo w) | sudo fdisk "${USB_DEVICE}"
 
 # Refresh partition table. 
-sync; partprobe
+sync; partprobe ${USB_DEVICE}
 
 # Create persistent file in the USB
 ####################################
 # Get logical partition name
 NEW_LOGICAL_PARTITION=$(fdisk -l "${USB_DEVICE}" | grep "^/dev/" | grep " 83 " | cut -d' ' -f1)
-PERSISTENCE_IMG_FILE=persistence
+
 # Format logical parition
+echo ">>>>>>>>>> Format ${NEW_LOGICAL_PARTITION} logical parition."
 mkfs.ext4 -F "${NEW_LOGICAL_PARTITION}"
 
 # Create an ext4-based image file to be used for persistence
+echo ">>>>>>>>>> Create an ext4-based image file to be used for persistence."
+PERSISTENCE_IMG_FILE=persistence
 PERSISTENCE_IMG_SIZE=$((${LOGICAL_PARTITION_SIZE_MB}-1))
+rm -f ${PERSISTENCE_IMG_FILE}
 dd if=/dev/null of=${PERSISTENCE_IMG_FILE} bs=1 count=0 seek="${PERSISTENCE_IMG_SIZE}M"
 sync
 mkfs.ext4 -F ${PERSISTENCE_IMG_FILE}
 
 # Add persistence.conf to the image file.
+echo ">>>>>>>>>> Add persistence.conf to the image file."
 PERSISTENCE_IMG_MNT_DIR=/tmp/persistence_img_mnt
 rm -rf ${PERSISTENCE_IMG_MNT_DIR}
 mkdir -p ${PERSISTENCE_IMG_MNT_DIR}
