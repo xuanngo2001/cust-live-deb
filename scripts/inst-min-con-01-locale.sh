6a6eef26480c425d27b8b40844af0a7f6575fc37
limelime <limelime@users.noreply.github.com>
2015-05-22 20:28:36 +0000

    Execute installation of locales earlier.

--- scripts/inst-min-con-01-locale.sh
+++ scripts/inst-min-con-01-locale.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# NOTE: Run this right after installing linux image. Otherwise, warning message about LANGUAGE locales not set
+#   when you apt-get install packages.
+
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -55,3 +58,13 @@ echo "${GV_LOG} * Install localepurge to purge non-UTF-8."
 
 # update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 LC_TIME=en_US.UTF-8 LC_CTYPE=en_US.UTF-8
 #   Explained: https://wiki.gentoo.org/wiki/Localization/HOWTO#Environment_variables_for_locales
+
+
+# Note: Locales warning message:
+#perl: warning: Setting locale failed.
+#perl: warning: Please check that your locale settings:
+#  LANGUAGE = (unset),
+#  LC_ALL = (unset),
+#  LANG = "en_US.UTF-8"
+#    are supported and installed on your system.
+#perl: warning: Falling back to the standard locale ("C").
