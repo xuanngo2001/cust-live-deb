2268adba616a0d8f600b0daa4280785f78a12ee5
limelime <limelime@users.noreply.github.com>
2015-05-22 21:13:53 +0000

    Add dircolors.256dark.

--- scripts/inst-min-con-bashrc.sh
+++ scripts/inst-min-con-bashrc.sh
@@ -5,3 +5,9 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 cp ${GV_SETTINGS_DIR}/bashrc_root /root/.bashrc
 
+cp ${GV_SETTINGS_DIR}/dircolors.256dark /root/.dircolors.256dark
+
+
+# Log
+echo "${GV_LOG} * Set common settings for /root/.bashrc."
+echo "${GV_LOG} * Set to use dircolors-solarized(i.e dircolors.256dark) to make ls more beautiful."
