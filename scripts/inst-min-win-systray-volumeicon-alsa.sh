cf62651efed7a3d5ae4192427bccb1df9c62db5f
limelime <limelime@users.noreply.github.com>
2015-05-24 02:01:08 +0000

    Add volume controller in system tray.

---
+++ scripts/inst-min-win-systray-volumeicon-alsa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+apt-get -y --force-yes install volumeicon-alsa
+
+# Log
+echo "${GV_LOG} * Install volume controller in system tray."
