147380bf937cf64406e75b24e6a9006b650ed7a8
limelime <limelime@users.noreply.github.com>
2015-05-24 03:17:05 +0000

    Add locate.

---
+++ scripts/inst-min-con-xtra-locate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+apt-get -y --force-yes install locate
+
+# Log
+echo "${GV_LOG} * Install locate to generate an index of files and directories."
+echo "${GV_LOG} * You can updatedb and then locate files faster."
+
