5c63fbeafb6a74fb93bdf00901908448b592ab3a
limelime <limelime@users.noreply.github.com>
2015-05-24 16:55:08 +0000

    Overwrite customization files.

--- scripts/inst-min-win-01-wm-jwm.sh
+++ scripts/inst-min-win-01-wm-jwm.sh
@@ -10,7 +10,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install jwm
-cp ${GV_SETTINGS_DIR}/.jwmrc* /root/
+yes | cp ${GV_SETTINGS_DIR}/.jwmrc* /root/
 
 
 # Log
