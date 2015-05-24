888d581080ebb9a4099ea1f3426b6c0b0763d485
limelime <limelime@users.noreply.github.com>
2015-05-24 03:17:39 +0000

    Add comment.

--- scripts/inst-std-accessories-terminal-xfce.sh
+++ scripts/inst-std-accessories-terminal-xfce.sh
@@ -13,3 +13,8 @@ apt-get -y --force-yes install xfce4-terminal
 
 # Log
 echo "${GV_LOG} * Install terminal: xfce4-terminal."
+
+#  * Running inst-std-accessories-terminal-xfce.sh ...
+#    * After this operation, 11.1 MB of additional disk space will be used.
+#    * Install terminal: xfce4-terminal.
+#    * Total size = 1007124K. Space Used = 9328K.
