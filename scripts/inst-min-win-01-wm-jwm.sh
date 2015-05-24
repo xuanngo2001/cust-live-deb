8716fb1b3fdf3ca7aeec2d642ee68fe69b0df414
limelime <limelime@users.noreply.github.com>
2015-05-24 06:09:54 +0000

    Customize JWM.

--- scripts/inst-min-win-01-wm-jwm.sh
+++ scripts/inst-min-win-01-wm-jwm.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install jwm
+cp ${GV_SETTINGS_DIR}/.jwmrc* /root/
 
+
+# Log
+echo "${GV_LOG} * Install Window Manager: JWM."
+echo "${GV_LOG} * Customize JWM."
+
+
+# Reference:
+# http://puppylinux.org/wikka/Configure_PuppyMenu
+#
