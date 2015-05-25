fc44067206b4afed6624b31b80ea8920344c5690
limelime <limelime@users.noreply.github.com>
2015-05-25 05:23:40 +0000

    update.

--- scripts/inst-std-system-conky.sh
+++ scripts/inst-std-system-conky.sh
@@ -10,9 +10,16 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install conky
 
+# Set conky.
+CONKYRC=conkyrc
+cp ${GV_SETTINGS_DIR}/${CONKYRC} /root/.${CONKYRC}
+
 # Log
 echo "${GV_LOG} * Install System Monitor: conky."
+echo "${GV_LOG} * Set ${CONKYRC}."
 
 # Reference
 # http://www.techdrivein.com/2010/12/13-breathtaking-conky-configurations.html
+# http://custom-linux.deviantart.com/gallery/39357745/Conky-Themes
+# http://conky.sourceforge.net/variables.html
 
