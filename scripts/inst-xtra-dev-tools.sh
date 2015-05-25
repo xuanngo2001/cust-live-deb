2b8d442998912de1fcebef753dee43588c6c5085
limelime <limelime@users.noreply.github.com>
2015-05-25 15:38:41 +0000

    update dev-tools.

--- scripts/inst-xtra-dev-tools.sh
+++ scripts/inst-xtra-dev-tools.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -10,19 +13,9 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # To use debconf-set-selections.
 apt-get -y --force-yes install debconf-utils
 
-# To get isohdpfx.bin and isolinux.bin in order to create isohybrid.
-# Note: boot.cat is automatically created
-apt-get -y --force-yes install isolinux 
-
-# To create isohybrid.
-apt-get -y --force-yes install xorriso
-
 # To view dependencies of packages. Better package selections.
 apt-get -y --force-yes install debtree
 
 # Log
 echo "${GV_LOG} * Install debconf-utils to use debconf-set-selections."
-echo "${GV_LOG} * Install xorriso to create isohybrid."
-echo "${GV_LOG} * Install isolinux to get isohdpfx.bin and isolinux.bin in order to create isohybrid."
-echo "${GV_LOG} * Note: boot.cat is automatically created."
 echo "${GV_LOG} * Install debtree to view dependencies of packages. Better package selections."
