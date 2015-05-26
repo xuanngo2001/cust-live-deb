c90d8a28221a02d29e9de9ad27fed4f453483cde
limelime <limelime@users.noreply.github.com>
2015-05-26 14:16:49 +0000

    Add sources list.

--- scripts/inst-min-con-deb-multimedia.sh
+++ scripts/inst-min-con-deb-multimedia.sh
@@ -9,18 +9,24 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-
-DEB_MULTIMEDIA_KEYRING=deb-multimedia-keyring_2014.2_all.deb
+# Install keyring.
+DEB_MULTIMEDIA_KEYRING=${GV_BINARY_DIR}/deb-multimedia-keyring_2014.2_all.deb
 DEB_MULTIMEDIA_KEYRING_SIZE=$(GF_SIZE_OF ${DEB_MULTIMEDIA_KEYRING})
 dpkg -i ${DEB_MULTIMEDIA_KEYRING}
-apt-get update
 rm -f ${DEB_MULTIMEDIA_KEYRING}
 
+# Add deb-multimedia sources list
+DEB_MULTIMEDIA_SRC_LIST=${GV_SETTINGS_DIR}/deb-multimedia-sources.list
+cat ${DEB_MULTIMEDIA_SRC_LIST} >> ${GV_SOURCES_LIST}
+
+# Update packages.
+apt-get update
 
 # Log
 echo "${GV_LOG} * Install ${DEB_MULTIMEDIA_KEYRING}."
-echo "${GV_LOG} * apt-get update"
+echo "${GV_LOG} * Add deb-multimedia sources list."
 echo "${GV_LOG} * Delete ${DEB_MULTIMEDIA_KEYRING}[${DEB_MULTIMEDIA_KEYRING_SIZE}K]."
+echo "${GV_LOG} * apt-get update"
 
 # Note:
 # Key might change, see http://www.deb-multimedia.org/
