a80d21563b4b98fc96b724378f8e95e056697c5c
limelime <limelime@users.noreply.github.com>
2015-05-29 18:07:59 +0000

    Add support for ntfs.

---
+++ scripts/inst-min-con-filesystem-ntfs-3g.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+apt-get -y --force-yes install ntfs-3g
+
+# Log
+echo "${GV_LOG} * Install support for ntfs: ntfs-3g."
+
