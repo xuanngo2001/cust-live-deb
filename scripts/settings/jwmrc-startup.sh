d39fd3d854d80e3b0e6c0b7dfa42a21fd0382850
limelime <limelime@users.noreply.github.com>
2015-06-02 13:48:33 +0000

    Open terminals script will add code in here.

--- scripts/settings/jwmrc-startup.sh
+++ scripts/settings/jwmrc-startup.sh
@@ -7,9 +7,3 @@ set -e
 /usr/bin/conky &
 /usr/bin/wicd-gtk -t &
 
-# Open 2 terminals.
-COLUMNS=80
-LINES=24
-Y_POSITION=240
-xfce4-terminal --title="Top" --geometry=${COLUMNS}x${LINES}+0+0 &
-xfce4-terminal --title="Bottom" --geometry=${COLUMNS}x${LINES}+0+${Y_POSITION} &
