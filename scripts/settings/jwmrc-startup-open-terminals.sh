14cba39ec68216a53b25dcc78f46445fec33f7f0
limelime <limelime@users.noreply.github.com>
2015-06-02 20:27:43 +0000

    Remove logging for terminal.

--- scripts/settings/jwmrc-startup-open-terminals.sh
+++ scripts/settings/jwmrc-startup-open-terminals.sh
@@ -1,5 +1,5 @@
 
 
 # Open 2 terminals on JWM startup.
-nohup x-terminal-emulator -e /root/open-terminals.sh 2>&1 | tee -a /dev/shm/startup.log &
+nohup x-terminal-emulator -e /root/open-terminals.sh &
 
