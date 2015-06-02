efa8a1213d3adb7592f7fbead6119fa2dca4fcd1
limelime <limelime@users.noreply.github.com>
2015-06-02 14:20:08 +0000

    Add log.

--- scripts/inst-std-internet-iceweseal.sh
+++ scripts/inst-std-internet-iceweseal.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -6,6 +12,8 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install iceweasel
 
+# Log
+echo "${GV_LOG} * Install web browser: iceweasel."
 
 # Resource:
 #   https://wiki.debian.org/Iceweasel
