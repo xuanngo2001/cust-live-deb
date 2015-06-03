b4a0f03d6178a0aff7839e4302914ead1488568c
limelime <limelime@users.noreply.github.com>
2015-06-03 15:35:11 +0000

    Add keyboard menu.

---
+++ scripts/inst-std-system-keyboard.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Insert keyboard menu in Administration
+sed -i "/Administration\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-keyboard.xml" /root/jwmrc-menus.xml
+
+
+# Log
+echo "${GV_LOG} * Insert keyboard menu in Administration."
+
+# Reference
+#   https://wiki.debian.org/Keyboard
