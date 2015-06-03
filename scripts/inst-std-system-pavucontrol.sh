a187b5e6577551194874b002938326adf1bdab65
limelime <limelime@users.noreply.github.com>
2015-06-03 15:50:21 +0000

    Add pavucontrol menu only if it is installed.

--- scripts/inst-std-system-pavucontrol.sh
+++ scripts/inst-std-system-pavucontrol.sh
@@ -9,10 +9,15 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install sound controller: pavucontrol.
 apt-get -y --force-yes install pavucontrol
+
+# Insert pavucontrol menu in Administration
+sed -i "/Administration\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-pavucontrol.xml" /root/jwmrc-menus.xml
 
 # Log
 echo "${GV_LOG} * Install PulseAudio Volume Controller: pavucontrol."
+echo "${GV_LOG} * Insert pavucontrol menu in Administration."
 
 # Reference
 # Tutorial: http://askubuntu.com/a/185827
