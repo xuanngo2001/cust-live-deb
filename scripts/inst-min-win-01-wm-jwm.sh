9e68411fc137415594d7aff6a97305d1b4b9e590
limelime <limelime@users.noreply.github.com>
2015-06-03 15:14:25 +0000

    Rename jwmrc menu file.

--- scripts/inst-min-win-01-wm-jwm.sh
+++ scripts/inst-min-win-01-wm-jwm.sh
@@ -14,7 +14,7 @@ apt-get -y --force-yes install jwm
 # Set JWM.
 JWMRC=jwmrc
 yes | cp ${GV_SETTINGS_DIR}/${JWMRC} /root/.${JWMRC}
-yes | cp ${GV_SETTINGS_DIR}/${JWMRC}-cust-menus.xml /root/
+yes | cp ${GV_SETTINGS_DIR}/${JWMRC}-menus.xml /root/
 yes | cp ${GV_SETTINGS_DIR}/${JWMRC}-tray.xml /root/
 yes | cp ${GV_SETTINGS_DIR}/${JWMRC}-startup.sh /root/
 
