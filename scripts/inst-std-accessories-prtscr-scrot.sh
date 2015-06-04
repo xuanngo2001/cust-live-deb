c8ab41a945a27b38134e5a7c1239002d24fe01ea
limelime <limelime@users.noreply.github.com>
2015-06-04 20:09:11 +0000

    Add applications to be used with PrintScreen key.

---
+++ scripts/inst-std-accessories-prtscr-scrot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+apt-get -y --force-yes install scrot
+
+# Log
+echo "${GV_LOG} * Install scrot to PrintScreen."
+
