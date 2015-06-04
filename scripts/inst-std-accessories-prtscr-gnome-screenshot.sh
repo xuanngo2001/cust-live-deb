dcaae6597f533ec4daceff23a37ce98e98bda733
limelime <limelime@users.noreply.github.com>
2015-06-04 21:05:34 +0000

    Insert keybinding for PrintScreen.

--- scripts/inst-std-accessories-prtscr-gnome-screenshot.sh
+++ scripts/inst-std-accessories-prtscr-gnome-screenshot.sh
@@ -9,9 +9,14 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-
+# Install gnome-screenshot to take screenshot.
 apt-get -y --force-yes install gnome-screenshot
 
-# Log
-echo "${GV_LOG} * Install gnome-screenshot to PrintScreen."
 
+# Insert gnome-screenshot to bind with PrintScreen key.
+sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-printscreen-gnome-screenshot.xml" /root/.jwmrc
+
+# Log
+echo "${GV_LOG} * Install gnome-screenshot to be used for PrintScreen key."
+echo "${GV_LOG} * Insert PrintScreen key binding."
+echo "${GV_LOG} * By default, screenshots are saved under ~/Pictures/."
--- scripts/inst-std-accessories-prtscr-scrot.sh
+++ scripts/inst-std-accessories-prtscr-scrot.sh
@@ -10,8 +10,13 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
+# Install scrot to take screenshot.
 apt-get -y --force-yes install scrot
 
+# Insert scrot to bind with PrintScreen key.
+sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-printscreen-scrot.xml" /root/.jwmrc
+
 # Log
-echo "${GV_LOG} * Install scrot to PrintScreen."
+echo "${GV_LOG} * Install scrot to be used for PrintScreen key."
+echo "${GV_LOG} * Insert PrintScreen key binding."
 
