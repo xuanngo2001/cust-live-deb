c59c2d8304e47176a42df0957d9bc0c2cb6f57c7
limelime <limelime@users.noreply.github.com>
2015-06-05 14:16:04 +0000

    Add mpv keybindings settings

--- scripts/inst-std-multimedia-mpv.sh
+++ scripts/inst-std-multimedia-mpv.sh
@@ -13,6 +13,10 @@ apt-get -y --force-yes install mpv
 # Add multiple aliases for MPV
 cat ${GV_SETTINGS_DIR}/bashrc-alias-mpv >> /root/.bashrc
 
+# Add mpv keybindings settings.
+cat ${GV_SETTINGS_DIR}/mpv-input.conf >> /root/.config/mpv/input.conf
+
 # Log
 echo "${GV_LOG} * Install Media Player: MPV."
 echo "${GV_LOG} * Add multiple aliases for MPV."
+echo "${GV_LOG} * Add mpv keybindings settings."
