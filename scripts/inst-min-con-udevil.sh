c047a427155d1a1436e5b2ab70ca2826ed45ddb2
limelime <limelime@users.noreply.github.com>
2015-06-09 14:49:00 +0000

    Correct template.

--- scripts/inst-min-con-udevil.sh
+++ scripts/inst-min-con-udevil.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
@@ -7,6 +8,7 @@
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
 
 # Install udevil to get devmon.
 apt-get -y --force-yes install udevil 
