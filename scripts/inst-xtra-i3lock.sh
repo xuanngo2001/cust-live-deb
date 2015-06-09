1e43901c83060a9ef7ccd680b97747857ee9b286
limelime <limelime@users.noreply.github.com>
2015-06-09 15:03:45 +0000

    Add keybing for lock screen.

--- scripts/inst-xtra-i3lock.sh
+++ scripts/inst-xtra-i3lock.sh
@@ -9,7 +9,12 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install i3lock
 apt-get -y --force-yes install i3lock
+
+# Add lockscreen keybing
+sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-lockscreen-i3lock.xml" /root/.jwmrc
 
 # Log
 echo "${GV_LOG} * Install screen locker: i3lock."
+echo "${GV_LOG} * Insert lockscreen key binding."
