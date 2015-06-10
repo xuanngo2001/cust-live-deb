4361adcf609203dd54f83e135bcf5d7dff030241
limelime <limelime@users.noreply.github.com>
2015-06-10 15:36:13 +0000

    Add set -e.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-
-###################### ERROR Handling
-COMPONENT=$1
-
-if [ -z "${COMPONENT}" ]; then
-  echo "ERROR: Argument missing:"
-  echo "./install.sh  min"
-  echo "   or"
-  echo "./install.sh  all"
-  exit 0
-fi
-
-if [ "${COMPONENT}" = "min" ]; then
-  COMPONENT="min-*"
-else
-  COMPONENT="*"
-fi
-
-######################
+set -e
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
@@ -40,18 +22,8 @@ echo "${SCRIPT_NAME}: ${DATE_STRING}: ${TOTAL_SIZE}" > ${INSTALL_LOG_SIZE}
 
 ###################### Main
 # Run scripts as per COMPONENT selected.
-CLEAN_SCRIPTNAME_PREFIX="inst-zclean-"
-for scriptfile in $( ls inst-${COMPONENT}.sh | grep -v "${CLEAN_SCRIPTNAME_PREFIX}" ); do
-  ./${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
-  # Log total size at the end of script.
-  total_size_tmp=$(GF_LOG_TOTAL_SIZE)
-  echo "${total_size_tmp}" 2>&1 | tee -a ${INSTALL_LOG}
-  echo "${scriptfile}: ${DATE_STRING}: ${total_size_tmp}" >> ${INSTALL_LOG_SIZE}
-done
-
-# Run clean up scripts.
-for scriptfile in $( ls ${CLEAN_SCRIPTNAME_PREFIX}*.sh ); do
-  ./${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
+for scriptfile in $( cat scripts-ls.lst ); do
+  ${scriptfile} 2>&1 | tee -a ${INSTALL_LOG}
   # Log total size at the end of script.
   total_size_tmp=$(GF_LOG_TOTAL_SIZE)
   echo "${total_size_tmp}" 2>&1 | tee -a ${INSTALL_LOG}
