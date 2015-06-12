7e49ef02130937b222748ebf072f7ce99d6c6f20
limelime <limelime@users.noreply.github.com>
2015-06-12 12:07:18 +0000

    Add apt-move

---
+++ scripts/repository/inst-xtra-deb-repo-apt-move.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install apt-move to create debian local repository.
+apt-get -y --force-yes install apt-move
+
+# Patch apt-move to support SHA256.
+yes | cp ${GV_SETTINGS_DIR}/apt-move-patch/move4 /usr/share/apt-move/move4
+yes | cp ${GV_SETTINGS_DIR}/apt-move-patch/apt-move /usr/bin/apt-move
+
+# Install apache 2 to turn apt-move into a webserver.
+apt-get -y --force-yes install apache2
+
+# Log
+echo "${GV_LOG} * Install apt-move to create debian local repository."
+echo "${GV_LOG} * Patch apt-move to support SHA256.."
+echo "${GV_LOG} * Install apache 2 to turn apt-move into a webserver."
+
