cfceccb89ead74a6bc416d526c28fef1d3ac8fa8
limelime <limelime@users.noreply.github.com>
2015-06-14 14:35:25 -0400

    breakdown to multiple lines for readability.

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 SYSTEM=$1
-./build-live.sh && chroot chroot/ /bin/bash -c "cd /root/scripts; chmod +x scripts-ls.sh; ./scripts-ls.sh ${SYSTEM}; chmod +x install.sh; ./install.sh" && ./mkiso.sh ${SYSTEM}
+# All below is a one-liner.
+./build-live.sh && \
+  chroot chroot/ /bin/bash -c "cd /root/scripts; \
+                               chmod +x scripts-ls.sh; \
+                               ./scripts-ls.sh ${SYSTEM}; \
+                               chmod +x install.sh; \
+                               ./install.sh" && \
+  ./mkiso.sh ${SYSTEM}
