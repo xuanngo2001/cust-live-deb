ca8ae2f18fbcefd0afac05aaff719c9f6a1dec52
limelime <limelime@users.noreply.github.com>
2015-06-14 14:41:44 -0400

    Umount system directories directly instead of chroot in the live system.

--- out-chroot.sh
+++ out-chroot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
-UMOUNT_CMD="umount -lf /sys /dev/pts /proc"
+LIVE_SYSTEM_DIR=chroot
+UMOUNT_CMD="umount -lf ${LIVE_SYSTEM_DIR}/sys ${LIVE_SYSTEM_DIR}/dev/pts ${LIVE_SYSTEM_DIR}/proc"
 echo "${UMOUNT_CMD}"
-chroot chroot/ /bin/bash -c "${UMOUNT_CMD}"
+/bin/bash -c "${UMOUNT_CMD}" || true
 
 
