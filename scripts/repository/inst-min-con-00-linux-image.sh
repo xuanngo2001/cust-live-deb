5642a60cb5c732e9611dd4c58ed43b3e544dbdb4
limelime <limelime@users.noreply.github.com>
2015-06-14 15:13:34 -0400

    Removed export PS1. It is already set in build-live.sh.

--- scripts/repository/inst-min-con-00-linux-image.sh
+++ scripts/repository/inst-min-con-00-linux-image.sh
@@ -8,7 +8,6 @@ mount none -t sysfs /sys
 mount none -t devpts /dev/pts
 export HOME=/root
 export LC_ALL=C
-export PS1="\e[01;31m(live):\W \$ \e[00m"
 
 apt-get update
 apt-get -y --force-yes install dialog dbus
