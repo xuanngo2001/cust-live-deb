b2e920db3e7511161bacc528c6d1533616f47c4e
limelime <limelime@users.noreply.github.com>
2015-06-14 15:04:06 -0400

    Improve comments. Will correctly write the number of CPU even if you messed up with .conkyrc file.

--- scripts/settings/startup-conky.sh
+++ scripts/settings/startup-conky.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -e
-# Description: 
+# Description: Change conkyrc settings according to the hardware.
+#               - Adjust the number of CPUs.
 
+### Get the number of cpu.
 CPU_NUM=$(grep -c ^processor /proc/cpuinfo)
 
-# Get out if less than 2 processors.
-if [ "${CPU_NUM}" -lt 2 ]; then
-  exit 0
-fi
-
-
+### Write CPU settings to a temporarily file.
 CONKY_CPU=/root/conky-cpus.txt
-echo "" > ${CONKY_CPU}
-for ((i=1; i<=CPU_NUM; i+=2))
+cat /dev/null > ${CONKY_CPU}
+for ((i=1; i<CPU_NUM; i+=2))
 do
   #echo -n "CPU${i}  \${cpu cpu${i}}% \${cpubar cpu${i}} " >> ${CONKY_CPU}
   echo -n "CPU${i}  \${cpu cpu${i}}% \${alignr} " >> ${CONKY_CPU}
   echo -e "CPU$((${i}+1))  \${cpu cpu$((${i}+1))}%" >> ${CONKY_CPU}
 done
 
+### Add number of CPU to .conkyrc.
 CPU_START="### CPU-START"
 CPU_END="### CPU-END"
 # Clear content between patterns.
@@ -26,7 +24,7 @@ sed  -i "/${CPU_START}/,/${CPU_END}/{//!d}" /root/.conkyrc
 # Insert content between patterns.
 sed  -i "/${CPU_START}/ r ${CONKY_CPU}" /root/.conkyrc
 
-# Clean up.
+### Clean up.
 rm -f ${CONKY_CPU}
 
 exit 0
