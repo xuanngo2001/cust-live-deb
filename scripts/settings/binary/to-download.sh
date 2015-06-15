18be687ee8fbc7f01514cd8110fb0d329597fac4
limelime <limelime@users.noreply.github.com>
2015-06-15 11:18:21 -0400

    Add what to download.

---
+++ scripts/settings/binary/to-download.sh
@@ -0,0 +1,2 @@
+wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2015.6_all.deb
+wget http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_6_all.deb
