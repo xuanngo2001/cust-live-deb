fb93239029ff9415b92e2079a35266b0ce172707
limelime <limelime@users.noreply.github.com>
2015-06-15 10:19:16 -0400

    Update deb-multimedia key.

--- scripts/repository/inst-min-con-deb-multimedia.sh
+++ scripts/repository/inst-min-con-deb-multimedia.sh
@@ -10,10 +10,12 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install keyring.
-DEB_MULTIMEDIA_KEYRING=${GV_BINARY_DIR}/deb-multimedia-keyring_2014.2_all.deb
+DEB_MULTIMEDIA_KEYRING=${GV_BINARY_DIR}/deb-multimedia-keyring_2015.6.1_all.deb
 DEB_MULTIMEDIA_KEYRING_SIZE=$(GF_SIZE_OF ${DEB_MULTIMEDIA_KEYRING})
 dpkg -i ${DEB_MULTIMEDIA_KEYRING}
 rm -f ${DEB_MULTIMEDIA_KEYRING}
+
+#apt-get -y --force-yes install deb-multimedia-keyring
 
 # Add deb-multimedia sources list
 # Ensure that these sources list have priority over the official repositories.
