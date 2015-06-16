1f38e6eb01ad833dca67019236cef962fa611500
limelime <limelime@users.noreply.github.com>
2015-06-16 14:43:43 -0400

    Add proxy.

---
+++ scripts/repository/inst-min-con-03-proxy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+PROXY=your_proxy:8080
+
+## Add proxy setting to /etc/enviroment.
+cat > /etc/environment <<EOF
+http_proxy="http://${PROXY}"
+https_proxy="http://${PROXY}"
+ftp_proxy="http://${PROXY}"
+HTTP_PROXY="http://${PROXY}"
+HTTPS_PROXY="http://${PROXY}"
+FTP_PROXY="http://${PROXY}"
+EOF
+
+## Add proxy setting to /etc/apt/apt.conf.d/proxy.
+cat > /etc/apt/apt.conf.d/proxy <<EOF
+Acquire::http::Proxy "http://${PROXY}";
+EOF
