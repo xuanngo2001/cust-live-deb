60b8f3c9b3594c1b4e7134324682426b1face9ce
limelime <limelime@users.noreply.github.com>
2015-06-16 10:06:28 -0400

    Strip zfs installation.

--- scripts/repository/inst-xtra-zfs.sh
+++ scripts/repository/inst-xtra-zfs.sh
@@ -6,13 +6,13 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-ZFS_REPO_KEY_DEB=${GV_BINARY_DIR}/zfsonlinux_6_all.deb
-ZFS_REPO_KEY_DEB_SIZE=$(GF_SIZE_OF ${ZFS_REPO_KEY_DEB})
 
 # Install required packages.
-apt-get -y --force-yes install lsb-release build-essential
-#linux-headers-$(uname -r)
+apt-get -y --force-yes install lsb-release libc6-dev
 
+# Instal ZFS
+ZFS_REPO_KEY_DEB=${GV_BINARY_DIR}/zfsonlinux_6_all.deb
+ZFS_REPO_KEY_DEB_SIZE=$(GF_SIZE_OF ${ZFS_REPO_KEY_DEB})
 dpkg -i ${ZFS_REPO_KEY_DEB}
 apt-get update
 apt-get -y --force-yes install debian-zfs
@@ -28,3 +28,8 @@ echo "${GV_LOG} * Add zfsonlinux repository to /etc/apt/sources.list.d/zfsonlinu
 echo "${GV_LOG} * Add GPG key to /etc/apt/trusted.gpg.d/zfsonlinux.gpg."
 echo "${GV_LOG} * Install debian-zfs: ${ZFS_VERSION}."
 echo "${GV_LOG} * Delete ${ZFS_REPO_KEY_DEB}[${ZFS_REPO_KEY_DEB_SIZE}K]."
+
+# Reference:
+# Tried packages from big to small:
+#	apt-get -y --force-yes install lsb-release build-essential
+#	apt-get -y --force-yes install lsb-release libc6-dev
