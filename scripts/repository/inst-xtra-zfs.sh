051506a2fd97a30c02c547cd2f80fb993798a58f
limelime <limelime@users.noreply.github.com>
2015-06-16 09:34:15 -0400

    Improve way to get the version number.

--- scripts/repository/inst-xtra-zfs.sh
+++ scripts/repository/inst-xtra-zfs.sh
@@ -10,7 +10,8 @@ ZFS_REPO_KEY_DEB=${GV_BINARY_DIR}/zfsonlinux_6_all.deb
 ZFS_REPO_KEY_DEB_SIZE=$(GF_SIZE_OF ${ZFS_REPO_KEY_DEB})
 
 # Install required packages.
-apt-get -y --force-yes install lsb-release linux-headers-$(uname -r)
+apt-get -y --force-yes install lsb-release build-essential
+#linux-headers-$(uname -r)
 
 dpkg -i ${ZFS_REPO_KEY_DEB}
 apt-get update
@@ -18,8 +19,12 @@ apt-get -y --force-yes install debian-zfs
 rm -f ${ZFS_REPO_KEY_DEB}
 
 # Log
+ZFS_VERSION=$(modinfo zfs | grep ^version | tr -s ' ')
+if [ -z "${ZFS_VERSION}" ]; then
+	ZFS_VERSION="ERROR: ZFS installation failed!"
+fi
 echo "${GV_LOG} * Install lsb-release."
 echo "${GV_LOG} * Add zfsonlinux repository to /etc/apt/sources.list.d/zfsonlinux.list."
 echo "${GV_LOG} * Add GPG key to /etc/apt/trusted.gpg.d/zfsonlinux.gpg."
-echo "${GV_LOG} * Install debian-zfs."
+echo "${GV_LOG} * Install debian-zfs: ${ZFS_VERSION}."
 echo "${GV_LOG} * Delete ${ZFS_REPO_KEY_DEB}[${ZFS_REPO_KEY_DEB_SIZE}K]."
