0fd462b588ad38688f1cb2d68def46622f032519
limelime <limelime@users.noreply.github.com>
2015-06-22 21:51:29 -0400

    Update startup-devmon.sh

--- scripts/settings/startup-devmon.sh
+++ scripts/settings/startup-devmon.sh
@@ -3,5 +3,5 @@ set -e
 
 # Start devmon on startup so that devices are automounted.
 #  Note: You need ntfs-3g driver to be able to write on ntfs partition.
-/usr/bin/devmon
+/usr/bin/devmon 2>&1 | tee -a /dev/shm/devmon.log &
 
