e7ed295f85e6892ffce8b1d580548d2e4c3bce13
limelime <limelime@users.noreply.github.com>
2015-06-23 11:25:36 -0400

    Add Monitor Switcher: arandr.

---
+++ scripts/repository/inst-std-system-monitor-switcher-arandr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install arandr.
+apt-get -y --force-yes install arandr
+
+
+# Log
+echo "${GV_LOG} * Install Monitor Switcher: arandr."
+
+
