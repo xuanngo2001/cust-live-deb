2cb7e083cb65a34bda005eeb115ece3480c963d4
limelime <limelime@users.noreply.github.com>
2015-06-23 08:02:16 -0400

    Log in RAM.

--- scripts/settings/startup-devmon.sh
+++ scripts/settings/startup-devmon.sh
@@ -3,5 +3,7 @@ set -e
 
 # Start devmon on startup so that devices are automounted.
 #  Note: You need ntfs-3g driver to be able to write on ntfs partition.
-/usr/bin/devmon 2>&1 | tee -a /dev/shm/devmon.log &
+SHM_LOG=/dev/shm/log
+mkdir -p ${SHM_LOG}
+/usr/bin/devmon 2>&1 | tee -a ${SHM_LOG}/devmon.log &
 
