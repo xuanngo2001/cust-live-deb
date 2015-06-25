22964cde0a3d6d4925258eb15ffc36ffdfb2d72f
limelime <limelime@users.noreply.github.com>
2015-06-25 14:37:53 -0400

    Add cheese.

---
+++ scripts/repository/inst-std-system-xtra-webcam-cheese.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install required packages.
+apt-get -y --force-yes install libegl*
+apt-get -y --force-yes install bumblebee
+
+# Install webcam: cheese.
+apt-get -y --force-yes install cheese
+
+
+# Log
+echo "${GV_LOG} * Install webcam: cheese."
+
+
+# bumblebee-nvidia
