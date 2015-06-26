0b75445b67b99f8b042795d680e0e834b942b420
limelime <limelime@users.noreply.github.com>
2015-06-26 12:52:18 -0400

    Add rejection.

--- scripts/repository/inst-std-system-xtra-webcam-cheese.sh
+++ scripts/repository/inst-std-system-xtra-webcam-cheese.sh
@@ -22,3 +22,15 @@ echo "${GV_LOG} * Install webcam: cheese."
 # Note: If cheese is opened but you get black screen, change to lower resolution(i.e. Cheese->Preference).
 
 # bumblebee package is not needed.
+
+
+# REJECTION:
+#  * Running inst-std-system-xtra-webcam-camorama.sh ...
+#    * After this operation, 41.1 MB of additional disk space will be used.
+#    * Install webcam: camorama.
+#    * Total size = 2043460K. Space Used = 31412K.
+#  * Running inst-std-system-xtra-webcam-cheese.sh ...
+#    * After this operation, 8,686 kB of additional disk space will be used.
+#    * After this operation, 102 MB of additional disk space will be used.
+#    * Install webcam: cheese.
+#    * Total size = 2172788K. Space Used = 129328K.
