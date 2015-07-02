88d024b5780cc274f616983d21ef867a212a256f
limelime <limelime@users.noreply.github.com>
2015-07-02 11:54:13 -0400

    Add rejection stats.

--- scripts/repository/inst-std-accessories-geeqie.sh
+++ scripts/repository/inst-std-accessories-geeqie.sh
@@ -16,3 +16,14 @@ cat ${GV_SETTINGS_DIR}/bashrc-alias-geeqie >> /root/.bashrc
 # Log
 echo "${GV_LOG} * Install Image Viewer: geeqie."
 echo "${GV_LOG} * Add alias for geeqie: g."
+
+# REJECTION:
+#  * Running inst-std-accessories-geeqie.sh ...
+#    * After this operation, 8,414 kB of additional disk space will be used.
+#    * Install Image Viewer: geeqie.
+#    * Add alias for geeqie: g.
+#    * Total size = 1201732K. Space Used = 8732K.
+#  * Running inst-std-accessories-gpicview.sh ...
+#    * After this operation, 876 kB of additional disk space will be used.
+#    * Install Image Viewer: gpicview.
+#    * Total size = 1202076K. Space Used = 344K.
