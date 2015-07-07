6b53775b5b9f69fe139b4ceff533323f6ee83386
limelime <limelime@users.noreply.github.com>
2015-07-07 13:15:04 -0400

    Breakdown galculator.

--- scripts/repository/inst-std-accessories-galculator.sh
+++ scripts/repository/inst-std-accessories-galculator.sh
@@ -8,7 +8,13 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install galculator
 apt-get -y --force-yes install galculator
+
+
+# Insert galculator in Accessories menu.
+sed -i "/Accessories\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-acc-galculator.xml" /root/jwmrc-menus.xml
 
 # Log
 echo "${GV_LOG} * Install Calculator: galculator."
+echo "${GV_LOG} * Insert galculator in Accessories menu."
