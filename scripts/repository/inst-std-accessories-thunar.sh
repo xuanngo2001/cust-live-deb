6a618f6e362521df8e9bbbf7246ffe3f486cc2e2
limelime <limelime@users.noreply.github.com>
2015-07-07 13:12:40 -0400

    Breakdown thunar menu.

--- scripts/repository/inst-std-accessories-thunar.sh
+++ scripts/repository/inst-std-accessories-thunar.sh
@@ -12,7 +12,13 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 PKGS_LIST="thunar thunar-media-tags-plugin thunar-archive-plugin thunar-volman"
 apt-get -y --force-yes install ${PKGS_LIST}
 
+
+# Insert Thunar in Accessories menu.
+sed -i "/Accessories\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-acc-thunar.xml" /root/jwmrc-menus.xml
+
+
 # Log
 echo "${GV_LOG} * Install File Manger: ${PKGS_LIST// /, }."
+echo "${GV_LOG} * Insert Thunar in Accessories menu."
 
 
