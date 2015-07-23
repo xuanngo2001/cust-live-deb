b981a97da64f8fed3317c4e7768127fe1e4d9144
limelime <limelime@users.noreply.github.com>
2015-07-23 08:48:49 -0400

    Add rar

--- scripts/repository/inst-min-con-compressors.sh
+++ scripts/repository/inst-min-con-compressors.sh
@@ -7,7 +7,9 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 apt-get -y --force-yes install bzip2
+apt-get -y --force-yes install rar
 
 # Log
 echo "${GV_LOG} * Install bzip2 compressor."
+echo "${GV_LOG} * Install rar compressor."
 
