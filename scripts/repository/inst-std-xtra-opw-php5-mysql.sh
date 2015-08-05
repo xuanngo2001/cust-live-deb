e24266b797e1ce5fad09d56b189f6d91cfbdfbfc
limelime <limelime@users.noreply.github.com>
2015-08-05 09:56:40 -0400

    Enable mod_rewrite module.

--- scripts/repository/inst-std-xtra-opw-php5-mysql.sh
+++ scripts/repository/inst-std-xtra-opw-php5-mysql.sh
@@ -12,7 +12,11 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Install php5-mysql to enable the PDO_MYSQL database driver for PHP and to run Drupal. 
 apt-get -y --force-yes install php5-mysql
 
+# Enable mod_rewrite module.
+a2enmod rewrite
+
 # Log
-echo "${GV_LOG} * Install php5-mysql to enable the PDO_MYSQL database driver for PHP and to run Drupal.."
+echo "${GV_LOG} * Install php5-mysql to enable the PDO_MYSQL database driver for PHP and to run Drupal."
+echo "${GV_LOG} * Enable mod_rewrite module."
 
 
