2409a21627fceb9b148bbcdef39146f73409f056
limelime <limelime@users.noreply.github.com>
2015-08-18 10:21:46 -0400

    Add macchanger.

---
+++ scripts/repository/inst-min-con-xtra-macchanger.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# Set macchanger to change MAC address whenever network device is up or down.
+debconf-set-selections ${GV_SETTINGS_DIR}/macchanger.seed
+
+
+# Install macchanger.
+apt-get -y --force-yes install macchanger
+
+# Log
+echo "${GV_LOG} * Install macchanger."
+echo "${GV_LOG} * Set macchanger to change MAC address whenever network device is up or down."
+echo "${GV_LOG} * To configure macchanger: dpkg-reconfigure macchanger"
+
+
