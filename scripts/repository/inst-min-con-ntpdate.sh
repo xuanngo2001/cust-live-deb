d9751d0d9be2586bf1e4b686991bfd1340fca8ef
limelime <limelime@users.noreply.github.com>
2015-08-18 10:10:55 -0400

    Add ntpdate.

--- scripts/repository/inst-min-con-ntpdate.sh
+++ scripts/repository/inst-min-con-ntpdate.sh
@@ -12,3 +12,12 @@ apt-get -y --force-yes install ntpdate
 echo "${GV_LOG} * Install client for setting system time from NTP servers: ntpdate."
 
 
+# REJECTION: ntpdate is deprecated as of September 2012: http://support.ntp.org/bin/view/Dev/DeprecatingNtpdate
+#  * Running inst-min-con-ntp.sh ...
+#    * After this operation, 1,871 kB of additional disk space will be used.
+#    * Install Network Time Protocol: ntp.
+#    * Total size = 1207648K. Space Used = 2508K.
+#  * Running inst-min-con-ntpdate.sh ...
+#    * After this operation, 233 kB of additional disk space will be used.
+#    * Install client for setting system time from NTP servers: ntpdate.
+#    * Total size = 1207944K. Space Used = 296K.
