a6b4e3ed17cb519fda1452a28b3d5c8a74aff7a4
limelime <limelime@users.noreply.github.com>
2015-08-21 15:37:53 -0400

    User alsamixer instead of pulseaudio's pavucontrol.

---
+++ scripts/repository/inst-std-system-alsamixer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Insert Alsa Mixer menu in Administration
+sed -i "/Administration\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-adm-alsamixer.xml" /root/jwmrc-menus.xml
+
+# Log
+echo "${GV_LOG} * Insert Alsa Mixer menu in Administration."
+
