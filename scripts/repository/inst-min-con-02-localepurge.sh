f896a0be7949c900378bf98ece7e88c0415b1815
limelime <limelime@users.noreply.github.com>
2015-08-24 10:56:09 -0400

    Add reconfigure localepurge command.

--- scripts/repository/inst-min-con-02-localepurge.sh
+++ scripts/repository/inst-min-con-02-localepurge.sh
@@ -17,6 +17,7 @@ apt-get -y --force-yes install localepurge
 
 # Log
 echo "${GV_LOG} * Install localepurge to purge documentation of all languages, except en, en_US, en_US.UTF-8."
+echo "${GV_LOG} * Reconfigure localepurge: dpkg-reconfigure localepurge"
 
 
 ### Test
