f94a0ee21939f3f2eb741f1871a3f96425efe9a0
limelime <limelime@users.noreply.github.com>
2015-08-24 16:30:23 -0400

    Add top and bottom window split.

--- scripts/tools/cld-win-split.sh
+++ scripts/tools/cld-win-split.sh
@@ -16,20 +16,46 @@ RIGHTMARGIN=7
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
 
-# new width and height
-W=$(( $SCREEN_WIDTH / 2 - $RIGHTMARGIN ))
-H=$(( $SCREEN_HEIGHT - $TOPMARGIN - $BOTTOMMARGIN ))
 
-# X, change to move left or right:
+### Move window to the corresponding section of the screen.
+SECTION=$(echo $1 | tr '[:upper:]' '[:lower:]')
+echo $SECTION
+case "${SECTION}" in
+  
+  left)
+    X=0
+    Y=0
+    W=$(( $SCREEN_WIDTH / 2 ))
+    H=$(( $SCREEN_HEIGHT ))
+    
+    ;;
 
-# moving to the right half of the screen:
-RIGHT=$1
-X=0
-if [ "$RIGHT" = "right" ]; then
-  X=$(( $SCREEN_WIDTH / 2 ))  
-fi
+  right)
+    X=$(( $SCREEN_WIDTH / 2 ))
+    Y=0
+    W=$(( $SCREEN_WIDTH / 2 ))
+    H=$(( $SCREEN_HEIGHT ))
+    ;;
 
+  top)
+    X=0
+    Y=0
+    W=$(( $SCREEN_WIDTH ))
+    H=$(( $SCREEN_WIDTH / 2 ))
+    ;;
+        
+  bottom)
+    X=0
+    Y=$(( $SCREEN_WIDTH / 2 ))
+    W=$(( $SCREEN_WIDTH ))
+    H=$(( $SCREEN_WIDTH / 2 ))
+    ;;
 
-Y=$TOPMARGIN
+  *)
+    echo "ERROR: Please provide section input(i.e. left, right, top or bottom)"
+    echo "   e.g. $0 right"
+    exit 1
+    ;;
+esac
 
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz && wmctrl -r :ACTIVE: -e 0,$X,$Y,$W,$H
