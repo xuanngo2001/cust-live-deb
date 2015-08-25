72567c94381bfb1127f0397126a44d8847407acb
limelime <limelime@users.noreply.github.com>
2015-08-25 12:41:09 -0400

    Fully working window split.

--- scripts/tools/cld-win-split.sh
+++ scripts/tools/cld-win-split.sh
@@ -2,51 +2,62 @@
 # Description: Move active window to the right, left, top, bottom half section of the screen.
 # Reference: http://unix.stackexchange.com/a/53228
 #            http://ssokolow.com/quicktile/
+# Note: xprop FRAME_EXTENTS doesn't seem to give the right number.
 
+# Define menu bar height
+MENU_BAR_HEIGHT=55
 
 # Get width of screen and height of screen
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
 
 
-#sum in px of all horizontal borders:
-RIGHTMARGIN=5
+### Calculate the window decorations(title bar height, borders)
+ACTIVE_WIN_ID=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
+# xprop lists the sizes in this order: left, right, top, bottom
+#   Select FRAME_EXTENTS
+#   Get the values
+#   Delete spaces
+LEFT_OFFSET=$(xprop -id ${ACTIVE_WIN_ID} | grep FRAME_EXTENTS | cut -d '=' -f 2 | tr -d ' ' | cut -d ',' -f 1)
+RIGHT_OFFSET=$(xprop -id ${ACTIVE_WIN_ID} | grep FRAME_EXTENTS | cut -d '=' -f 2 | tr -d ' ' | cut -d ',' -f 2)
+TOP_OFFSET=$(xprop -id ${ACTIVE_WIN_ID} | grep FRAME_EXTENTS | cut -d '=' -f 2 | tr -d ' ' | cut -d ',' -f 3)
+BOTTOM_OFFSET=$(xprop -id ${ACTIVE_WIN_ID} | grep FRAME_EXTENTS | cut -d '=' -f 2 | tr -d ' ' | cut -d ',' -f 4)
+
+echo $LEFT_OFFSET, $RIGHT_OFFSET, $TOP_OFFSET, $BOTTOM_OFFSET
+echo $VERTICAL_OFFSET, $HORIZONTAL_OFFSET
+
 
 ### Move window to the corresponding section of the screen.
-## Reduce the screen height due to the bottom system tray bar.
-BOTTOMMARGIN=55 # Define the height in px of the bottom system tray bar.
-SCREEN_HEIGHT=$(( $SCREEN_HEIGHT - $BOTTOMMARGIN ))
-
 SECTION=$(echo $1 | tr '[:upper:]' '[:lower:]')
 case "${SECTION}" in
   
   left)
-    X=0
+    X=$LEFT_OFFSET
     Y=0
-    W=$(( $SCREEN_WIDTH / 2 - $RIGHTMARGIN))
-    H=$(( $SCREEN_HEIGHT ))
+    W=$(( $SCREEN_WIDTH / 2 - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$(( $SCREEN_HEIGHT - $MENU_BAR_HEIGHT ))
     
     ;;
 
   right)
-    X=$(( $SCREEN_WIDTH / 2 ))
+    X=$(( $SCREEN_WIDTH / 2 + $LEFT_OFFSET ))
     Y=0
-    W=$(( $SCREEN_WIDTH / 2 - $RIGHTMARGIN))
-    H=$(( $SCREEN_HEIGHT ))
+    W=$(( $SCREEN_WIDTH / 2 - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$(( $SCREEN_HEIGHT - $MENU_BAR_HEIGHT ))
     ;;
 
   top)
     X=0
-    Y=0
-    W=$(( $SCREEN_WIDTH ))
-    H=$(( $SCREEN_HEIGHT / 2 ))
+    Y=$(( $TOP_OFFSET ))
+    W=$(( $SCREEN_WIDTH - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$(( ($SCREEN_HEIGHT-$MENU_BAR_HEIGHT) / 2 - $TOP_OFFSET - $BOTTOM_OFFSET ))
     ;;
         
   bottom)
     X=0
-    Y=$(( $SCREEN_HEIGHT / 2 ))
-    W=$(( $SCREEN_WIDTH ))
-    H=$(( $SCREEN_HEIGHT / 2 ))
+    Y=$(( ($SCREEN_HEIGHT-$MENU_BAR_HEIGHT) / 2 + $TOP_OFFSET ))
+    W=$(( $SCREEN_WIDTH - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$(( ($SCREEN_HEIGHT-$MENU_BAR_HEIGHT) / 2 - $TOP_OFFSET - $BOTTOM_OFFSET ))
     ;;
 
   *)
@@ -56,5 +67,8 @@ case "${SECTION}" in
     ;;
 esac
 
+
+
 # When resizing a window, the window must not be in a maximized state.
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz && wmctrl -r :ACTIVE: -e 0,$X,$Y,$W,$H
+
