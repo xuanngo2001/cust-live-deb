717e774f604af67f68fc61e53371b1a17973adec
limelime <limelime@users.noreply.github.com>
2015-08-26 14:33:01 -0400

    Add script: Archive install-log files modified 7 days ago.

---
+++ install-log/archive-install-log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Description: Archive install-log files modified 7 days ago.
+
+ARCHIVE_DIR="archive"
+while IFS='' read -r FILENAME || [[ -n "$FILENAME" ]]; do
+  
+  echo "Archive ${FILENAME} ..."
+  tar -zcvf ${FILENAME}.tar.gz ${FILENAME} -C ${ARCHIVE_DIR} && rm -f ${FILENAME}
+  
+done < <( find . -mtime +7 | grep -v 'tar.gz' | grep -v '.sh' )
+# Find all files modified 7 days ago
+#   Exclude tar.gz
+#   Exclude .sh
