c724fb631216b3960fb2ff47f59530bc151b0ded
limelime <limelime@users.noreply.github.com>
2015-08-27 11:03:03 -0400

    Remove export.

--- scripts/repository/inst-xtra-wifite.sh
+++ scripts/repository/inst-xtra-wifite.sh
@@ -8,9 +8,6 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-#export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true DEBCONF_DEBUG=developer
-export DEBCONF_DEBUG=developer
-
 # Install wifite
 debconf-set-selections -v ${GV_SETTINGS_DIR}/wifite-wireshark.seed
 apt-get -y --force-yes install wifite
