14af2500eb59ab012d1008d90e1c8d3f678dbfb9
limelime <limelime@users.noreply.github.com>
2015-08-27 12:41:01 -0400

    Fix: Configure macchanger to change MAC address whenever network device is up or down.

--- scripts/repository/inst-min-con-xtra-macchanger.sh
+++ scripts/repository/inst-min-con-xtra-macchanger.sh
@@ -10,12 +10,19 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-# Set macchanger to change MAC address whenever network device is up or down.
+# Preseed macchanger to change MAC address whenever network device is up or down.
+#   Note: It doesn't work but this prevents question being asked.
 debconf-set-selections -v ${GV_SETTINGS_DIR}/macchanger.seed
 
 
 # Install macchanger.
 apt-get -y --force-yes install macchanger
+
+
+# Change directly the configuration of macchanger so that it changes MAC address whenever network device is up or down.
+#   This step is needed because preseed doesn't work.
+sed -i 's/ENABLE_ON_POST_UP_DOWN=.*/ENABLE_ON_POST_UP_DOWN=true/' /etc/default/macchanger
+
 
 # Log
 echo "${GV_LOG} * Install macchanger."
