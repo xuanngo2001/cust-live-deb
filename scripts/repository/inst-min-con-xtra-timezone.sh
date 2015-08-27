3e90dfccb6a55445a15207ee23c423b93062afcf
limelime <limelime@users.noreply.github.com>
2015-08-27 13:55:27 -0400

    Clean up: Set timezone properly.

--- scripts/repository/inst-min-con-xtra-timezone.sh
+++ scripts/repository/inst-min-con-xtra-timezone.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Setup debconf parameters
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true DEBCONF_DEBUG=developer
-
 # Manually write timezone string to /etc/timezone because 'dpkg-reconfigure tzdata' non-interactive doesn't work. But interactive does.
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=510908: tzdata should use data from debconf if neither /etc/localtime nor
+#     /etc/timezone, otherwise it should default to the value read from these files
 echo "America/Montreal"> /etc/timezone
 
-# Set timezone
-debconf-set-selections -v ${GV_SETTINGS_DIR}/tzdata-config.seed
-dpkg-reconfigure tzdata
+# Need to trigger this so that timezone in /etc/timezone is set correctly. 
+dpkg-reconfigure -f noninteractive tzdata
 
 # Log
-echo "${GV_LOG} * Set time zone to EST (America/Montreal)."
+echo "${GV_LOG} * Set time zone to EST (America/Montreal) in /etc/timezone."
 
 # Debug
 # cat /etc/timezone
