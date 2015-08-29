2b1dfc4aac05d8dc96f8b5c34d0f21bc5bf9ea1e
limelime <limelime@users.noreply.github.com>
2015-08-29 13:09:02 -0400

    Update fimg.sh

--- scripts/tools/fimg.sh
+++ scripts/tools/fimg.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Description: Find all images from current directory.
 
 #http://stackoverflow.com/questions/16758105/linux-find-list-all-graphic-image-files-with-find
 find . -type f -exec file {} \; | grep -o -P '^.+: \w+ image'
