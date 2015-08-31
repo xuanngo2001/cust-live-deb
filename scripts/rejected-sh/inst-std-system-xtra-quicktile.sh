6b95e471232dd386a670a52cb1803c9b0ec03a02
limelime <limelime@users.noreply.github.com>
2015-08-31 12:34:32 -0400

    Reject quicktile. cld-win-split.sh is sufficient.

--- scripts/rejected-sh/inst-std-system-xtra-quicktile.sh
+++ scripts/rejected-sh/inst-std-system-xtra-quicktile.sh
@@ -27,3 +27,12 @@ echo "${GV_LOG} * Install dependencies: ${DEPENDENCIES_LIST// /, }."
 echo "${GV_LOG} * Install quicktile from binary."
 echo "${GV_LOG} * Set quicktile to run on JWN startup."
 echo "${GV_LOG} * Configuration file is located at /root/.config/quicktile.cfg."
+
+# REJECTION:
+#  * Running inst-std-system-xtra-quicktile.sh ...
+#    * After this operation, 3,086 kB of additional disk space will be used.
+#    * Install dependencies: python, python-gtk2, python-xlib, python-dbus, python-wnck.
+#    * Install quicktile from binary.
+#    * Set quicktile to run on JWN startup.
+#    * Configuration file is located at /root/.config/quicktile.cfg.
+#    * Disk size = 2909428K. Space Used = 2668K.
