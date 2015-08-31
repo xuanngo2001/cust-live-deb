90d7e7bfb52d2ddecaed6bf685c73f3861353e7a
limelime <limelime@users.noreply.github.com>
2015-08-31 09:34:39 -0400

    Add mtp-tools to mount smartphones.

---
+++ scripts/repository/inst-min-con-mtp-tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+apt-get -y --force-yes install mtp-tools
+
+# Log
+echo "${GV_LOG} * Install mtp-tools to mount smartphones."
+
+
