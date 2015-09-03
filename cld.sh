acdcc57c40e7de0440ce31d48349b41bef0e284b
limelime <limelime@users.noreply.github.com>
2015-09-02 23:36:10 -0400

    Update cld.sh

--- cld.sh
+++ cld.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 set -e
+ACTION=$(echo $1 | tr '[:upper:]' '[:lower:]')
+case "${ACTION}" in
+  
+  left)
+    X=$LEFT_OFFSET
+    Y=0
+    W=$(( $SCREEN_WIDTH / 2 - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$SCREEN_HEIGHT
+    
+    ;;
+
+  right)
+    X=$(( $SCREEN_WIDTH / 2 + $LEFT_OFFSET ))
+    Y=0
+    W=$(( $SCREEN_WIDTH / 2 - $LEFT_OFFSET - $RIGHT_OFFSET ))
+    H=$SCREEN_HEIGHT  
+    ;;
+
+  *)
+    echo "ERROR: Please provide ACTION(i.e. left, right, top or bottom)"
+    echo "   e.g. $0 right"
+    exit 1
+    ;;
+esac
