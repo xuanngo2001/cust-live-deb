787befee6a58561597ee92097b4d22674ff47baf
limelime <limelime@users.noreply.github.com>
2015-09-07 14:44:20 -0400

    update.

--- scripts/repository/z_inst-min-win-01-wm-jwm.sh
+++ scripts/repository/z_inst-min-win-01-wm-jwm.sh
@@ -12,11 +12,25 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install dependencies: dh-autoreconf.
 apt-get -y --force-yes install dh-autoreconf
+apt-get -y --force-yes install libx11-dev
 
+INITIAL_LOCATION=$(realpath .)
+JWM_WORK_DIR=${GV_BINARY_DIR}/jwm
+cd ${JWM_WORK_DIR}
+JWM_FILENAME=jwm-s1263.tar.gz
+tar -xvzf ${JWM_FILENAME}.tar.gz
+cd ${JWM_FILENAME}
+autoreconf
+./configure
+make
+make install
 
-
+cd ${INITIAL_LOCATION}
 
 # Log
 echo "${GV_LOG} * Install dependencies: dh-autoreconf."
 
+
+# Reference:
+#   -https://wiki.archlinux.org/index.php/JWM#Minimal_PKGBUILD_example
 
