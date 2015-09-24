bd21a5386a6d4812e7638fe064083e0caca77cc7
limelime <limelime@users.noreply.github.com>
2015-09-24 13:18:26 -0400

    Pool names are good.

--- scripts/repository/inst-min-con-ntp.sh
+++ scripts/repository/inst-min-con-ntp.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
+set -e
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_SETTINGS_DIR}: Hold settings data.
+# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install NTP.
 apt-get -y --force-yes install ntp
+
 
 # Log
 echo "${GV_LOG} * Install Network Time Protocol: ntp."
