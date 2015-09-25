7381ab8525a330c935d1fa8e5cee9f9fd0fd040c
limelime <limelime@users.noreply.github.com>
2015-09-25 17:24:47 -0400

    Extract ATA and DEVICE names correctly.

--- scripts/tools/cld-zfs-hd.sh
+++ scripts/tools/cld-zfs-hd.sh
@@ -5,7 +5,8 @@ set -e
 # Get ataX.
 while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
  
-  ATA=$(echo ${LINE}| sed -e 's/*.\/\(ata.\)\/.*/ xuan\1ngo/')
-  echo ${ATA}
+  ATA=$(echo ${LINE}| sed -e "s/.*\/\(ata[0-9]*\)\/.*/\1/")
+  DEVICE=$(echo ${LINE}| sed -e "/.*\/ata[0-9]*\/.*/ {s/.*block\///}")
+  echo "${ATA} ${DEVICE}" 
  
 done < <( ls -al /sys/block/* )
