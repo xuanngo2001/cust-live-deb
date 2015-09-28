bad155d3963378f1cd9dc27bb3a99068964486da
limelime <limelime@users.noreply.github.com>
2015-09-28 14:46:00 -0400

    Add instructions to download sweet home 3d apps.

--- scripts/settings/binary/to-download.sh
+++ scripts/settings/binary/to-download.sh
@@ -6,3 +6,7 @@ wget http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_6_al
 
 # Wifite: search self.REVISION in wifite.py.
 wget https://raw.github.com/derv82/wifite/master/wifite.py
+
+# DL SweetHome3D-5.0.jar
+# DL TexturesLibraryEditor-1.4.jar
+# DL FurnitureLibraryEditor-1.17.jar
