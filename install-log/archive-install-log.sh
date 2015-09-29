0b51ebe25184ff652d1220d43491ce5d0cecfb03
limelime <limelime@users.noreply.github.com>
2015-09-29 13:43:44 -0400

    Exclude directory.

--- install-log/archive-install-log.sh
+++ install-log/archive-install-log.sh
@@ -7,9 +7,9 @@ ARCHIVE_DIR="archive"
 while IFS='' read -r FILENAME || [[ -n "$FILENAME" ]]; do
   
   echo "Archive ${FILENAME} ..."
-  tar -zcvf ${FILENAME}.tar.gz ${FILENAME} -C ${ARCHIVE_DIR} && rm -f ${FILENAME}
+  # tar -zcvf ${FILENAME}.tar.gz ${FILENAME} -C ${ARCHIVE_DIR} && rm -f ${FILENAME}
   
-done < <( find . -mtime +7 | grep -v 'tar.gz' | grep -v '.sh' )
+done < <( find . -type f -mtime +7 | grep -v 'tar.gz' | grep -v '.sh' )
 # Find all files modified 7 days ago
 #   Exclude tar.gz
 #   Exclude .sh
