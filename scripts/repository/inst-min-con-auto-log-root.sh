d47dff880222a0201cd8827033f6fcf641ec4d39
limelime <limelime@users.noreply.github.com>
2015-09-29 14:40:15 -0400

    Remove verbose.

--- scripts/repository/inst-min-con-auto-log-root.sh
+++ scripts/repository/inst-min-con-auto-log-root.sh
@@ -4,7 +4,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-mkdir -pv /etc/systemd/system/getty@tty1.service.d/
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
 cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf<<EOF
 [Service]
 ExecStart=
