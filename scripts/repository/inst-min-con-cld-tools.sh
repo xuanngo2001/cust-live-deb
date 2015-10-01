01f44950c79917ff31f5d4fd05b13071c843b9e6
limelime <limelime@users.noreply.github.com>
2015-10-01 14:34:53 -0400

    Add symbolic links to tools scripts.

--- scripts/repository/inst-min-con-cld-tools.sh
+++ scripts/repository/inst-min-con-cld-tools.sh
@@ -10,11 +10,22 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Add custom live debian tool to /usr/local/bin/.
 TOOLS_DIR=tools
-LOCAL_BIN=/usr/local/bin/
-for file in $(ls -1 ${TOOLS_DIR}/*.sh)
+LOCAL_BIN=/usr/local/bin
+for FILE in $(ls -1 ${TOOLS_DIR}/*.sh)
 do
-  yes | cp $file $LOCAL_BIN && echo "${GV_LOG} * Copied $file to $LOCAL_BIN."
+  # Prepare variable name.
+  SCRIPT_NAME=$(echo ${FILE} | sed "s/${TOOLS_DIR}\///") # Remove 'tools/'.
+  LINK_NAME=$(echo ${SCRIPT_NAME} | sed 's/cld-//' | sed 's/\.sh//') # Create symbolic link without 'cld-' and '.sh'
+  
+  # Remove symbolic link regardless of whether it existed or not.
+  rm -f ${LOCAL_BIN}/${LINK_NAME}
+  
+  # Copy scripts and create symbolic links.
+  yes | cp ${FILE} ${LOCAL_BIN} && \
+        echo "${GV_LOG} * Copied ${FILE} to ${LOCAL_BIN}/." && \
+        ln -s ${LOCAL_BIN}/${SCRIPT_NAME} ${LOCAL_BIN}/${LINK_NAME}
+        
 done
 
 # Make all scripts executable.
-chmod +x $LOCAL_BIN/*.sh
+chmod +x ${LOCAL_BIN}/*.sh
