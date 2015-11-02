9ad645b694e11f88c841d81c65f4721ce9ea00c8
limelime <limelime@users.noreply.github.com>
2015-11-02 11:05:14 -0500

    Relocate to ${GV_CLD_ROOT_DIR}.

--- scripts/repository/inst-min-win-cld-dev-tools-pandoc.sh
+++ scripts/repository/inst-min-win-cld-dev-tools-pandoc.sh
@@ -5,6 +5,7 @@ set -e
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 # ${GV_SETTINGS_DIR}: Hold settings data.
 # ${GV_BINARY_DIR}: Hold settings binary data.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -19,10 +20,10 @@ mkdir -p ${DOC_DIR}
 yes | cp -R ${GV_SETTINGS_DIR}/doc/cust-live-deb.wiki/*.html ${DOC_DIR}
 
 # Insert documentation: developer manual in Help.
-sed -i "/Help\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-help-dev.xml" /root/jwmrc-menus.xml
+sed -i "/Help\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-help-dev.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Insert documentation: user manual in Help.
-sed -i "/Help\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-help-user.xml" /root/jwmrc-menus.xml
+sed -i "/Help\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-help-user.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 
 # Log
