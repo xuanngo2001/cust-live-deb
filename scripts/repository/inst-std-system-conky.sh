bbce75be285980a025c9e2c33a15394f2b79cb0b
limelime <limelime@users.noreply.github.com>
2015-11-02 11:08:49 -0500

    Relocate to ${GV_CLD_ROOT_DIR}.

--- scripts/repository/inst-std-system-conky.sh
+++ scripts/repository/inst-std-system-conky.sh
@@ -5,6 +5,7 @@ set -e
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 # ${GV_SETTINGS_DIR}: Hold settings data.
 # ${GV_BINARY_DIR}: Hold settings binary data.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -17,12 +18,12 @@ CONKYRC=conkyrc
 yes | cp ${GV_SETTINGS_DIR}/${CONKYRC} /root/.${CONKYRC}
 
 # Set conky to run on JWM startup.
-cat ${GV_SETTINGS_DIR}/jwmrc-startup-conky.sh >> /root/jwmrc-startup.sh
+cat ${GV_SETTINGS_DIR}/jwmrc-startup-conky.sh >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
 
 # Add on the fly settings to Conky.
-yes | cp ${GV_SETTINGS_DIR}/startup-conky.sh /root/
-chmod +x /root/startup-conky.sh
-sed -i "/## Insert your script files here ##/a /root/startup-conky.sh&" /etc/init.d/startup.sh
+yes | cp ${GV_SETTINGS_DIR}/startup-conky.sh "${GV_CLD_ROOT_DIR}"
+chmod +x "${GV_CLD_ROOT_DIR}/startup-conky.sh"
+sed -i "/## Insert your script files here ##/a ${GV_CLD_ROOT_DIR}/startup-conky.sh&" /etc/init.d/startup.sh
 
 # Log
 echo "${GV_LOG} * Install System Monitor: conky."
