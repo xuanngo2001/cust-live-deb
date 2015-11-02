0e7391694e03dc4d985625f3bc0f910777f535bc
limelime <limelime@users.noreply.github.com>
2015-11-02 11:00:23 -0500

    Relocate to GV_CLD_ROOT_DIR.

--- scripts/repository/inst-min-con-udevil.sh
+++ scripts/repository/inst-min-con-udevil.sh
@@ -5,6 +5,7 @@ set -e
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 # ${GV_SETTINGS_DIR}: Hold settings data.
 # ${GV_BINARY_DIR}: Hold settings binary data.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -15,9 +16,9 @@ apt-get -y --force-yes install udevil
 
 
 # Insert devmon in startup script.
-yes | cp ${GV_SETTINGS_DIR}/startup-devmon.sh /root/
-chmod +x /root/startup-devmon.sh
-sed -i "/## Insert your script files here ##/a /root/startup-devmon.sh&" /etc/init.d/startup.sh
+yes | cp "${GV_SETTINGS_DIR}/startup-devmon.sh" "${GV_CLD_ROOT_DIR}"
+chmod +x "${GV_CLD_ROOT_DIR}/startup-devmon.sh"
+sed -i "/## Insert your script files here ##/a ${GV_CLD_ROOT_DIR}/startup-devmon.sh&" /etc/init.d/startup.sh
 
 # Log
 echo "${GV_LOG} * Install udevil to auto mount USB devices."
