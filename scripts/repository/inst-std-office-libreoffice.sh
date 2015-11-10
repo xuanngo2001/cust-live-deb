455c242d2b9341154c404ed850f1ca58a69c15ac
limelime <limelime@users.noreply.github.com>
2015-11-10 13:13:37 -0500

    Add version and disk space usage.

--- scripts/repository/inst-std-office-libreoffice.sh
+++ scripts/repository/inst-std-office-libreoffice.sh
@@ -17,9 +17,24 @@ apt-get -y --force-yes install libreoffice-draw
 #apt-get -y --force-yes install libreoffice-math
  
 # Log
+LO_VERSION=$(libreoffice --version | head -n1)
+echo "${GV_LOG} * Install ${LO_VERSION}."
 echo "${GV_LOG} * Install Office Suite: Word processor - libreoffice-writer."
 echo "${GV_LOG} * Install Office Suite: Spreadsheet - libreoffice-calc."
 echo "${GV_LOG} * Install Office Suite: Presentation - libreoffice-impress."
 echo "${GV_LOG} * Install Office Suite: Drawing - libreoffice-draw."
 echo "${GV_LOG} * NOT install Office Suite: Database - libreoffice-base."
 echo "${GV_LOG} * NOT install Office Suite: Equation - libreoffice-math."
+
+# REJECTION:
+#  * Running inst-std-office-libreoffice.sh ...
+#    * After this operation, 273 MB of additional disk space will be used.
+#    * After this operation, 42.4 MB of additional disk space will be used.
+#    * After this operation, 18.0 MB of additional disk space will be used.
+#    * Install Office Suite: Word processor - libreoffice-writer.
+#    * Install Office Suite: Spreadsheet - libreoffice-calc.
+#    * Install Office Suite: Presentation - libreoffice-impress.
+#    * Install Office Suite: Drawing - libreoffice-draw.
+#    * NOT install Office Suite: Database - libreoffice-base.
+#    * NOT install Office Suite: Equation - libreoffice-math.
+#    * Disk size = 2439400K. Space Used = 420276K.
