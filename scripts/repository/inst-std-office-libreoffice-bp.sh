e3f5b2cf4ef019b091e58952590c7291102e8198
limelime <limelime@users.noreply.github.com>
2015-11-12 15:17:56 -0500

    Add space used for libreoffice5.

--- scripts/repository/inst-std-office-libreoffice-bp.sh
+++ scripts/repository/inst-std-office-libreoffice-bp.sh
@@ -36,26 +36,13 @@ echo "${GV_LOG} * Install Office Suite: Equation."
 echo "${GV_LOG} * https://wiki.documentfoundation.org/ReleasePlan"
 
 # REJECTION:
-#  * Running inst-std-office-libreoffice.sh ...
-#    * After this operation, 273 MB of additional disk space will be used.
-#    * After this operation, 42.4 MB of additional disk space will be used.
-#    * After this operation, 18.0 MB of additional disk space will be used.
-#    * Install Office Suite: Word processor - libreoffice-writer.
-#    * Install Office Suite: Spreadsheet - libreoffice-calc.
-#    * Install Office Suite: Presentation - libreoffice-impress.
-#    * Install Office Suite: Drawing - libreoffice-draw.
-#    * NOT install Office Suite: Database - libreoffice-base.
-#    * NOT install Office Suite: Equation - libreoffice-math.
-#    * Disk size = 2439400K. Space Used = 420276K.
-
-# REJECTION: All libreoffice applications.
-#  * Running inst-std-office-libreoffice.sh ...
-#    * After this operation, 273 MB of additional disk space will be used.
-#    * After this operation, 42.4 MB of additional disk space will be used.
-#    * After this operation, 18.0 MB of additional disk space will be used.
-#    * After this operation, 9,899 kB of additional disk space will be used.
-#    * After this operation, 1,497 kB of additional disk space will be used.
-#    * Install LibreOffice 4.3.3.2 430m0(Build:2).
+#  * Running inst-std-office-libreoffice-bp.sh ...
+#    * After this operation, 287 MB of additional disk space will be used.
+#    * After this operation, 43.8 MB of additional disk space will be used.
+#    * After this operation, 17.0 MB of additional disk space will be used.
+#    * After this operation, 9,910 kB of additional disk space will be used.
+#    * After this operation, 1,610 kB of additional disk space will be used.
+#    * Install LibreOffice 5.0.2.2 00m0(Build:2).
 #    * Install Office Suite: Word processor.
 #    * Install Office Suite: Spreadsheet.
 #    * Install Office Suite: Presentation.
@@ -63,4 +50,4 @@ echo "${GV_LOG} * https://wiki.documentfoundation.org/ReleasePlan"
 #    * Install Office Suite: Database.
 #    * Install Office Suite: Equation.
 #    * https://wiki.documentfoundation.org/ReleasePlan
-#    * Disk size = 2687820K. Space Used = 434064K.
+#    * Disk size = 2710340K. Space Used = 456568K.
