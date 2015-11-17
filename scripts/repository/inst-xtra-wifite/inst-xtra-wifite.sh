9a197daf968fb5f3502fbc12cd7927f7bc106bf3
limelime <limelime@users.noreply.github.com>
2015-11-17 10:53:21 -0500

    Refactored wifite.

--- scripts/repository/inst-xtra-wifite/inst-xtra-wifite.sh
+++ scripts/repository/inst-xtra-wifite/inst-xtra-wifite.sh
@@ -2,19 +2,17 @@
 set -e
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install wifite
-debconf-set-selections -v ${GV_SETTINGS_DIR}/wifite-wireshark.seed
+debconf-set-selections -v wifite-wireshark.seed
 apt-get -y --force-yes install wifite
 
 # Update wifite to revision 87.
-yes | cp ${GV_BINARY_DIR}/wifite87.py /usr/bin/wifite
+yes | cp wifite87.py /usr/bin/wifite
 
 # Log
 echo "${GV_LOG} * Install wifite."
