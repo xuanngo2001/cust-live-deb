7975f43a6c54d776fe7f944e786246ce022e8ca5
limelime <limelime@users.noreply.github.com>
2015-11-17 09:46:35 -0500

    Refactored win-split.

--- scripts/repository/inst-min-win-cld-win-split/inst-min-win-cld-win-split.sh
+++ scripts/repository/inst-min-win-cld-win-split/inst-min-win-cld-win-split.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -17,7 +15,7 @@ apt-get -y --force-yes install x11-utils
 
 
 # Add key binding
-sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-win-split.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
+sed -i "/NEW KEYS HERE -->/ r jwmrc-key-win-split.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
 
 
 # Log
