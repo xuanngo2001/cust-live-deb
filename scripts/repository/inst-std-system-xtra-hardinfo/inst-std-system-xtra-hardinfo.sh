5e24c0b07d6056105b6d10467e1ee43b5a1dfaab
limelime <limelime@users.noreply.github.com>
2015-11-17 10:39:12 -0500

    Refactored hardinfo.

--- scripts/repository/inst-std-system-xtra-hardinfo/inst-std-system-xtra-hardinfo.sh
+++ scripts/repository/inst-std-system-xtra-hardinfo/inst-std-system-xtra-hardinfo.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -14,7 +12,7 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 apt-get -y --force-yes install hardinfo
 
 # Insert System Information menu in Administration
-sed -i "/Administration\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-adm-hardinfo.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
+sed -i "/Administration\">/ r jwmrc-menus-adm-hardinfo.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 
 # Log
