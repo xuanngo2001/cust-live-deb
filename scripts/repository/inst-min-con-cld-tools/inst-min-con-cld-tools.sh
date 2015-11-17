96d8353ab69cd51df3285408dc8a8f9310636bc3
limelime <limelime@users.noreply.github.com>
2015-11-17 09:09:50 -0500

    Refactor cld-tools.

--- scripts/repository/inst-min-con-cld-tools/inst-min-con-cld-tools.sh
+++ scripts/repository/inst-min-con-cld-tools/inst-min-con-cld-tools.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
+set -e
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Add custom live debian tool to /usr/local/bin/.
-TOOLS_DIR=tools
 LOCAL_BIN=/usr/local/bin
-for FILE in $(ls -1 ${TOOLS_DIR}/*.sh)
+for FILE_PATH in $(find ../../tools/ -type f -name '*.sh')
 do
+  # Get the absolute path.
+  FILE_PATH=$(realpath "${FILE_PATH}")
+
   # Prepare variable name.
-  SCRIPT_NAME=$(echo ${FILE} | sed "s/${TOOLS_DIR}\///") # Remove 'tools/'.
+  SCRIPT_NAME=$(basename "${FILE_PATH}")
   LINK_NAME=$(echo ${SCRIPT_NAME} | sed 's/cld-//' | sed 's/\.sh//') # Create symbolic link without 'cld-' and '.sh'
-  
+
   # Remove symbolic link regardless of whether it existed or not.
   rm -f ${LOCAL_BIN}/${LINK_NAME}
   
   # Copy scripts and create symbolic links.
-  yes | cp ${FILE} ${LOCAL_BIN} && \
-        echo "${GV_LOG} * Copied ${FILE} to ${LOCAL_BIN}/." && \
+  yes | cp ${FILE_PATH} ${LOCAL_BIN} && \
+        echo "${GV_LOG} * Copied ${FILE_PATH} to ${LOCAL_BIN}/." && \
         ln -s ${LOCAL_BIN}/${SCRIPT_NAME} ${LOCAL_BIN}/${LINK_NAME}
         
 done
