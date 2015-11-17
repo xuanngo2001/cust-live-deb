090426dddaa2d8aa8e582c69f4269fb616ae313c
limelime <limelime@users.noreply.github.com>
2015-11-17 08:45:53 -0500

    Refactored startup script.

--- scripts/repository/inst-min-con-03-startup-script/inst-min-con-03-startup-script.sh
+++ scripts/repository/inst-min-con-03-startup-script/inst-min-con-03-startup-script.sh
@@ -3,13 +3,11 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-cp ${GV_SETTINGS_DIR}/startup.sh /etc/init.d/
+cp startup.sh /etc/init.d/
 chmod +x /etc/init.d/startup.sh
 cd /etc/init.d/
 insserv startup.sh
