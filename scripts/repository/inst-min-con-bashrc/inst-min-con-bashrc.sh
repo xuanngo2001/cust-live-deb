f392f73f72d44404e267b87a537a728444cb1b5e
limelime <limelime@users.noreply.github.com>
2015-11-17 08:53:13 -0500

    Refactored bashrc.

--- scripts/repository/inst-min-con-bashrc/inst-min-con-bashrc.sh
+++ scripts/repository/inst-min-con-bashrc/inst-min-con-bashrc.sh
@@ -4,11 +4,11 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Add common bashrc codes.
-cat ${GV_SETTINGS_DIR}/bashrc-common-* >> /root/.bashrc
+cat bashrc-common-* >> /root/.bashrc
 
 # Set dircolors
 DIR_COLORS_FILE=dircolors-gnu-ls-colors.txt
-cp ${GV_SETTINGS_DIR}/${DIR_COLORS_FILE} /root/.dircolors
+cp ${DIR_COLORS_FILE} /root/.dircolors
 
 
 # Log
