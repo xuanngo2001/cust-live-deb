a0ab4ae125add6b4b6c37c3032df1622c9d01a8b
limelime <limelime@users.noreply.github.com>
2015-11-17 09:30:29 -0500

    Remove unused variables.

--- scripts/repository/inst-min-con-xtra-php5/inst-min-con-xtra-php5.sh
+++ scripts/repository/inst-min-con-xtra-php5/inst-min-con-xtra-php5.sh
@@ -3,13 +3,11 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Install gnuplot.
+# Install php5.
 apt-get -y --force-yes install php5
 
 # Log
