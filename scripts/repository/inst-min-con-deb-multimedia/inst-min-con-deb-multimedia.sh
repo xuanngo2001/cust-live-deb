09aa0e32cdda40a805d0cc2eb34944e1001fb599
limelime <limelime@users.noreply.github.com>
2015-11-17 09:12:48 -0500

    Refactored deb multimedia.

--- scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
+++ scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
@@ -3,14 +3,12 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install keyring.
-DEB_MULTIMEDIA_KEYRING=${GV_BINARY_DIR}/deb-multimedia-keyring_2015.6.1_all.deb
+DEB_MULTIMEDIA_KEYRING=deb-multimedia-keyring_2015.6.1_all.deb
 DEB_MULTIMEDIA_KEYRING_SIZE=$(GF_SIZE_OF ${DEB_MULTIMEDIA_KEYRING})
 dpkg -i ${DEB_MULTIMEDIA_KEYRING}
 rm -f ${DEB_MULTIMEDIA_KEYRING}
@@ -20,7 +18,7 @@ rm -f ${DEB_MULTIMEDIA_KEYRING}
 # Add deb-multimedia sources list
 # Ensure that these sources list have priority over the official repositories.
 #   Put them below local repository but above official repository.
-DEB_MULTIMEDIA_SRC_LIST=${GV_SETTINGS_DIR}/sources-deb-multimedia.list
+DEB_MULTIMEDIA_SRC_LIST=sources-deb-multimedia.list
 cat ${DEB_MULTIMEDIA_SRC_LIST} >> ${GV_SOURCES_LIST}
 
 # Update packages.
