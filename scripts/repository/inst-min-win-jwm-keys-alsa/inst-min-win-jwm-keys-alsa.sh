66c5dc27571a8efc9fd949d0eadae65870be2979
limelime <limelime@users.noreply.github.com>
2015-11-17 09:49:21 -0500

    Refactored jwm keys alsa.

--- scripts/repository/inst-min-win-jwm-keys-alsa/inst-min-win-jwm-keys-alsa.sh
+++ scripts/repository/inst-min-win-jwm-keys-alsa/inst-min-win-jwm-keys-alsa.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -12,10 +10,10 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Add ALSA key binding
-sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-alsa.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
+sed -i "/NEW KEYS HERE -->/ r jwmrc-key-alsa.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
 
 # On startup, auto-detect ALSA controller.
-yes | cp "${GV_SETTINGS_DIR}/jwmrc-startup-alsa.sh" "${GV_CLD_ROOT_DIR}"
+yes | cp "jwmrc-startup-alsa.sh" "${GV_CLD_ROOT_DIR}"
 echo "${GV_CLD_ROOT_DIR}/jwmrc-startup-alsa.sh&" >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
 
 # Log
