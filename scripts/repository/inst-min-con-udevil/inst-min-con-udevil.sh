86b7e35b60ce81f184f918dd20e6035c04d80adf
limelime <limelime@users.noreply.github.com>
2015-11-17 09:21:39 -0500

    Refactored udevil.

--- scripts/repository/inst-min-con-udevil/inst-min-con-udevil.sh
+++ scripts/repository/inst-min-con-udevil/inst-min-con-udevil.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -16,7 +14,7 @@ apt-get -y --force-yes install udevil
 
 
 # Insert devmon in startup script.
-yes | cp "${GV_SETTINGS_DIR}/startup-devmon.sh" "${GV_CLD_ROOT_DIR}"
+yes | cp "startup-devmon.sh" "${GV_CLD_ROOT_DIR}"
 chmod +x "${GV_CLD_ROOT_DIR}/startup-devmon.sh"
 sed -i "/## Insert your script files here ##/a ${GV_CLD_ROOT_DIR}/startup-devmon.sh&" /etc/init.d/startup.sh
 
