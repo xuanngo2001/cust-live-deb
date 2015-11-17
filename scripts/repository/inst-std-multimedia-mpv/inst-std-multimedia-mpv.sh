18c9061541b6d64ce527b116b7a46662ec07dc94
limelime <limelime@users.noreply.github.com>
2015-11-17 10:20:15 -0500

    Refactored mpv.

--- scripts/repository/inst-std-multimedia-mpv/inst-std-multimedia-mpv.sh
+++ scripts/repository/inst-std-multimedia-mpv/inst-std-multimedia-mpv.sh
@@ -2,8 +2,6 @@
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
@@ -11,12 +9,12 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 apt-get -y --force-yes install mpv
 
 # Add multiple aliases for MPV
-cat ${GV_SETTINGS_DIR}/bashrc-alias-mpv >> /root/.bashrc
+cat bashrc-alias-mpv >> /root/.bashrc
 
 # Add useful mpv keybindings settings.
 MPV_CONFIG_DIR=/root/.config/mpv/
 mkdir -p ${MPV_CONFIG_DIR} 
-cat ${GV_SETTINGS_DIR}/mpv-input.conf >> ${MPV_CONFIG_DIR}/input.conf
+cat mpv-input.conf >> ${MPV_CONFIG_DIR}/input.conf
 
 # Log
 echo "${GV_LOG} * Install Media Player: MPV."
