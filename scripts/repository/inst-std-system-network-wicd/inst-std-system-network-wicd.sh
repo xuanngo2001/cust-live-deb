35c40273990639af7ccc01c610e7959b72782aa1
limelime <limelime@users.noreply.github.com>
2015-11-17 10:32:49 -0500

    Refactored wicd.

--- scripts/repository/inst-std-system-network-wicd/inst-std-system-network-wicd.sh
+++ scripts/repository/inst-std-system-network-wicd/inst-std-system-network-wicd.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -16,11 +14,11 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 apt-get -y --force-yes install wicd
 
 # Start wicd daemon on startup.
-#yes | cp ${GV_SETTINGS_DIR}/startup-wicd.sh "${GV_CLD_ROOT_DIR}"
+#yes | cp startup-wicd.sh "${GV_CLD_ROOT_DIR}"
 #sed -i "/## Insert your script files here ##/a ${GV_CLD_ROOT_DIR}/startup-wicd.sh&" /etc/init.d/startup.sh
 
 # Set wicd to run on JWM startup.
-cat ${GV_SETTINGS_DIR}/jwmrc-startup-wicd.sh >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
+cat jwmrc-startup-wicd.sh >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
 
 # Log
 echo "${GV_LOG} * Install Network Manager: wicd."
