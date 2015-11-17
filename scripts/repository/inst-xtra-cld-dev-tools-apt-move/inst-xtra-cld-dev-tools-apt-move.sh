c6a8ca0d4228c48a03824ef8eb2aac677f4c4eb6
limelime <limelime@users.noreply.github.com>
2015-11-17 10:46:54 -0500

    Refactored apt-move.

--- scripts/repository/inst-xtra-cld-dev-tools-apt-move/inst-xtra-cld-dev-tools-apt-move.sh
+++ scripts/repository/inst-xtra-cld-dev-tools-apt-move/inst-xtra-cld-dev-tools-apt-move.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -15,11 +13,11 @@ apt-get -y --force-yes install apt-move
 
 apt-get -y --force-yes install patch
 # Patch apt-move to support SHA256.
-#yes | cp ${GV_SETTINGS_DIR}/apt-move-patch/move4 /usr/share/apt-move/move4
-#yes | cp ${GV_SETTINGS_DIR}/apt-move-patch/apt-move /usr/bin/apt-move
+#yes | cp apt-move-patch/move4 /usr/share/apt-move/move4
+#yes | cp apt-move-patch/apt-move /usr/bin/apt-move
 
-patch -p1 /usr/bin/apt-move ${GV_SETTINGS_DIR}/apt-move-sha256issue-20130821.diff || true
-patch -p1 /usr/share/apt-move/move4 ${GV_SETTINGS_DIR}/apt-move-sha256issue-20130821.diff || true
+patch -p1 /usr/bin/apt-move apt-move-sha256issue-20130821.diff || true
+patch -p1 /usr/share/apt-move/move4 apt-move-sha256issue-20130821.diff || true
 
 
 # Install apache 2 to turn apt-move into a webserver.
