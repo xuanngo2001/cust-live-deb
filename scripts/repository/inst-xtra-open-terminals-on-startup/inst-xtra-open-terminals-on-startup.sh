d808eeb3081029380b2609425e32c7debc963543
limelime <limelime@users.noreply.github.com>
2015-11-17 10:49:52 -0500

    Refactored open terminals.

--- scripts/repository/inst-xtra-open-terminals-on-startup/inst-xtra-open-terminals-on-startup.sh
+++ scripts/repository/inst-xtra-open-terminals-on-startup/inst-xtra-open-terminals-on-startup.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -16,9 +14,9 @@ apt-get -y --force-yes install wmctrl
 apt-get -y --force-yes install x11-utils
 
 # Make open-terminals.sh run when JWM start. 
-yes | cp ${GV_SETTINGS_DIR}/open-terminals.sh "${GV_CLD_ROOT_DIR}"
+yes | cp open-terminals.sh "${GV_CLD_ROOT_DIR}"
 chmod +x "${GV_CLD_ROOT_DIR}/open-terminals.sh"
-cat ${GV_SETTINGS_DIR}/jwmrc-startup-open-terminals.sh >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
+cat jwmrc-startup-open-terminals.sh >> "${GV_CLD_ROOT_DIR}/jwmrc-startup.sh"
 
 # Log
 echo "${GV_LOG} * Install x11-xserver-utils for xrandr."
