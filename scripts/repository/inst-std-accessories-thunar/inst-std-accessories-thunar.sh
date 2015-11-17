f5745e87dffc9fc5eddd9314b679250d0a97b096
limelime <limelime@users.noreply.github.com>
2015-11-17 10:17:23 -0500

    Refactored thunar.

--- scripts/repository/inst-std-accessories-thunar/inst-std-accessories-thunar.sh
+++ scripts/repository/inst-std-accessories-thunar/inst-std-accessories-thunar.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -15,7 +13,7 @@ apt-get -y --force-yes install ${PKGS_LIST}
 
 
 # Insert Thunar in Accessories menu.
-sed -i "/Accessories\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-acc-thunar.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
+sed -i "/Accessories\">/ r jwmrc-menus-acc-thunar.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 
 # Log
