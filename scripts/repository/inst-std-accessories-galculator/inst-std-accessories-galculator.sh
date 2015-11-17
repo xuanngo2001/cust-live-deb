c04649ed9183689efe1731755853f144dbda97bf
limelime <limelime@users.noreply.github.com>
2015-11-17 09:52:59 -0500

    Refactored gcalculator

--- scripts/repository/inst-std-accessories-galculator/inst-std-accessories-galculator.sh
+++ scripts/repository/inst-std-accessories-galculator/inst-std-accessories-galculator.sh
@@ -2,8 +2,6 @@
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -14,7 +12,7 @@ apt-get -y --force-yes install galculator
 
 
 # Insert galculator in Accessories menu.
-sed -i "/Accessories\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-acc-galculator.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
+sed -i "/Accessories\">/ r jwmrc-menus-acc-galculator.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Log
 echo "${GV_LOG} * Install Calculator: galculator."
