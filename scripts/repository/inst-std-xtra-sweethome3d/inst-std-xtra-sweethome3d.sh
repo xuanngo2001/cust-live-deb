c3583254bee340a18c0312121231b48bfbc956cb
limelime <limelime@users.noreply.github.com>
2015-11-17 12:47:19 -0500

    Revert to default version of Debian repository.

--- scripts/repository/inst-std-xtra-sweethome3d/inst-std-xtra-sweethome3d.sh
+++ scripts/repository/inst-std-xtra-sweethome3d/inst-std-xtra-sweethome3d.sh
@@ -24,23 +24,10 @@ SH3D_LIST=$(echo ${SH3D_LIST} | tr -s ' ')
 # Install all listed sweethome3d.                                
 apt-get -y --force-yes install ${SH3D_LIST}
 
-# Install the latest version
-SH3D_DIR=sweethome3d
-
-SH3D_JAR=SweetHome3D-5.0.jar
-SH3D_TEXTURE_EDITOR_JAR=TexturesLibraryEditor-1.4.jar
-SH3D_FURNITURE_EDITOR_JAR=FurnitureLibraryEditor-1.17.jar
-
-yes | cp ${SH3D_DIR}/${SH3D_JAR}                  /usr/share/sweethome3d/sweethome3d.jar
-yes | cp ${SH3D_DIR}/${SH3D_TEXTURE_EDITOR_JAR}   /usr/share/sweethome3d/sweethome3d-textures-editor.jar
-yes | cp ${SH3D_DIR}/${SH3D_FURNITURE_EDITOR_JAR} /usr/share/sweethome3d/sweethome3d-furniture-editor.jar
-
 
 # Log
 echo "${GV_LOG} * Install Sweet Home 3D: ${SH3D_LIST// /, }."
-echo "${GV_LOG} * Update to ${SH3D_JAR}."
-echo "${GV_LOG} * Update to ${SH3D_TEXTURE_EDITOR_JAR}."
-echo "${GV_LOG} * Update to ${SH3D_FURNITURE_EDITOR_JAR}."
+
 
 
 
