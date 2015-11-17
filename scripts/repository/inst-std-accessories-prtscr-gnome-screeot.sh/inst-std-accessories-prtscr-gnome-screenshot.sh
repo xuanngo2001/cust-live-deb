7d944399db14e53ba81bcaff2dd6f2a7f04626aa
limelime <limelime@users.noreply.github.com>
2015-11-17 09:55:51 -0500

    Refactored gnome screenshot.

--- scripts/repository/inst-std-accessories-prtscr-gnome-screeot.sh/inst-std-accessories-prtscr-gnome-screenshot.sh
+++ scripts/repository/inst-std-accessories-prtscr-gnome-screeot.sh/inst-std-accessories-prtscr-gnome-screenshot.sh
@@ -3,8 +3,6 @@ set -e
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -15,7 +13,7 @@ apt-get -y --force-yes install gnome-screenshot
 
 
 # Insert gnome-screenshot to bind with PrintScreen key.
-sed -i "/NEW KEYS HERE -->/ r ${GV_SETTINGS_DIR}/jwmrc-key-printscreen-gnome-screenshot.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
+sed -i "/NEW KEYS HERE -->/ r jwmrc-key-printscreen-gnome-screenshot.xml" "${GV_CLD_ROOT_DIR}/jwmrc-key.xml"
 
 # Log
 echo "${GV_LOG} * Install gnome-screenshot to be used for PrintScreen key."
