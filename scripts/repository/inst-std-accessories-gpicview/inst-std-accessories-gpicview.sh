2340dbd77f23fb0aea36d192eef4beca991e8c3b
limelime <limelime@users.noreply.github.com>
2015-11-17 09:54:05 -0500

    Refactored gpicview.

--- scripts/repository/inst-std-accessories-gpicview/inst-std-accessories-gpicview.sh
+++ scripts/repository/inst-std-accessories-gpicview/inst-std-accessories-gpicview.sh
@@ -2,8 +2,6 @@
 
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -13,7 +11,7 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 apt-get -y --force-yes install gpicview
 
 # Insert gpicview in Accessories menu.
-sed -i "/Accessories\">/ r ${GV_SETTINGS_DIR}/jwmrc-menus-acc-gpicview.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
+sed -i "/Accessories\">/ r jwmrc-menus-acc-gpicview.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Log
 echo "${GV_LOG} * Install Image Viewer: gpicview."
