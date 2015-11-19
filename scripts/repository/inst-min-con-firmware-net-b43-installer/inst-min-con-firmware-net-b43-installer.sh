10e5de266aaed453bf79eb57aa6973f118258133
limelime <limelime@users.noreply.github.com>
2015-11-19 16:54:49 -0500

    Add documentation.

--- scripts/repository/inst-min-con-firmware-net-b43-installer/inst-min-con-firmware-net-b43-installer.sh
+++ scripts/repository/inst-min-con-firmware-net-b43-installer/inst-min-con-firmware-net-b43-installer.sh
@@ -11,6 +11,7 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 #apt-get -y --force-yes install firmware-b43-installer
 
 # Manually install b43 firmware without download from http://www.lwfinger.com/b43-firmware/broadcom-wl-5.100.138.tar.bz2
+#   Install script can be found at http://www.lwfinger.com/b43-firmware/install_bcm43xx_firmware_5.100.138
 apt-get -y --force-yes install b43-fwcutter
 tar -xjvf broadcom-wl-5.100.138.tar.bz2
 b43-fwcutter -w /lib/firmware broadcom-wl-5.100.138/linux/wl_apsta.o
