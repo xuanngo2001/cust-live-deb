e92d09c5bb96fccdee851581c9df376551f26b21
limelime <limelime@users.noreply.github.com>
2015-11-19 12:41:59 -0500

    Menu: Insert Blender 3D menu in Graphics. #73.

--- scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
+++ scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
@@ -13,10 +13,13 @@ apt-get -y --force-yes install libgl1-mesa-dri
 # Install blender.
 apt-get -y --force-yes install blender
 
+# Insert Blender 3D menu in Graphics
+sed -i "/Graphics\">/ r jwmrc-menus-graphics-blender.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Log
 echo "${GV_LOG} * Install required packages for blender."
-echo "${GV_LOG} * Install blender."
+echo "${GV_LOG} * Install blender3d."
+echo "${GV_LOG} * Insert Blender 3D menu in Graphics."
 
 # Note:
 # Worked on Intel graphic: apt-get -y --force-yes install libgl1-mesa-dri libgl1-mesa-glx
