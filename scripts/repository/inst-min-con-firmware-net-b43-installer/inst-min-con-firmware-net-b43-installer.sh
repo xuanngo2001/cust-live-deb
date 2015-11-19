1723fddeb9e8e6ee178c048e1819ffed1bb9d108
limelime <limelime@users.noreply.github.com>
2015-11-19 16:50:46 -0500

    Change: Manually install b43 firmware without download from http://www.lwfinger.com/b43-firmware/broadcom-wl-5.100.138.tar.bz2.

--- scripts/repository/inst-min-con-firmware-net-b43-installer/inst-min-con-firmware-net-b43-installer.sh
+++ scripts/repository/inst-min-con-firmware-net-b43-installer/inst-min-con-firmware-net-b43-installer.sh
@@ -8,7 +8,12 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # These packages are part of non-free repository.
-apt-get -y --force-yes install firmware-b43-installer
+#apt-get -y --force-yes install firmware-b43-installer
+
+# Manually install b43 firmware without download from http://www.lwfinger.com/b43-firmware/broadcom-wl-5.100.138.tar.bz2
+apt-get -y --force-yes install b43-fwcutter
+tar -xjvf broadcom-wl-5.100.138.tar.bz2
+b43-fwcutter -w /lib/firmware broadcom-wl-5.100.138/linux/wl_apsta.o
 
 # Log
 echo "${GV_LOG} * Install b43 for Dell Wireless 1397 WLAN Mini-Card Driver."
