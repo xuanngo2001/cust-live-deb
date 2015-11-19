8653f087738ff46df848de50ff23532b03bf9daf
limelime <limelime@users.noreply.github.com>
2015-11-19 13:35:47 -0500

    Add global variable, GV_SOURCES_LIST.

--- scripts/repository/inst.template.sh
+++ scripts/repository/inst.template.sh
@@ -2,9 +2,8 @@
 set -e
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
-# ${GV_SETTINGS_DIR}: Hold settings data.
-# ${GV_BINARY_DIR}: Hold settings binary data.
 # ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
