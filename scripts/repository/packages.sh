27f0537ad16a87180a22c243d98130e499871860
limelime <limelime@users.noreply.github.com>
2015-11-23 09:56:23 -0500

    Add script to extract all package names from inst-*.sh scripts. Note: it is not definitive.

---
+++ scripts/repository/packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Description: Get all package names from inst-*.sh scripts. Note: it is not definitive.
+
+PACKAGE_LIST_FILE=./packages.list
+echo "# These are potential list of packages needed." > "${PACKAGE_LIST_FILE}"
+while IFS='' read -r SCRIPT_FILE || [[ -n "$SCRIPT_FILE" ]]; do
+  
+  # Add script name.
+  echo "# ${SCRIPT_FILE}" >> "${PACKAGE_LIST_FILE}"
+  
+  # Extract package names from apt-get
+  APT_GET_PACKAGES=$(grep '^apt-get .* install ' "${SCRIPT_FILE}" || true) # If apt-get not found, still return true to prevent exiting this script.
+  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | sed 's/.* install //' | tr ' ' '\n' )
+  echo "${APT_GET_PACKAGES}" >> "${PACKAGE_LIST_FILE}"
+ 
+done < <( find . -type f -name "inst-*.sh" | sort )
+
+
