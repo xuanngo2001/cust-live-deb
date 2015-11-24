0dbfb7f8a57f2e124ad5dcb8a62e11605da554d6
limelime <limelime@users.noreply.github.com>
2015-11-24 15:28:57 -0500

    Blender requires libjemalloc1.

--- scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
+++ scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install required packages.
-apt-get -y --force-yes install libgl1-mesa-dri
+apt-get -y --force-yes install libgl1-mesa-dri libjemalloc1
 
 # Install blender.
 apt-get -y --force-yes install blender
