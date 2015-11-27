f39f95fb6a8de37a00b98bc329e5b176e923fc02
limelime <limelime@users.noreply.github.com>
2015-11-27 16:38:55 -0500

    Reject default libreoffice, too old.

--- scripts/rejected/inst-std-office-libreoffice/inst-std-office-libreoffice.sh
+++ scripts/rejected/inst-std-office-libreoffice/inst-std-office-libreoffice.sh
@@ -7,7 +7,7 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Install Office Suite: Excluding base & math
+# Install Office Suite.
 apt-get -y --force-yes install libreoffice-writer
 apt-get -y --force-yes install libreoffice-calc
 apt-get -y --force-yes install libreoffice-impress
@@ -15,7 +15,7 @@ apt-get -y --force-yes install libreoffice-draw
 apt-get -y --force-yes install libreoffice-base
 apt-get -y --force-yes install libreoffice-math
 
-# Insert LibreOffice5 menu in Office
+# Insert LibreOffice menu in Office
 sed -i "/Office\">/ r jwmrc-menus-office-libreoffice.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
  
 # Log
@@ -29,20 +29,7 @@ echo "${GV_LOG} * Install Office Suite: Database."
 echo "${GV_LOG} * Install Office Suite: Equation."
 echo "${GV_LOG} * https://wiki.documentfoundation.org/ReleasePlan"
 
-# REJECTION:
-#  * Running inst-std-office-libreoffice.sh ...
-#    * After this operation, 273 MB of additional disk space will be used.
-#    * After this operation, 42.4 MB of additional disk space will be used.
-#    * After this operation, 18.0 MB of additional disk space will be used.
-#    * Install Office Suite: Word processor - libreoffice-writer.
-#    * Install Office Suite: Spreadsheet - libreoffice-calc.
-#    * Install Office Suite: Presentation - libreoffice-impress.
-#    * Install Office Suite: Drawing - libreoffice-draw.
-#    * NOT install Office Suite: Database - libreoffice-base.
-#    * NOT install Office Suite: Equation - libreoffice-math.
-#    * Disk size = 2439400K. Space Used = 420276K.
-
-# REJECTION: All libreoffice applications.
+# REJECTION: Too old.
 #  * Running inst-std-office-libreoffice.sh ...
 #    * After this operation, 273 MB of additional disk space will be used.
 #    * After this operation, 42.4 MB of additional disk space will be used.
@@ -59,18 +46,18 @@ echo "${GV_LOG} * https://wiki.documentfoundation.org/ReleasePlan"
 #    * https://wiki.documentfoundation.org/ReleasePlan
 #    * Disk size = 2687820K. Space Used = 434064K.
 
-#   * Running inst-std-office-libreoffice.sh ...
-#     * After this operation, 287 MB of additional disk space will be used.
-#     * After this operation, 43.8 MB of additional disk space will be used.
-#     * After this operation, 17.0 MB of additional disk space will be used.
-#     * After this operation, 9,910 kB of additional disk space will be used.
-#     * After this operation, 1,610 kB of additional disk space will be used.
-#     * Install LibreOffice 5.0.2.2 00m0(Build:2).
-#     * Install Office Suite: Word processor.
-#     * Install Office Suite: Spreadsheet.
-#     * Install Office Suite: Presentation.
-#     * Install Office Suite: Drawing.
-#     * Install Office Suite: Database.
-#     * Install Office Suite: Equation.
-#     * https://wiki.documentfoundation.org/ReleasePlan
-#     * Disk size = 2705716K. Space Used = 451568K.
+#  * Running inst-std-office-libreoffice-bp.sh ...
+#    * After this operation, 287 MB of additional disk space will be used.
+#    * After this operation, 43.8 MB of additional disk space will be used.
+#    * After this operation, 17.0 MB of additional disk space will be used.
+#    * After this operation, 9,910 kB of additional disk space will be used.
+#    * After this operation, 1,610 kB of additional disk space will be used.
+#    * Install LibreOffice 5.0.2.2 00m0(Build:2).
+#    * Install Office Suite: Word processor.
+#    * Install Office Suite: Spreadsheet.
+#    * Install Office Suite: Presentation.
+#    * Install Office Suite: Drawing.
+#    * Install Office Suite: Database.
+#    * Install Office Suite: Equation.
+#    * https://wiki.documentfoundation.org/ReleasePlan
+#    * Disk size = 2710340K. Space Used = 456568K.
