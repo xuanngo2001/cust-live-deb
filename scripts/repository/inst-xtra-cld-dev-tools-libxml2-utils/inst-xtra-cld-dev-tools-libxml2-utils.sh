710bfaad63f66747f4f1817082056c1a1cc5a56b
limelime <limelime@users.noreply.github.com>
2015-11-30 11:31:45 -0500

    Add space used.

--- scripts/repository/inst-xtra-cld-dev-tools-libxml2-utils/inst-xtra-cld-dev-tools-libxml2-utils.sh
+++ scripts/repository/inst-xtra-cld-dev-tools-libxml2-utils/inst-xtra-cld-dev-tools-libxml2-utils.sh
@@ -15,3 +15,8 @@ apt-get -y --force-yes install libxml2-utils
 # Log
 echo "${GV_LOG} * Install libxml2-utils to validate xml file."
 
+# REJECTION:
+#  * Running inst-xtra-cld-dev-tools-libxml2-utils.sh ...
+#    * After this operation, 152 kB of additional disk space will be used.
+#    * Install libxml2-utils to validate xml file.
+#    * Disk size = 3253404K. Space Used = 280K.
