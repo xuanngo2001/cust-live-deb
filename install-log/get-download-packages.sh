15525d48f76877277c03ef7790393871a9c81b98
limelime <limelime@users.noreply.github.com>
2015-12-01 12:48:19 -0500

    Add error handling.

--- install-log/get-download-packages.sh
+++ install-log/get-download-packages.sh
@@ -2,8 +2,25 @@
 set -e
 # Description: Get from the install log packages that are download from the internet.
 
+INSTALL_LOG=$1
+
+# Error handling
+if [ -z "${INSTALL_LOG}" ]; then
+  echo "Error: Install log file is required. Aborted!"
+  echo " e.g.: $0 install.log-work_2015-12-01_10.31.57"
+  exit 1; 
+fi
+
+if [ ! -f "${INSTALL_LOG}" ]; then
+  echo "Error: ${INSTALL_LOG}: no such file. Aborted!"
+  echo " e.g.: $0 install.log-work_2015-12-01_10.31.57"  
+  exit 1; 
+fi
+
+INSTALL_LOG=$(realpath "${INSTALL_LOG}")
+
 # echo | get line starting with 'Get:' | Exclude localhost | Exclude '/DiffIndex ' | Exclude 'InRelease [' | Exclude 'Packages [' | Exclude 'Translation-en ['
-GET_LINE=$(cat $1 | grep "^Get:" | grep -vF 'http://localhost/' | grep -vF '/DiffIndex ' | grep -vF 'InRelease [' | grep -vF 'Packages [' | grep -vF 'Translation-en [')
+GET_LINE=$(cat "${INSTALL_LOG}" | grep "^Get:" | grep -vF 'http://localhost/' | grep -vF '/DiffIndex ' | grep -vF 'InRelease [' | grep -vF 'Packages [' | grep -vF 'Translation-en [')
   
 while IFS='' read -r LINE || [[ -n "${LINE}" ]]; do
 
