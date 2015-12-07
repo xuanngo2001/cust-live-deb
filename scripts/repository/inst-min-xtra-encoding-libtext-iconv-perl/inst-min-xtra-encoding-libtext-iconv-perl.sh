ea0b9f2145baf5b1ffca9f5bba08a5841362e1cb
limelime <limelime@users.noreply.github.com>
2015-12-07 12:29:02 -0500

    Add iconv.

---
+++ scripts/repository/inst-min-xtra-encoding-libtext-iconv-perl/inst-min-xtra-encoding-libtext-iconv-perl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+### Install libtext-iconv-perl so that you can use iconv to convert to other encoding.
+apt-get -y --force-yes install libtext-iconv-perl
+
+
+# Log
+echo "${GV_LOG} * Install libtext-iconv-perl so that you can use iconv to convert to other encoding."
+
+
