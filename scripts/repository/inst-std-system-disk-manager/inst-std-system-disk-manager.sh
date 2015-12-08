26460919322844f92bc8574838de75c8d9a02e12
limelime <limelime@users.noreply.github.com>
2015-12-08 15:44:30 -0500

    Add space used.

--- scripts/repository/inst-std-system-disk-manager/inst-std-system-disk-manager.sh
+++ scripts/repository/inst-std-system-disk-manager/inst-std-system-disk-manager.sh
@@ -20,3 +20,19 @@ echo "${GV_LOG} * Insert disk-manager menu in Administration."
 
 # Reference:
 # * http://flomertens.free.fr/disk-manager/
+
+# REJECTION:
+#  * Running inst-std-system-disk-manager.sh ...
+#    * After this operation, 14.3 MB of additional disk space will be used.
+#    * Install Auto Mount Partition Manager: Disk Manager.
+#    * Insert disk-manager menu in Administration.
+#    * Disk size = 3503468K. Space Used = 24548K.
+#  * Running inst-std-system-gnome-disk-utility.sh ...
+#    * After this operation, 11.1 MB of additional disk space will be used.
+#    * Install Partition Manager: gnome-disk-utility.
+#    * Disk size = 3509744K. Space Used = 6276K.
+#  * Running inst-std-system-gparted.sh ...
+#    * After this operation, 17.3 MB of additional disk space will be used.
+#    * Install Partition Manager: gparted.
+#    * Insert gparted menu in Administration.
+#    * Disk size = 3525672K. Space Used = 15928K.
--- scripts/repository/inst-std-system-gparted/inst-std-system-gparted.sh
+++ scripts/repository/inst-std-system-gparted/inst-std-system-gparted.sh
@@ -19,3 +19,19 @@ echo "${GV_LOG} * Insert gparted menu in Administration."
 
 # Reference
 # Tutorial: http://askubuntu.com/a/185827
+
+# REJECTION:
+#  * Running inst-std-system-disk-manager.sh ...
+#    * After this operation, 14.3 MB of additional disk space will be used.
+#    * Install Auto Mount Partition Manager: Disk Manager.
+#    * Insert disk-manager menu in Administration.
+#    * Disk size = 3503468K. Space Used = 24548K.
+#  * Running inst-std-system-gnome-disk-utility.sh ...
+#    * After this operation, 11.1 MB of additional disk space will be used.
+#    * Install Partition Manager: gnome-disk-utility.
+#    * Disk size = 3509744K. Space Used = 6276K.
+#  * Running inst-std-system-gparted.sh ...
+#    * After this operation, 17.3 MB of additional disk space will be used.
+#    * Install Partition Manager: gparted.
+#    * Insert gparted menu in Administration.
+#    * Disk size = 3525672K. Space Used = 15928K.
