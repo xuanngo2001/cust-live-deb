9084fb3cc65aa2622c870c1c4afc5796d684d7c9
limelime <limelime@users.noreply.github.com>
2015-12-14 15:35:57 -0500

    Add support for Flash.

--- scripts/repository/inst-std-internet-iceweasel/inst-std-internet-iceweasel.sh
+++ scripts/repository/inst-std-internet-iceweasel/inst-std-internet-iceweasel.sh
@@ -9,9 +9,13 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install web browser: iceweasel.
 apt-get -y --force-yes install iceweasel
+apt-get -y --force-yes install flashplugin-nonfree
 
 # Log
 echo "${GV_LOG} * Install web browser: iceweasel."
+echo "${GV_LOG} * To update Adobe Flash Player: update-flashplugin-nonfree --install."
+echo "${GV_LOG} * To test Adobe Flash Player: http://www.cmegroup.com/trading/interest-rates/countdown-to-fomc.html."
+
 
 # Resource:
 #   https://wiki.debian.org/Iceweasel
