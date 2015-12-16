0d41a83f64cf32934a32818c36294cca11dc3006
limelime <limelime@users.noreply.github.com>
2015-12-16 12:43:22 -0500

    Add required packages to compile VirtualBox.

--- scripts/repository/inst-xtra-virtualbox5/inst-xtra-virtualbox5.sh
+++ scripts/repository/inst-xtra-virtualbox5/inst-xtra-virtualbox5.sh
@@ -9,19 +9,29 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-# Install latest version of Virtualbox
+# Install Virtualbox source repository
 GF_ADD_SOURCE_LIST "deb http://download.virtualbox.org/virtualbox/debian jessie contrib"
 apt-key add oracle_vbox.asc
 apt-get update
+
+# Install required packages to compile VirtualBox.
+PKGS_LIST="dkms libc6-dev linux-headers-$(uname -r)"
+apt-get -y --force-yes install ${PKGS_LIST}
+
+Install VirtualBox
 apt-get -y --force-yes install virtualbox-5.0
 
 
+# Clean up
+apt-get -y --force-yes purge ${PKGS_LIST}
+apt-get -y --force-yes autoremove
 
 # Log
 VBOX_VER=$(VBoxManage --version)
 if [ -z "${VBOX_VER}" ]; then
   VBOX_VER="Error: ${SCRIPT_NAME} installation failed!"
 fi
+echo "${GV_LOG} * Install ${PKGS_LIST// /, } to compile VirtualBox."
 echo "${GV_LOG} * Install VirtualBox ${VBOX_VER}."
 
 # Reference:
