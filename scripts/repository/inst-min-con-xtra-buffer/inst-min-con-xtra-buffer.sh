b2dfa1289775ab30b197b0cf18246878c702eaa9
limelime <limelime@users.noreply.github.com>
2015-12-28 09:34:16 -0500

    Add buffer. #107.

---
+++ scripts/repository/inst-min-con-xtra-buffer/inst-min-con-xtra-buffer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+apt-get -y --force-yes install buffer
+
+# Log
+echo "${GV_LOG} * Install buffer."
+
