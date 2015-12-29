b51a929b4b085ac2aa1d6ef62041821db46276d1
limelime <limelime@users.noreply.github.com>
2015-12-29 10:39:44 -0500

    Archive install.log that starts with ok-*

--- install-log/archive-install-log.sh
+++ install-log/archive-install-log.sh
@@ -9,7 +9,7 @@ while IFS='' read -r FILENAME || [[ -n "$FILENAME" ]]; do
   echo "Archive ${FILENAME} ..."
   tar -zcvf ${ARCHIVE_DIR}/${FILENAME}.tar.gz ${FILENAME} && rm -f ${FILENAME}
   
-done < <( find . -type f -mtime +7 | grep -v 'tar.gz' | grep -v '.sh' )
+done < <( find . -type f -mtime +7 | grep '^ok-' | grep -v 'tar.gz' | grep -v '.sh' )
 # Find all files modified 7 days ago
 #   Exclude tar.gz
 #   Exclude .sh
