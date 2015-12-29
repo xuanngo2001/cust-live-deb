9c5483b0a3f8f27500ffd4879611cca3b2ee98a1
limelime <limelime@users.noreply.github.com>
2015-12-29 16:23:59 -0500

    Add iat. Close #100.

---
+++ scripts/repository/inst-min-con-xtra-iso-iat/inst-min-con-xtra-iso-iat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install iat
+apt-get -y --force-yes install iat
+
+
+# Log
+echo "${GV_LOG} * Install iat to convert BIN, MDF, PDI, CDI, NRG, and B5I into ISO-9660."
+
