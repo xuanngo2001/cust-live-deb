61b0efd75b68fd64723b9b3b633ad4c4d62ab73f
limelime <limelime@users.noreply.github.com>
2016-01-07 14:42:39 -0500

    Add size.

--- scripts/repository/inst-min-con-xtra-pdf-python-pdfminer/inst-min-con-xtra-pdf-python-pdfminer.sh
+++ scripts/repository/inst-min-con-xtra-pdf-python-pdfminer/inst-min-con-xtra-pdf-python-pdfminer.sh
@@ -15,3 +15,21 @@ apt-get -y --force-yes install python-pdfminer
 
 # Log
 echo "${GV_LOG} * Install python-pdfminer."
+
+# * >>>>>>>>> Running inst-min-con-xtra-pdf-poppler-utils.sh ...
+#   * After this operation, 4,134 kB of additional disk space will be used.
+#   * Install poppler-utils.
+#   * Disk size = 2468992K. Space Used = 5536K.
+#   * The following NEW packages will be installed:
+#   ```bash
+#   libopenjpeg5 libpoppler46 poppler-utils
+#   ```
+# * >>>>>>>>> Running inst-min-con-xtra-pdf-python-pdfminer.sh ...
+#   * After this operation, 16.9 MB of additional disk space will be used.
+#   * Install python-pdfminer.
+#   * Disk size = 2500256K. Space Used = 31264K.
+#   * The following NEW packages will be installed:
+#   ```bash
+#   libpython-stdlib libpython2.7-minimal libpython2.7-stdlib mime-support
+#   python python-minimal python-pdfminer python2.7 python2.7-minimal
+#   ```
