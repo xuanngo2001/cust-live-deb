68d6f2daa2b4679cc39b50642ad93fabca72cde6
limelime <limelime@users.noreply.github.com>
2016-01-07 11:23:07 -0500

    Parameterize chroot directory path.

--- out-chroot.sh
+++ out-chroot.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 set -e
+# Description: Get out of chroot environment.
 
-LIVE_SYSTEM_DIR=chroot
-UMOUNT_CMD="umount -lf ${LIVE_SYSTEM_DIR}/sys ${LIVE_SYSTEM_DIR}/dev/pts ${LIVE_SYSTEM_DIR}/proc"
+CHROOT_DIR=$1
+
+# Error Handling
+  CMD_EXAMPLES=$(printf "%s\n%s\n" \
+                        "  e.g. $0 <CHROOT_DIR>"\
+                        "  e.g. $0 /path/to/chroot/"\
+                )
+  if [ ! -d "${CHROOT_DIR}" ]; then
+    echo "Error: Chroot directory: ${CHROOT_DIR}: no such directory. Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
+  
+
+# Unmount everything.  
+UMOUNT_CMD="umount -lf ${CHROOT_DIR}/sys ${CHROOT_DIR}/dev/pts ${CHROOT_DIR}/proc"
 echo "${UMOUNT_CMD}"
 /bin/bash -c "${UMOUNT_CMD}" || true
 
