c8ca51809d1bfb3986c0900bc751387509aa6308
limelime <limelime@users.noreply.github.com>
2016-01-07 11:20:05 -0500

    Parameterize chroot directory.

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
+# Description: Update installation scripts.
 
 
-# Build live system.
-LIVE_SYSTEM_DIR=chroot
+CHROOT_DIR=$1
+
+# Error Handling
+  CMD_EXAMPLES=$(printf "%s\n%s\n" \
+                        "  e.g. $0 <CHROOT_DIR>"\
+                        "  e.g. $0 /path/to/chroot/"\
+                )
+  if [ ! -d "${CHROOT_DIR}" ]; then
+    echo "Error: Chroot directory: ${CHROOT_DIR}: no such directory. Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
+
 
 # Copy all scripts to live system.
-yes | cp -R scripts ${LIVE_SYSTEM_DIR}/root
+yes | cp -vR ./scripts "${CHROOT_DIR}/root"
