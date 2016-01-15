5dc89312563281e4496aa75eba050d745ad00f9d
limelime <limelime@users.noreply.github.com>
2016-01-15 09:51:51 -0500

    Add curl.

---
+++ scripts/repository/inst-min-con-xtra-curl/inst-min-con-xtra-curl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# Install curl.
+apt-get -y --force-yes install curl
+
+
+# Log
+echo "${GV_LOG} * Install curl."
