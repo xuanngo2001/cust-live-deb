f497aa52528501aa465897041ccde751a7a1a29a
limelime <limelime@users.noreply.github.com>
2016-01-15 09:52:33 -0500

    Add free variant fonts of Times, Arial and Courier.

---
+++ scripts/repository/inst-std-font-fonts-liberation/inst-std-font-fonts-liberation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# Install fonts-liberation.
+apt-get -y --force-yes install fonts-liberation
+
+
+# Log
+echo "${GV_LOG} * Install fonts-liberation to get free variants of the Times, Arial and Courier fonts."
+echo "${GV_LOG} * Otherwise, install ttf-mscorefonts-installer to get Microsoft TrueType core fonts."
