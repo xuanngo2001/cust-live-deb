251975d239aa5a6386d22d636076f860137cbff8
limelime <limelime@users.noreply.github.com>
2016-01-21 17:15:38 -0500

    Debtree was rejected. Hence, dependency-graph.sh is no longer valid. Fix #77.

--- scripts/repository/inst-min-con-cld-tools/tools/cld-dependency-graph.sh
+++
@@ -1,6 +0,0 @@
-PACKAGE=$1
-DEPTH=$2
-
-debtree --no-recommends --no-conflicts ${PACKAGE} > ${PACKAGE}.dot
-dot -Tpng -o ${PACKAGE}.png ${PACKAGE}.dot
-echo "${PACKAGE}.dot and ${PACKAGE}.png created."
