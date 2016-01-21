2fd11668a0b70dd26c8143fa2e76523d68703014
limelime <limelime@users.noreply.github.com>
2016-01-21 15:22:29 -0500

    Dynamically add pinta in menu.

--- scripts/repository/inst-std-graphic-pinta/inst-std-graphic-pinta.sh
+++ scripts/repository/inst-std-graphic-pinta/inst-std-graphic-pinta.sh
@@ -6,7 +6,12 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install Image Editor: Pinta.
 apt-get -y --force-yes install pinta
+
+# Insert Pinta menu in Graphics
+sed -i "/Graphics\">/ r jwmrc-menus-graphics-pinta.xml" "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Log
 echo "${GV_LOG} * Install Image Editor: Pinta."
+echo "${GV_LOG} * Insert Pinta menu in Graphics."
