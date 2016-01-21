f3cf597daeffdfd2c3edc501653c7a1586b028fc
limelime <limelime@users.noreply.github.com>
2016-01-21 17:29:04 -0500

    Add error handling.

--- cld-create-manuals.sh
+++ cld-create-manuals.sh
@@ -5,7 +5,7 @@ set -e
 WIKI_DIR=./scripts/settings/doc/cust-live-deb.wiki/
 
 # Error handling
-  if [ ! -d "${WIKI_DIR}" ]; then echo "Error: Log directory: ${WIKI_DIR}: no such directory. Aborted!"; exit 1; fi;
+  if [ ! -d "${WIKI_DIR}" ]; then echo "Error: Wiki directory: ${WIKI_DIR}: no such directory. Aborted!"; exit 1; fi;
 
 WIKI_DIR=$(readlink -ev "${WIKI_DIR}")
 
