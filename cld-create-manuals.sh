b98bed6311ef63011b1aa7885b7330c022a0a384
limelime <limelime@users.noreply.github.com>
2016-01-21 17:24:53 -0500

    Add error handling.

--- cld-create-manuals.sh
+++ cld-create-manuals.sh
@@ -3,4 +3,10 @@ set -e
 # Description: Generate new version of manuals.
 
 WIKI_DIR=./scripts/settings/doc/cust-live-deb.wiki/
+
+# Error handling
+  if [ ! -d "${WIKI_DIR}" ]; then echo "Error: Log directory: ${WIKI_DIR}: no such directory. Aborted!"; exit 1; fi;
+
+WIKI_DIR=$(readlink -ev "${WIKI_DIR}")
+
 ( cd ${WIKI_DIR}; ./generate-manual.sh )
