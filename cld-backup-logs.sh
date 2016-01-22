9bb262748599ff8ea89f0ccb718fe1edb67fdb3a
limelime <limelime@users.noreply.github.com>
2016-01-22 10:48:41 -0500

    Move most of script file logs to its own directory. #115.

--- cld-backup-logs.sh
+++ cld-backup-logs.sh
@@ -3,22 +3,23 @@ set -e
 # Description: Backup logs generated.
 
 LOG_DIR=$1
-BASE_SCRIPT_DIR=$2
-BASE_SCRIPT_DIR=./scripts
+DEST_DIR=$2
 
 # Error handling
   if [ ! -d "${LOG_DIR}" ]; then echo "Error: Log directory: ${LOG_DIR}: no such directory. Aborted!"; exit 1; fi;
-  if [ ! -d "${BASE_SCRIPT_DIR}" ]; then echo "Error: Destination directory: ${BASE_SCRIPT_DIR}: no such directory. Aborted!"; exit 1; fi;
+  if [ ! -d "${DEST_DIR}" ]; then echo "Error: Destination directory: ${DEST_DIR}: no such directory. Aborted!"; exit 1; fi;
 
 LOG_DIR=$(readlink -ev "${LOG_DIR}")
-BASE_SCRIPT_DIR=$(readlink -ev "${BASE_SCRIPT_DIR}")
+DEST_DIR=$(readlink -ev "${DEST_DIR}")
 
+BASE_SCRIPT_DIR=./scripts
+BASE_SCRIPT_DIR=$(readlink -ev "${BASE_SCRIPT_DIR}")
 REPO_DIR="${BASE_SCRIPT_DIR}/repository"
 REPO_DIR=$(readlink -ev "${REPO_DIR}")
 
 # Processing log of scripts.
 while IFS='' read -r LOG_FILE || [[ -n "$LOG_FILE" ]]; do
-echo $LOG_FILE
+
   SCRIPT_DIR_NAME=$(basename "${LOG_FILE}" | sed 's/.sh.log.md//' | sed 's/.sh.log//' )
   SCRIPT_DIR="${REPO_DIR}/${SCRIPT_DIR_NAME}"
   if [ -d "${SCRIPT_DIR}" ]; then
@@ -28,11 +29,11 @@ echo $LOG_FILE
     if [ -f "${SCRIPT_DIR}/${LOG_FILENAME}" ]; then
       
       if ! cmp "${LOG_FILE}" "${SCRIPT_DIR}/${LOG_FILENAME}"; then
-        yes | cp -a "${LOG_FILE}" "${SCRIPT_DIR}"
+        yes | mv "${LOG_FILE}" "${SCRIPT_DIR}"
       fi
       
     else
-      cp -a "${LOG_FILE}" "${SCRIPT_DIR}"
+      mv "${LOG_FILE}" "${SCRIPT_DIR}"
     fi
     
     
@@ -45,3 +46,5 @@ done < <( find "${LOG_DIR}" -type f -name "inst-*.sh*" )
 
 # Processing install.log
 # Processing dpkg.log
+
+yes | cp -R "${LOG_DIR}" "${DEST_DIR}"
