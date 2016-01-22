3b4019f2c6d72a5649412ce3507a9db51de3ab2c
limelime <limelime@users.noreply.github.com>
2016-01-22 15:11:11 -0500

    Remove Help manuals in text format before adding the HTML format.

--- scripts/repository/inst-std-help-manuals-html/inst-std-help-manuals-html.sh
+++ scripts/repository/inst-std-help-manuals-html/inst-std-help-manuals-html.sh
@@ -9,6 +9,8 @@ set -o pipefail
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Remove Help manuals in text format before adding the HTML format.
+sed -i '/<Menu label="Help">/ , /<\/Menu>/{//!d}' "${GV_CLD_ROOT_DIR}/jwmrc-menus.xml"
 
 # Copy all documentations to /root/doc.
 DOC_DIR=/root/cld-doc
