d8b7855547530c37d436d593b19f6ecfd218a6fd
limelime <limelime@users.noreply.github.com>
2016-02-05 15:14:37 -0500

    Add install drush using phar binary file.

---
+++ scripts/repository/inst-std-xtra-opw-drush8-phar/inst-std-xtra-opw-drush8-phar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# Install Drush using phar binary
+DRUSH_PHAR=drush.phar-8.0.3
+chmod +x ${DRUSH_PHAR}
+
+LOCAL_BIN=/usr/local/bin
+yes | cp -av "${DRUSH_PHAR}" ${LOCAL_BIN}/drush 
+ 
+
+# Log
+echo "${GV_LOG} * Install $(drush --version | tr -s ' ') to manage Drupal."
+echo "${GV_LOG} * List of commands at http://drushcommands.com/."
+
+# Reference:
+#   -Download at https://github.com/drush-ops/drush/releases
+#   -https://pantheon.io/blog/easily-install-drush-phar
+
---
+++ scripts/repository/inst-std-xtra-opw-drush8-phar/to-dl.sh
@@ -0,0 +1,2 @@
+# Manually download phar file from https://github.com/drush-ops/drush/releases
+# chmod +x drush.phar
