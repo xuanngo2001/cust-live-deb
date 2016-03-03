fdf9e7f8cc647d384ec9f4a05ba38ae6844a8509
limelime <limelime@users.noreply.github.com>
2016-03-03 15:35:20 -0500

    rejected.

---
+++ scripts/rejected/zinst-xtra-imdb-python-imdbpy/inst-xtra-imdb-python-imdbpy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+# ${GV_SOURCES_LIST}: APT sources list file
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install IMDbPY.
+apt-get -y --force-yes install python-imdbpy
+
+
+# Log
+echo "${GV_LOG} * Install IMDbPY."
+
+#   * Running inst-xtra-imdb-python-imdbpy.sh ...
+#     * After this operation, 990 kB of additional disk space will be used.
+#     * Install IMDbPY.
+#     * Disk size = 4163692K. Space Used = 2152K.
