cc93a0294cc550342832e2f897d20401d92b7e25
limelime <limelime@users.noreply.github.com>
2016-03-07 10:37:38 -0500

    Add jwmrc-startup-configure-conky.sh to dynamically configure conkyrc before running conky.

--- scripts/repository/inst-std-system-conky/jwmrc-startup-conky.sh
+++ scripts/repository/inst-std-system-conky/jwmrc-startup-conky.sh
@@ -1,5 +1,8 @@
 
 
-# Run system monitory conky on JWM startup.
+# Dynamically configure conkyrc before running conky.
+/root/cld/jwmrc-startup-configure-conky.sh &
+
+# Run system monitor conky on JWM startup.
 /usr/bin/conky &
 
