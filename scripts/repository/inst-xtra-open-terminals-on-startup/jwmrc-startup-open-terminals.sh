40a2a3abda05ce64f57c54caabc3e85e0c55f36c
limelime <limelime@users.noreply.github.com>
2016-03-08 10:01:14 -0500

    Add comment.

--- scripts/repository/inst-xtra-open-terminals-on-startup/jwmrc-startup-open-terminals.sh
+++ scripts/repository/inst-xtra-open-terminals-on-startup/jwmrc-startup-open-terminals.sh
@@ -1,5 +1,6 @@
 
 
 # Open 2 terminals on JWM startup.
+#   Note: open-terminals.sh needs terminal title to be "open-2-terminals".
 nohup x-terminal-emulator -T "open-2-terminals" -e /root/cld/open-terminals.sh
 
