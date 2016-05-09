1983fe7aa7d35b03450f678b6959b55fe622713a
limelime <limelime@users.noreply.github.com>
2016-05-09 16:20:09 -0400

    Add live links to security and update repositories.

--- scripts/repository/inst-zclean-00-apt-get-distro-upgrade/inst-zclean-00-apt-get-distro-upgrade.sh
+++ scripts/repository/inst-zclean-00-apt-get-distro-upgrade/inst-zclean-00-apt-get-distro-upgrade.sh
@@ -9,6 +9,9 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
+GF_ADD_SOURCE_LIST live "deb http://security.debian.org/ jessie/updates main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free"
+
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-contrib jessie-updates contrib"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-main jessie-updates main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-non-free jessie-updates non-free"
