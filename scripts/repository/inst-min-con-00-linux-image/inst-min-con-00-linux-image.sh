8468c4dcc617dbb21d40353ec6fdf5a65c1b30f3
limelime <limelime@users.noreply.github.com>
2016-05-13 10:18:40 -0400

    Fix mkinitramfs command to create initrd.img-* for a specific kernel.

--- scripts/repository/inst-min-con-00-linux-image/inst-min-con-00-linux-image.sh
+++ scripts/repository/inst-min-con-00-linux-image/inst-min-con-00-linux-image.sh
@@ -16,10 +16,19 @@ dbus-uuidgen > /var/lib/dbus/machine-id
 apt-get -y --force-yes install linux-image-amd64 live-boot
 
 
-# Manually create /boot/initrd.img-*
-#   Exclude this if current version is 3.16.0-4-amd64 and new version is 4.5.0-0.bpo.1-amd64.
-#mkinitramfs -d /etc/initramfs-tools -o /boot/initrd.img-$(uname -r)  -r /
+# Get _installed_ kernel version.
+KERNEL_VERSION=$(dpkg-query -W -f='${binary:Package}\n' linux-image-* | head -n 1 | sed 's/linux-image-//')
+
+
+# Manually create /boot/initrd.img-* for a specific kernel.
+mkinitramfs -d /etc/initramfs-tools -o /boot/initrd.img-${KERNEL_VERSION} ${KERNEL_VERSION} -r /
 
 echo "${GV_LOG} * Generate dbus-uuidgen to /var/lib/dbus/machine-id."
-echo "${GV_LOG} * Install Linux image: $(uname -r)."
+echo "${GV_LOG} * Install Linux image: ${KERNEL_VERSION}."
 
+# Note: Don't use $(uname -r). It returns the host kernel version.
+# CLDS: * mkinitramfs -d /etc/initramfs-tools -o /boot/initrd.img-4.5.0-0.bpo.1-amd64  -r /
+# WARNING: missing /lib/modules/3.16.0-4-amd64
+# Ensure all necessary drivers are built into the linux image!
+# depmod: ERROR: could not open directory /lib/modules/3.16.0-4-amd64: No such file or directory
+# depmod: FATAL: could not search modules: No such file or directory
