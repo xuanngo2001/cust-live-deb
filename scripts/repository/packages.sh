6130c46af7266f5795f5ef6c43e735c9b132bdd0
limelime <limelime@users.noreply.github.com>
2016-06-07 11:42:45 -0400

    Improve apt-get filtering.

--- scripts/repository/packages.sh
+++ scripts/repository/packages.sh
@@ -10,10 +10,13 @@ while IFS='' read -r SCRIPT_FILE || [[ -n "$SCRIPT_FILE" ]]; do
   echo "# ${SCRIPT_FILE}" >> "${PACKAGE_LIST_FILE}"
   
   # Extract package names from apt-get
-  APT_GET_PACKAGES=$(grep '^ *apt-get .* install ' "${SCRIPT_FILE}" || true) # If apt-get not found, still return true to prevent exiting this script.
-  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | sed 's/.* install //' | tr ' ' '\n' )
+  APT_GET_PACKAGES=$(grep '.*apt-get.*install' "${SCRIPT_FILE}" || true) # If apt-get not found, still return true to prevent exiting this script.
+  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | grep -v '^#' || true) # Remove if it is commented out.
+  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | sed 's/.* install //' | tr ' ' '\n' || true) # Replace space with newline.
+  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | grep -v '^-' || true)  # Remove option(-XX)
+  APT_GET_PACKAGES=$(echo "${APT_GET_PACKAGES}" | awk NF || true)  # Remove empty line.
   echo "${APT_GET_PACKAGES}" >> "${PACKAGE_LIST_FILE}"
- 
+
 done < <( find ./inst-* -type f -name "inst-*.sh" | sort )
 
 
