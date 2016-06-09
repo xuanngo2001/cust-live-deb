d744951940091ca4ac2027312c70c2d3537688ed
limelime <limelime@users.noreply.github.com>
2016-06-09 09:01:09 -0400

    Add key.

--- scripts/repository/inst-xtra-cld-dev-tools-aptly/inst-xtra-cld-dev-tools-aptly.sh
+++ scripts/repository/inst-xtra-cld-dev-tools-aptly/inst-xtra-cld-dev-tools-aptly.sh
@@ -10,12 +10,18 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Add nightly build repository.
 GF_ADD_SOURCE_LIST live "deb http://repo.aptly.info/ nightly main"
 
-# Install aptly
-gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 9E3E53F19C7DE460
+# Add repository key.
+apt-key add pubkey.txt
 apt-get update
+
+# Install aptly
 apt-get -y --force-yes install xz-utils aptly
 
 # Log
 echo "${GV_LOG} * Add aptly nightly build repository."
 echo "${GV_LOG} * Install $(aptly version) to create local debian repository."
 echo "${GV_LOG} * Add xz-utils package because aptly snapshot merge needs it." 
+
+
+# Get key
+#   -wget -q https://www.aptly.info/pubkey.txt
