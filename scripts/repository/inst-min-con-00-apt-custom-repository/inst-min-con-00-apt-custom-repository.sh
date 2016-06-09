582514f09e16323733d852cb8feee4adbc8d694f
limelime <limelime@users.noreply.github.com>
2016-06-09 12:42:25 -0400

    Add all backport component URLs.

--- scripts/repository/inst-min-con-00-apt-custom-repository/inst-min-con-00-apt-custom-repository.sh
+++ scripts/repository/inst-min-con-00-apt-custom-repository/inst-min-con-00-apt-custom-repository.sh
@@ -16,14 +16,27 @@ apt-key add aptly-master-key.pub
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/aptly-nightly-main nightly main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/deb-multimedia-main jessie main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/deb-multimedia-non-free jessie non-free"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-backports-contrib jessie-backports contrib"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-backports-main jessie-backports main"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-backports-non-free jessie-backports non-free"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-contrib jessie contrib"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-main jessie main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-non-free jessie non-free"
-GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/local jessie main"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-contrib jessie-updates contrib"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-main jessie-updates main"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-security-non-free jessie-updates non-free"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-updates-contrib jessie-updates contrib"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-updates-main jessie-updates main"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/jessie-updates-non-free jessie-updates non-free"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/virtualbox-contrib jessie contrib"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/zfsonlinux-main jessie main"
 
+
+
+# Add default sources list
+GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian jessie main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://security.debian.org/ jessie/updates main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free"
 
 # Update source.list
 apt-get update
