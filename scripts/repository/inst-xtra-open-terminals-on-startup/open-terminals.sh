912205468913cbe62a8e3f23ba78d4a47d3d2f41
limelime <limelime@users.noreply.github.com>
2016-06-27 14:46:39 -0400

    Be more verbose.

--- scripts/repository/inst-xtra-open-terminals-on-startup/open-terminals.sh
+++ scripts/repository/inst-xtra-open-terminals-on-startup/open-terminals.sh
@@ -11,7 +11,7 @@ set -e
 
 
 # Before doing anything, wait for Conky process to start.
-timeout 5s /bin/bash -c "while ! pgrep conky; do sleep 1s; done; sleep 1s"
+timeout 5s /bin/bash -c "while ! pgrep conky; do sleep 1s; echo 'Wait for Conky to run: 1s.'; done; sleep 1s"
 
 # Abort if multiple conky instances exist.
 CONKY_INSTANCES=$(wmctrl -lG | grep -i Conky | wc -l)
