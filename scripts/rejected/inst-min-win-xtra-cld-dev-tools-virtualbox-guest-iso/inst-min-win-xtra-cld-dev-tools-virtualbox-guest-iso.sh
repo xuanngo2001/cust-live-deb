7691122090a11f11b46d0f5d075bcb3d2967807e
limelime <limelime@users.noreply.github.com>
2016-06-30 09:54:43 -0400

    eod update.

---
+++ scripts/rejected/inst-min-win-xtra-cld-dev-tools-virtualbox-guest-iso/inst-min-win-xtra-cld-dev-tools-virtualbox-guest-iso.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly download dependent packages.
+apt-get -d -y --force-yes install linux-headers-amd64
+
+# Install required packages to compile VirtualBox guest additions.
+KERNEL_VERSION=$(dpkg-query -W -f='${binary:Package}\n' linux-image-* | head -n 1 | sed 's/linux-image-//')
+PKGS_LIST="dkms libc6-dev linux-headers-${KERNEL_VERSION} build-essential module-assistant"
+apt-get -y --force-yes install ${PKGS_LIST}
+m-a -i prepare
+
+
+yes | cp -a VBoxGuestAdditions_5.0.20.iso /root
+yes | cp -a inst-*.sh /root
+
+# Install VBoxGuestAdditions
+VBOXGUEST_ADD_ISO="VBoxGuestAdditions.iso"
+VBOXGUEST_ADD_ISO_SIZE=$(GF_SIZE_OF ${VBOXGUEST_ADD_ISO})
+VBOXGUEST_ADD_MNT_DIR=/tmp/VBoxGuestAdditions
+if [ -f ${VBOXGUEST_ADD_ISO} ]
+then
+  rm -rf ${VBOXGUEST_ADD_MNT_DIR}
+  mkdir ${VBOXGUEST_ADD_MNT_DIR}
+  mount -o loop,ro ${VBOXGUEST_ADD_ISO} ${VBOXGUEST_ADD_MNT_DIR}
+  cd ${VBOXGUEST_ADD_MNT_DIR}
+  ./VBoxLinuxAdditions.run
+  cd -
+  umount ${VBOXGUEST_ADD_MNT_DIR}
+  rm -f ${VBOXGUEST_ADD_ISO}
+else
+  echo "${GV_LOG} * ERROR: ${VBOXGUEST_ADD_ISO} is missing. Get it from virtualbox.org."
+fi
+
+
+# Log
+VBOXGUEST_ADD_VERSION=$(modinfo vboxguest | grep ^version | tr -s ' ')
+if [ -z "${VBOXGUEST_ADD_VERSION}" ]; then
+	VBOXGUEST_ADD_VERSION="ERROR: VirtualBox guest additions installation failed!"
+fi
+echo "${GV_LOG} * Install VirtualBox guest additions to Shared folder/clipboard, auto window scaling, etc."
+echo "${GV_LOG} * Assumed packages installed: bzip2 & Xserver installed."
+echo "${GV_LOG} * Install ${PKGS_LIST// /, } to compile VirtualBox guest additions."
+echo "${GV_LOG} * VirtualBox guest additions installed: ${VBOXGUEST_ADD_VERSION}."
+echo "${GV_LOG} * Delete ${VBOXGUEST_ADD_ISO}[${VBOXGUEST_ADD_ISO_SIZE}K]. Space used could be negative due to this deletion."
+
+
+# Reference:
+# https://forums.virtualbox.org/viewtopic.php?t=15679
+# Tried packages from big to small:
+#	apt-get -y --force-yes install dkms build-essential linux-headers-$(uname -r)
+#	apt-get -y --force-yes install dkms libc6-dev linux-headers-$(uname -r)
---
+++ scripts/rejected/inst-min-win-xtra-cld-dev-tools-virtualbox-guest-iso/inst-min-win-xtra-cld-dev-tools-virtualbox-guest-iso.sh.log.md
@@ -0,0 +1,28 @@
+* >>>>>>>>> Running inst-min-win-xtra-cld-dev-tools-virtualbox-guest-iso.sh ...
+  * After this operation, 102 MB of additional disk space will be used.
+  * After this operation, 217 MB of additional disk space will be used.
+  * After this operation, 61.7 MB of additional disk space will be used.
+  * Install VirtualBox guest additions to Shared folder/clipboard, auto window scaling, etc.
+  * Assumed packages installed: bzip2 & Xserver installed.
+  * Install dkms, libc6-dev, linux-headers-4.6.0-0.bpo.1-amd64, build-essential, module-assistant to compile VirtualBox guest additions.
+  * VirtualBox guest additions installed: version: 5.0.20 r106931.
+  * Delete VBoxGuestAdditions.iso[56796K]. Space used could be negative due to this deletion.
+  * Disk size = 2642388K. Space Used = 417944K.
+  * The following NEW packages will be installed:
+  ```bash
+  binutils cpp-4.9 gcc-4.9 libasan1 libatomic1 libcilkrts5 libcloog-isl4
+  libgcc-4.9-dev libgomp1 libisl10 libitm1 liblsan0 libmpc3 libmpfr4
+  libquadmath0 libtsan0 libubsan0 linux-compiler-gcc-4.9-x86
+  linux-headers-4.6.0-0.bpo.1-amd64 linux-headers-4.6.0-0.bpo.1-common
+  linux-headers-amd64 linux-kbuild-4.6
+  binutils build-essential cpp cpp-4.9 dkms dpkg-dev g++ g++-4.9 gcc gcc-4.9
+  guile-2.0-libs libasan1 libatomic1 libc-dev-bin libc6-dev libcilkrts5
+  libcloog-isl4 libdpkg-perl libgc1c2 libgcc-4.9-dev libgomp1 libisl10 libitm1
+  liblsan0 libltdl7 libmpc3 libmpfr4 libquadmath0 libstdc++-4.9-dev
+  libtimedate-perl libtsan0 libubsan0 libunistring0 linux-compiler-gcc-4.9-x86
+  linux-headers-4.6.0-0.bpo.1-amd64 linux-headers-4.6.0-0.bpo.1-common
+  linux-kbuild-4.6 linux-libc-dev make-guile module-assistant patch perl
+  perl-modules
+  cpp-4.8 gcc-4.8 libasan0 libgcc-4.8-dev linux-compiler-gcc-4.8-x86
+  linux-headers-3.16.0-4-amd64 linux-headers-3.16.0-4-common linux-kbuild-3.16
+  ```
