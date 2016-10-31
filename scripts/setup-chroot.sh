38bd1f5a5a3f0979a87a660388bd6bf4af647491
limelime <limelime@users.noreply.github.com>
2016-10-31 09:48:27 -0400

    Add more info about mount.

--- scripts/setup-chroot.sh
+++ scripts/setup-chroot.sh
@@ -2,6 +2,7 @@
 set -e
 # Description: Setup chroot environment.
 #   -Mount /proc and devices.
+#   -More info: http://shallowsky.com/blog/tags/chroot/
 
 mount none -t proc /proc
 mount none -t sysfs /sys
