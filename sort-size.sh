dfbd4112b2663121b590365486488eb25ad0ebf5
limelime <limelime@users.noreply.github.com>
2016-11-17 13:01:05 -0500

    Add comment and echo display format.

--- sort-size.sh
+++ sort-size.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 # Description: List script size used in ascending order.
+# Output format are dependent of the ACTION
 
 # Ensure install-size-history.data is up-to-date.
 ./sc-reformat-log.sh
@@ -14,12 +15,15 @@ SCRIPT_LOG_PARSABLE=install-size-history.data
 ACTION=$(echo "${ACTION}" | tr '[:upper:]' '[:lower:]')  # Lowercase to avoid case typo.
 case "${ACTION}" in
 
+  # Display package(script name) space used size in kilobytes.
   package)
     cut -d ';' -f 2,5 "${SCRIPT_LOG_PARSABLE}" | sort -u -t ';' -k2,2n | grep -v 'install\.sh'
+    echo "<script-name>; <space-used-kb>"
     ;;
   
   *)
     sort -t ';' -k5,5n "${SCRIPT_LOG_PARSABLE}"
-    exit 1
+    echo "<system>: <script-name>; <date-time>; <disk-size-kb>; <space-used-kb>"
     ;;
 esac
+
