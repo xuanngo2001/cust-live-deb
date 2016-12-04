6503fb2cfdc1c68487fadbdfef7e559f3ba7edac
limelime <limelime@users.noreply.github.com>
2016-12-03 22:43:49 -0500

    Update cld-dpkg-list.sh

--- scripts/repository/inst-min-con-cld-tools/tools/cld-dpkg-list.sh
+++ scripts/repository/inst-min-con-cld-tools/tools/cld-dpkg-list.sh
@@ -3,17 +3,16 @@ set -e
 # Description: List Debian packages with its version number.
 #     Return values can be feed to packages.list
 
-ADD_VERSION="$1"
+VERSION="$1"
 
 
 # Error handling.
 
-
 # Main
-DATE_STRING=$(date +"%Y-%m-%d_%0k.%M.%S")
-LOG_FILE="${LOG_DIR}/dpkg_${DATE_STRING}.log"
-if [ "${ADD_VERSION}" = "false" ]; then
+if [ -z "${VERSION}"]; then
+  dpkg-query -W -f='${binary:Package} (=${Version})\n'
+else if [ "${VERSION}" = "no-version" ]; then
   dpkg-query -W -f='${binary:Package}\n'  
 else
-  dpkg-query -W -f='${binary:Package} (=${Version})\n'
+  echo "$0 [,no-version]"
 fi
