440540aff8a43fcfa89a6e7b268543f03c698a2f
limelime <limelime@users.noreply.github.com>
2016-12-07 13:38:39 -0500

    Format results.

--- qcheck-show-removed-packages.sh
+++ qcheck-show-removed-packages.sh
@@ -4,11 +4,20 @@
 MAIN_LOG=/media/master/github/cust-live-deb/logs/main.log
 
   REMOVED_PACKAGES_MARKER="The following packages will be REMOVED:"
-  PKG_REMOVED=$(cat "${MAIN_LOG}" | sed -n "/${REMOVED_PACKAGES_MARKER}/,/^[^ *]/p" | grep '^  ') # Get package list.
+  PKG_REMOVED=$(cat "${MAIN_LOG}" | sed -n "/${REMOVED_PACKAGES_MARKER}/,/^[^ *]/p" | grep '^  ')
+  PKG_REMOVED=$(echo "${PKG_REMOVED}" | tr ' ' '\n')  # 1 package per line.
+  PKG_REMOVED=$(echo "${PKG_REMOVED}" | awk NF)       # Remove empty line.
+  PKG_REMOVED=$(echo "${PKG_REMOVED}" | sed 's/^/   /') # Indent results.
 
-
+  # Add newline as a separation.
+  echo ""
+  
   if [ $(echo "${PKG_REMOVED}"| tr -d ' '| wc -w) -gt 0 ]; then
+    echo "${REMOVED_PACKAGES_MARKER}"
     echo "${PKG_REMOVED}"
   else
     echo "No package has been removed!"
   fi
+  
+  # Add newline as a separation.
+  echo ""  
