d1cfa55c5b3c353e859b56062c1bf631928b723f
limelime <limelime@users.noreply.github.com>
2016-12-15 10:34:57 -0500

    Add requirement.

--- scripts/repository/inst-std-internet-xtra-google-chrome/inst-std-internet-xtra-google-chrome.sh
+++ scripts/repository/inst-std-internet-xtra-google-chrome/inst-std-internet-xtra-google-chrome.sh
@@ -28,7 +28,7 @@ if [ -z "${GOOGLE_CHROME_VERSION}" ]; then
   GOOGLE_CHROME_VERSION="ERROR: google-chrome installation failed!"
 fi
 
-# Run Google Chrome as root.
+# Run Google Chrome as root. Consider: x-www-browser. 
 GOOGLE_CHROME_BIN=/opt/google/chrome/google-chrome
 sed -i.original "s/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\"$/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\" --no-sandbox/" "${GOOGLE_CHROME_BIN}"
 
