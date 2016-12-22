0fb6127a5ba9143a5d883eec2fec0826c3ec9f76
limelime <limelime@users.noreply.github.com>
2016-12-22 14:51:06 -0500

    Add policies.

---
+++ scripts/repository/inst-std-internet-xtra-google-chrome/policies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+# Description: Pre-configure Google Chrome using policies.
+#   Testing: Run chrome://policy
+# References:
+#   -http://unix.stackexchange.com/questions/110613/how-can-i-make-chrome-stop-asking-to-be-the-default-browser
+#     -List of switches: http://peter.sh/experiments/chromium-command-line-switches/
