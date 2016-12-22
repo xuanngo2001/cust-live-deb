a9199a05a3a8af8ce7f64a3d5ec4869dfc327e10
limelime <limelime@users.noreply.github.com>
2016-12-22 18:50:04 -0500

    Fix formatting. Fix #146.

--- qcheck-jwm-missing-icons.sh
+++ qcheck-jwm-missing-icons.sh
@@ -4,26 +4,25 @@
 REPOSITORY_DIR=./scripts/repository/
 
 
+# Add newline as a separation.
+echo ""
 
-  # Add newline as a separation.
-  echo ""
-
-	while IFS='' read -r FILENAME || [[ -n "${FILENAME}" ]]; do
-	 
-    ICON_CONTENT=$(grep 'icon=' "${FILENAME}" | sed 's/^.* icon=/icon=/' | sed 's/ .*//')
-    ICON_CONTENT=$(echo "${ICON_CONTENT}" | sed 's/icon="//' | sed 's/".*//')
+while IFS='' read -r FILENAME || [[ -n "${FILENAME}" ]]; do
+ 
+  ICON_CONTENT=$(grep 'icon=' "${FILENAME}" | sed 's/^.* icon=/icon=/' | sed 's/ .*//')
+  ICON_CONTENT=$(echo "${ICON_CONTENT}" | sed 's/icon="//' | sed 's/".*//')
+  
+  if [ ! -z "${ICON_CONTENT}" ]; then
+  
+		while IFS='' read -r ICON_PATH || [[ -n "${ICON_PATH}" ]]; do    
+			if [ ! -e "${ICON_PATH}" ]; then
+			  echo "Error: ${ICON_PATH} not found: ${FILENAME}"
+			fi
+		done < <( echo "${ICON_CONTENT}" )
     
-    if [ ! -z "${ICON_CONTENT}" ]; then
-    
-			while IFS='' read -r ICON_PATH || [[ -n "${ICON_PATH}" ]]; do    
-				if [ ! -e "${ICON_PATH}" ]; then
-				  echo "Error: ${ICON_PATH} not found: ${FILENAME}"
-				fi
-			done < <( echo "${ICON_CONTENT}" )
-      
-    fi 
-	 
-	done < <( find "${REPOSITORY_DIR}" -type f -name *.xml )
+  fi 
+ 
+done < <( find "${REPOSITORY_DIR}" -type f -name *.xml )
 
-  # Add newline as a separation.
-  echo ""  
+# Add newline as a separation.
+echo ""  
