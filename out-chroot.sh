2475a54771dc59ec426fb4dc9ff119ae2d6a3f96
limelime <limelime@users.noreply.github.com>
2016-12-27 12:52:37 -0500

    Add check: root/ directory is missing.

--- out-chroot.sh
+++ out-chroot.sh
@@ -14,7 +14,15 @@ CHROOT_DIR=$1
     echo "${CMD_EXAMPLES}"
     exit 1;
   fi
+
   CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
+  
+  if [ ! -d "${CHROOT_DIR}/root" ]; then
+    echo "Error: root/ directory is missing from ${CHROOT_DIR}: Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  
   
 
 # Unmount everything.  
