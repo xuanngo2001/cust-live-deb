308f3c6a582c7a22fe85d4d33345d24e3eb67f60
limelime <limelime@users.noreply.github.com>
2016-12-28 18:32:46 -0500

    Rename to default-jre.

--- scripts/repository/inst-min-con-xtra-default-jre/inst-min-con-xtra-default-jre.sh
+++ scripts/repository/inst-min-con-xtra-default-jre/inst-min-con-xtra-default-jre.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
-
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
 
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Examples:
+#   apt-get install -d -y --force-yes <your-package>  # Explicitly download dependent packages 
+
+# Install default-jre.
 apt-get -y --force-yes install default-jre
+
 
 # Log
 echo "${GV_LOG} * Install JAVA JRE: default-jre."
