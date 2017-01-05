724576035c12b89f08f1ccb44133029be90decf8
limelime <limelime@users.noreply.github.com>
2017-01-05 09:36:39 -0500

    Use new rar version due to segmentation fault with 'RAR 4.20' with linux-image-4.8.0-0.bpo.2-amd64-unsigned (4.8.11-1~bpo8+1)

--- scripts/repository/inst-min-con-compressors/inst-min-con-compressors.sh
+++ scripts/repository/inst-min-con-compressors/inst-min-con-compressors.sh
@@ -13,9 +13,13 @@ apt-get -y --force-yes install zip
 apt-get -y --force-yes install unzip
 apt-get -y --force-yes install p7zip
 
+# Use new rar version due to segmentation fault with 'RAR 4.20' with linux-image-4.8.0-0.bpo.2-amd64-unsigned (4.8.11-1~bpo8+1) 
+yes | cp -a ./rarlinux-x64-5.4.0/rar /usr/bin
+
 # Log
 echo "${GV_LOG} * Install de/compressor: bzip2."
 echo "${GV_LOG} * Install de/compressor: rar."
+echo "${GV_LOG} * Install de/compressor: unrar."
 echo "${GV_LOG} * Install de/compressor: unzip."
 echo "${GV_LOG} * Install de/compressor: zip."
 echo "${GV_LOG} * Install de/compressor: p7zip."
