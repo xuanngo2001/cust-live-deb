7aacc4b9c7cb60b5d3107c184ecf864516dc2af5
limelime <limelime@users.noreply.github.com>
2017-01-10 15:09:04 -0500

    Narrow field width.

--- scripts/repository/inst-min-con-cld-tools/tools/if-usage-sum.sh
+++ scripts/repository/inst-min-con-cld-tools/tools/if-usage-sum.sh
@@ -9,11 +9,11 @@ BYTES_RECEIVED=$(echo "${CONTENT}" | cut -d'|' -f3)
 BYTES_TRANSMITTED=$(echo "${CONTENT}" | cut -d'|' -f4)
 TOTAL_BYTES_RECEIVED=$(awk '{sum+=$1} END {printf "%.0f", sum}' <(echo "${BYTES_RECEIVED}"))
 TOTAL_BYTES_TRANSMITTED=$(awk '{sum+=$1} END {printf "%.0f", sum}' <(echo "${BYTES_TRANSMITTED}"))
-TOTAL_RECEIVED_STR=$(numfmt --to=iec-i --suffix=B --format="%5f" "${TOTAL_BYTES_RECEIVED}")
-TOTAL_TRANSMITTED_STR=$(numfmt --to=iec-i --suffix=B --format="%5f" "${TOTAL_BYTES_TRANSMITTED}")
+TOTAL_RECEIVED_STR=$(numfmt --to=iec-i --suffix=B --format="%7f" "${TOTAL_BYTES_RECEIVED}")
+TOTAL_TRANSMITTED_STR=$(numfmt --to=iec-i --suffix=B --format="%7f" "${TOTAL_BYTES_TRANSMITTED}")
 
 echo "${CONTENT}"
 echo ""
-printf "%20s: %5s \n" "Total downloaded" "${TOTAL_RECEIVED_STR}"
-printf "%20s: %5s\n"  "Total uploaded"   "${TOTAL_TRANSMITTED_STR}"
+printf "%20s: %7s \n" "Total downloaded" "${TOTAL_RECEIVED_STR}"
+printf "%20s: %7s\n"  "Total uploaded"   "${TOTAL_TRANSMITTED_STR}"
 echo ""
