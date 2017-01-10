2f750edb5c88efaca00d6358162799ec50aeef2f
limelime <limelime@users.noreply.github.com>
2017-01-09 19:03:07 -0500

    Use UTC time.

--- scripts/repository/inst-min-con-systemd-hooks/systemd/cld-systemd-before-shutdown-halt-reboot.sh
+++ scripts/repository/inst-min-con-systemd-hooks/systemd/cld-systemd-before-shutdown-halt-reboot.sh
@@ -4,5 +4,5 @@ set -e
 # Reference: 
 #   -http://unix.stackexchange.com/a/41756
 #   -https://wiki.archlinux.org/index.php/systemd
-echo "$0: $(date)" >> /root/cld/boot-sequence.txt
+echo "$0: $(date -u)" >> /root/cld/boot-sequence.txt
 exit 0
