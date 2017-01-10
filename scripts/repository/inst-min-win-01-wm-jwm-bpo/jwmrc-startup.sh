c233968c5a88c6232235f0e846448a5dbaec24a1
limelime <limelime@users.noreply.github.com>
2017-01-09 19:07:33 -0500

    Revert back.

--- scripts/repository/inst-min-win-01-wm-jwm-bpo/jwmrc-startup.sh
+++ scripts/repository/inst-min-win-01-wm-jwm-bpo/jwmrc-startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Description: Commands that will run on JWM startup.
-echo "$0: $(date -u)" >> /root/cld/boot-sequence.txt
+echo "$0: $(date)" >> /root/cld/boot-sequence.txt
 ### Insert applications that will run on JWM startup here.
 
 
