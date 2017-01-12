7104abc9c63977a0ea8bed6111a005be62f00e5d
limelime <limelime@users.noreply.github.com>
2017-01-12 14:18:34 -0500

    Add test cases.

--- scripts/repository/inst-std-accessories-terminal-stterm/test-stterm.sh
+++ scripts/repository/inst-std-accessories-terminal-stterm/test-stterm.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 set -e
 # Description: Test ssterm
+# fc-list
+
+#FONT_LINE="Unknown:style=Regular:size=16"
+#stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&
+
+FONT_LINE="Liberation Mono:style=Regular:size=16"
+stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&
+
+#FONT_LINE="Monospace:style=Regular:size=16"
+#stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&
+
+FONT_LINE="Monospace:style=Regular:size=15"
+stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&
+
+FONT_LINE="Monospace:style=Regular:size=14"
+stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&
+
+FONT_LINE="Monospace:style=Regular:size=12"
+stterm -T "${FONT_LINE}" -f "${FONT_LINE}"&  
