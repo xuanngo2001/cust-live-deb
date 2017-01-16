3c812f9739d974e9cb51b0382c6ccf849dc22fb9
limelime <limelime@users.noreply.github.com>
2017-01-16 08:55:09 -0500

    Redesign systemd.

--- scripts/repository/inst-min-con-systemd-hooks/systemd/user/cld-user-after-boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "$0: $(date)" >> /root/cld/boot-sequence.txt
-# Description: After boot, run the following scripts:
-
--- scripts/repository/inst-min-con-systemd-hooks/systemd/user/cld-user-before-shutdown-halt-reboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "$0: $(date)" >> /root/cld/boot-sequence.txt
-# Description: Before reboot/halt/shutdown, run the following scripts:
-
