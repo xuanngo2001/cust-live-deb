107fd3b9fb937062c67b0bc3280abbb896066c41
limelime <limelime@users.noreply.github.com>
2017-01-19 13:00:08 -0500

    Rejected.

--- scripts/repository/inst-std-accessories-terminal-stterm/inst-std-accessories-terminal-stterm.sh
+++ scripts/repository/inst-std-accessories-terminal-stterm/inst-std-accessories-terminal-stterm.sh
@@ -7,18 +7,11 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Examples:
-#   apt-get install -d -y --force-yes <your-package>  # Explicitly download dependent packages 
-
 # Install stterm.
 apt-get -y --force-yes install stterm
 
-# Set stterm as the default emulator
-STTERM_NAME=stter
-chmod +x "${STTERM_NAME}"
-yes | cp -av "${STTERM_NAME}" /usr/local/bin/
-update-alternatives --set x-terminal-emulator /usr/local/bin/${STTERM_NAME}
-
 # Log
 echo "${GV_LOG} * Install stterm."
-echo "${GV_LOG} * Set stter as the default emulator."
+
+# REJECTED:
+#   -Can't use 'Delete' button to delete character in the middle of string.
--- scripts/repository/inst-std-accessories-terminal-stterm/inst-std-accessories-terminal-stterm.sh.log.md
+++ scripts/repository/inst-std-accessories-terminal-stterm/inst-std-accessories-terminal-stterm.sh.log.md
@@ -1,4 +1,3 @@
 * >>>>>>>>> Running inst-std-accessories-terminal-stterm.sh ...
   * Install stterm.
-  * Set stterm as the default emulator.
-  * Disk size = 3204744K. Space Used = 4K.
+  * Disk size = 2970608K. Space Used = 4K.
