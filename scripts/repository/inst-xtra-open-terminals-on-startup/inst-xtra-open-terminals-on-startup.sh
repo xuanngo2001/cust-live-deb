72a1f635e14852adb16342804a522b7edfb83ea9
limelime <limelime@users.noreply.github.com>
2017-01-20 14:06:50 -0500

    Remove xterm.

--- scripts/repository/inst-xtra-open-terminals-on-startup/inst-xtra-open-terminals-on-startup.sh
+++ scripts/repository/inst-xtra-open-terminals-on-startup/inst-xtra-open-terminals-on-startup.sh
@@ -12,7 +12,7 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 apt-get -y --force-yes install x11-xserver-utils
 apt-get -y --force-yes install wmctrl
 apt-get -y --force-yes install x11-utils
-#apt-get -y --force-yes install xterm
+
 
 # Make open-terminals.sh run when JWM start. 
 yes | cp -av open-terminals.sh "${GV_CLD_ROOT_DIR}"
@@ -26,6 +26,5 @@ echo "${GV_CLD_ROOT_DIR}/${JWM_START_SCRIPT}&" >> "${GV_CLD_ROOT_DIR}/jwmrc-star
 echo "${GV_LOG} * Install x11-xserver-utils for xrandr."
 echo "${GV_LOG} * Install wmctrl."
 echo "${GV_LOG} * Install x11-utils for xprop."
-echo "${GV_LOG} * Install xterm for resize."
 echo "${GV_LOG} * Copy open-terminal.sh in ${GV_CLD_ROOT_DIR}."
 echo "${GV_LOG} * Add ${JWM_START_SCRIPT} in ${GV_CLD_ROOT_DIR}/jwmrc-startup.sh."
