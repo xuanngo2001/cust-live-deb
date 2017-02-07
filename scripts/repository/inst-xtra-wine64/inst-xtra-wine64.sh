5fd5c6bb96d2a48df475bb17e9972c1d2d5532fd
limelime <limelime@users.noreply.github.com>
2017-02-07 11:22:14 -0500

    Use backport as repo.

--- scripts/repository/inst-xtra-wine64/inst-xtra-wine64.sh
+++ scripts/repository/inst-xtra-wine64/inst-xtra-wine64.sh
@@ -7,13 +7,19 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Add jessie-backports repository.
+GF_ADD_SOURCE_LIST live "deb http://http.debian.net/debian jessie-backports main contrib non-free"
+
 # Enable multi-architectures
 dpkg --add-architecture i386
 apt update
 
 # Install wine64.
-apt-get -y --force-yes install wine wine-development fonts-wine
-#apt-get -y --force-yes install wine wine32 wine64 libwine libwine:i386 fonts-wine
+#apt-get -y --force-yes install wine wine-development fonts-wine
+apt-get -t jessie-backports -y --force-yes install wine-development fonts-wine
+
 
 # Log
 echo "${GV_LOG} * Install wine64."
+
+# http://www.gamersonlinux.com/forum/threads/diablo-ii-expansion-guide.217/
