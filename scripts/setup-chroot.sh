d7510fbe13d63a973d0c3b17da563f81c5f79992
limelime <limelime@users.noreply.github.com>
2017-02-28 13:33:44 -0500

    removed.

--- scripts/setup-chroot.sh
+++ scripts/setup-chroot.sh
@@ -4,8 +4,5 @@ set -e
 #   -Mount /proc and devices.
 #   -More info: http://shallowsky.com/blog/tags/chroot/
 
-mount none -t proc /proc
-mount none -t sysfs /sys
-mount none -t devpts /dev/pts
 export HOME=/root
 export LC_ALL=C
