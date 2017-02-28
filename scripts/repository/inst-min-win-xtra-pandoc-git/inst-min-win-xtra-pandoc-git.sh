b0a6c072d9ac07428c18549eb4a29cd1557d1b90
limelime <limelime@users.noreply.github.com>
2017-02-28 10:55:43 -0500

    Ensure that dependent packages are downloaded.

--- scripts/repository/inst-min-win-xtra-pandoc-git/inst-min-win-xtra-pandoc-git.sh
+++ scripts/repository/inst-min-win-xtra-pandoc-git/inst-min-win-xtra-pandoc-git.sh
@@ -7,12 +7,15 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Ensure that dependent packages are downloaded.
+  apt-get install -d -y --force-yes pandoc
+
 # Install the latest version from Git
   PANDOC_DEB=$(find . -name 'pandoc*.deb' | sort -r | head -n1 )
   dpkg -i "${PANDOC_DEB}"
 
 # Get the version number.
-PANDOC_VER=$(pandoc --version | head -n1)
+  PANDOC_VER=$(pandoc --version | head -n1)
 
 # Log
 echo "${GV_LOG} * Install ${PANDOC_VER} from git."
