09d7e982425cb1aff44bebeec776aea23d4c4bd6
limelime <limelime@users.noreply.github.com>
2017-03-07 12:29:01 -0500

    Add game user.

---
+++ scripts/repository/inst-xtra-games-users-poweruser/inst-xtra-games-users-poweruser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+
+# Add games user.
+useranme='poweruser'
+adduser --disabled-password --gecos "" "${useranme}"
+(echo "${useranme}";echo "${useranme}") | passwd "${useranme}"
+
+
+
+# Log
+echo "${GV_LOG} * Created user: ${useranme}/${useranme}."
