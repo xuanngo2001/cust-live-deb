ed2c0e6fda39bd7fae99e19d4529a35f8127820f
limelime <limelime@users.noreply.github.com>
2017-03-07 13:02:22 -0500

    #209

--- scripts/repository/inst-min-con-bashrc/inst-min-con-bashrc.sh
+++ scripts/repository/inst-min-con-bashrc/inst-min-con-bashrc.sh
@@ -10,13 +10,13 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Add common bashrc codes.
-cat bashrc-common-* >> /root/.bashrc
+cat bashrc-common-* >> ${HOME}/.bashrc
 
 # Set dircolors
 DIR_COLORS_FILE=dircolors-gnu-ls-colors.txt
-yes | cp -av ${DIR_COLORS_FILE} /root/.dircolors
+yes | cp -av ${DIR_COLORS_FILE} ${HOME}/.dircolors
 
 
 # Log
-echo "${GV_LOG} * Set common settings for /root/.bashrc."
+echo "${GV_LOG} * Set common settings for ${HOME}/.bashrc."
 echo "${GV_LOG} * Set dircolors to use ${DIR_COLORS_FILE} to make ls more beautiful."
