404e78899cbabfcf238f5695b652f361fa04fc27
limelime <limelime@users.noreply.github.com>
2017-03-09 14:06:42 -0500

    Refactor.

---
+++ scripts/repository/inst-min-con-udevil-bpo/cld/bootstrap/devmon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Start devmon on startup so that devices are automounted.
+#  Note: You need ntfs-3g driver to be able to write on ntfs partition.
+SHM_LOG=/usr/local/cld/log/log
+mkdir -p ${SHM_LOG}
+/usr/bin/devmon 2>&1 | tee -a ${SHM_LOG}/devmon.log &
+
