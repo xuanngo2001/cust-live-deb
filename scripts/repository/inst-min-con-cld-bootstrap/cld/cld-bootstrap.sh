2dd0b8235d9244008ae190de4653c39c40f16b07
limelime <limelime@users.noreply.github.com>
2017-03-13 12:24:18 -0400

    Log what are exected.

--- scripts/repository/inst-min-con-cld-bootstrap/cld/cld-bootstrap.sh
+++ scripts/repository/inst-min-con-cld-bootstrap/cld/cld-bootstrap.sh
@@ -2,4 +2,4 @@
 set -e
 # Description: Run all scripts in bootstrap/ Cust-Live-Deb.
 
-find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \;
+find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
--- scripts/repository/inst-min-win-01-wm-jwm-bpo/cld/app/jwm/jwm-bootstrap.sh
+++ scripts/repository/inst-min-win-01-wm-jwm-bpo/cld/app/jwm/jwm-bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 # Description: Commands that will run on JWM startup.
-echo "$0: $(date)" >> /root/cld/boot-sequence.txt
-### Insert applications that will run on JWM startup here.
 
-
-find /usr/local/cld/app/jwm/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \;
+find /usr/local/cld/app/jwm/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
