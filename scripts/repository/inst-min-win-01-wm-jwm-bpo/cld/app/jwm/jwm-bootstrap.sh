09faf8540b6e58c8bd9c986505c61c2cccf87ec7
limelime <limelime@users.noreply.github.com>
2017-03-13 17:01:57 -0400

    Update jwm-bootstrap.sh

--- scripts/repository/inst-min-win-01-wm-jwm-bpo/cld/app/jwm/jwm-bootstrap.sh
+++ scripts/repository/inst-min-win-01-wm-jwm-bpo/cld/app/jwm/jwm-bootstrap.sh
@@ -2,4 +2,4 @@
 set -e
 # Description: Commands that will run on JWM startup.
 
-find /usr/local/cld/app/jwm/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
+find /usr/local/cld/app/jwm/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; 2>&1 | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
