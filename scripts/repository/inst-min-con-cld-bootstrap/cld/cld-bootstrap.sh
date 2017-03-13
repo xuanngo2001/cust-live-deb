ae1ab03d1de76da4b3932d929619b8fc8cffae87
limelime <limelime@users.noreply.github.com>
2017-03-13 17:01:17 -0400

    Update cld-bootstrap.sh

--- scripts/repository/inst-min-con-cld-bootstrap/cld/cld-bootstrap.sh
+++ scripts/repository/inst-min-con-cld-bootstrap/cld/cld-bootstrap.sh
@@ -2,4 +2,4 @@
 set -e
 # Description: Run all scripts in bootstrap/ Cust-Live-Deb.
 
-find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
+find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; 2>&1 | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
