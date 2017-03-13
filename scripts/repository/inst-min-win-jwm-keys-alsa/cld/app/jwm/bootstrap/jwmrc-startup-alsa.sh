ec240aaf2e74fc514321e5e8bee46b393f74f528
limelime <limelime@users.noreply.github.com>
2017-03-13 16:59:06 -0400

    Update jwmrc-startup-alsa.sh

--- scripts/repository/inst-min-win-jwm-keys-alsa/cld/app/jwm/bootstrap/jwmrc-startup-alsa.sh
+++ scripts/repository/inst-min-win-jwm-keys-alsa/cld/app/jwm/bootstrap/jwmrc-startup-alsa.sh
@@ -2,7 +2,7 @@
 set -e
 # Description: Set ALSA controller name. It is not always Master. It could be something else(e.g. Headphone).
 
-
+CLD_JWMRC_DIR=/usr/local/cld/app/jwm/jwmrc
 # Set ALSA controller name.
 ALSA_CONTROL=$(amixer scontrols | head -n 1 | cut -d "'" -f2)
 sed -i "s/Master/${ALSA_CONTROL}/" "${CLD_JWMRC_DIR}/jwmrc-key.xml"
