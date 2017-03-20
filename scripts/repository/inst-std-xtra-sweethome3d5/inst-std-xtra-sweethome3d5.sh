811ee6faab4db865632a7188618cde5760132ab1
limelime <limelime@users.noreply.github.com>
2017-03-20 14:27:09 -0400

    clean up.

--- scripts/repository/inst-std-xtra-sweethome3d5/inst-std-xtra-sweethome3d5.sh
+++ scripts/repository/inst-std-xtra-sweethome3d5/inst-std-xtra-sweethome3d5.sh
@@ -30,8 +30,6 @@ chmod +x ${LOCAL_BIN}/sweethome3d*
 # Insert Sweet Home 3D menu in Graphics
 sed -i "/Graphics\">/ r jwmrc-menus-graphics-sweethome3d.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
-#./jmf.sh
-
 # Log
 echo "${GV_LOG} * Install ${SH3D_JAR}."
 echo "${GV_LOG} * Install ${SH3D_TEXTURE_EDITOR_JAR}."
