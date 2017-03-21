9efea6008285bb4b2e7b3ed566cc393284a3f944
limelime <limelime@users.noreply.github.com>
2017-03-21 13:00:47 -0400

    Use printf.

--- scripts/repository/inst-min-con-cld-bin/cld/bin/cld-dirs-create.sh
+++ scripts/repository/inst-min-con-cld-bin/cld/bin/cld-dirs-create.sh
@@ -12,7 +12,11 @@ case "${action}" in
   jwmrc)
       mkdir -p ./cld/app/jwm/jwmrc
       ;;
-      
+
+  jwm-bootstrap)
+      mkdir -p ./cld/app/jwm/bootstrap
+      ;;
+                  
   bashrc)
       mkdir -p ./cld/bashrc.d
       ;;
@@ -20,10 +24,20 @@ case "${action}" in
   after-boot)
       mkdir -p ./cld/systemd/runlevel/after-boot
       ;;
-
+      
+  before-shutdown)
+      mkdir -p ./cld/systemd/runlevel/before-shutdown
+      ;;
+      
    *)
       echo "Error: Action: ${action} is unknown. Aborted!"
-      exit 1;
+      printf "    %-20s %s\n" "bin"               "=> ./cld/bin"
+      printf "    %-20s %s\n" "jwmrc"             "=> ./cld/app/jwm/jwmrc"
+      printf "    %-20s %s\n" "jwm-bootstrap"     "=> ./cld/app/jwm/bootstrap"
+      printf "    %-20s %s\n" "bashrc"            "=> ./cld/bashrc.d"
+      printf "    %-20s %s\n" "after-boot"        "=> ./cld/systemd/runlevel/after-boot"
+      printf "    %-20s %s\n" "before-shutdown"   "=> ./cld/systemd/runlevel/before-shutdown"
+            exit 1;
       ;;
 esac
 
