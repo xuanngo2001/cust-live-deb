65532d198ae847536e37e7429f6dd572480dea62
limelime <limelime@users.noreply.github.com>
2017-03-23 10:20:27 -0400

    No need to manually copy volumeicon config file.

--- scripts/repository/inst-min-win-systray-volumeicon-alsa/cld/app/jwm/bootstrap/jwmrc-startup-volumeicon.sh
+++ scripts/repository/inst-min-win-systray-volumeicon-alsa/cld/app/jwm/bootstrap/jwmrc-startup-volumeicon.sh
@@ -1,8 +1,3 @@
-# Set left click to display volume slider: lmb_slider=true
-  VOLUME_ICON_CONFIG_DIR=${HOME}/.config/volumeicon/
-  mkdir -p ${VOLUME_ICON_CONFIG_DIR}
-  yes | cp ${CLD_APP_DIR}/volumeicon/config/volumeicon ${VOLUME_ICON_CONFIG_DIR}
-
 # Wait for JWM
   timeout 5s /bin/bash -c "while ! pgrep Xorg; do sleep 1s; done; sleep 1s"
 
