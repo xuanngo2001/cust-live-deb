94388ef987dfe4f79d2b7ef654cd0cb6c4f2fcfc
limelime <limelime@users.noreply.github.com>
2017-03-27 16:59:41 -0400

    Need absolute path. This script can be called when $VARIABLES are not loaded.

--- scripts/repository/inst-min-con-03-systemd-hooks/cld/bin/cld-run-scripts.sh
+++ scripts/repository/inst-min-con-03-systemd-hooks/cld/bin/cld-run-scripts.sh
@@ -2,7 +2,7 @@
 set -e
 # Description: Run all scripts in bootstrap/ Cust-Live-Deb.
 
-#find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; 2>&1 | tee -a "${CLD_LOG_DIR}/boot-sequence.log" "${CLD_LOG_DIR}/$0.log"
+#find /usr/local/cld/bootstrap/ -name '*.sh' -type f -execdir echo "Executing {}" \; -execdir chmod +x {} \; -execdir {} \; 2>&1 | tee -a "${cld_log_dir}/boot-sequence.log" "${cld_log_dir}/$0.log"
 
 script_dir=$1
 
@@ -12,11 +12,12 @@ if [ -z "${script_dir}" ]; then
 fi
 
 script_dir=$(readlink -ev "${script_dir}")
+cld_log_dir=/usr/local/cld/log
 while IFS='' read -r script_file || [[ -n "${script_file}" ]]; do
   
   this_script_name=$(basename $0)
-  chmod +x ${script_file}          2>&1 | tee -a "${CLD_LOG_DIR}/${this_script_name}.log"
-  echo "Executing ${script_file}"  2>&1 | tee -a "${CLD_LOG_DIR}/${this_script_name}.log" "${CLD_LOG_DIR}/boot-sequence.log"
-  (${script_file}                  2>&1 | tee -a "${CLD_LOG_DIR}/${this_script_name}.log")&
+  chmod +x ${script_file}          2>&1 | tee -a "${cld_log_dir}/${this_script_name}.log"
+  echo "Executing ${script_file}"  2>&1 | tee -a "${cld_log_dir}/${this_script_name}.log" "${cld_log_dir}/boot-sequence.log"
+  (${script_file}                  2>&1 | tee -a "${cld_log_dir}/${this_script_name}.log")&
 
 done < <( find "${script_dir}" -type f -name '*.sh')
