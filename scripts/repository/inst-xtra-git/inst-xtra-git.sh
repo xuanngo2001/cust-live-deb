4afbac37570bdee9e6c6e681f648d0678e97bccc
limelime <limelime@users.noreply.github.com>
2017-04-27 15:03:27 -0400

    add git.

---
+++ scripts/repository/inst-xtra-git/inst-xtra-git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Examples:
+#   apt-get install -d -y --force-yes <your-package>  # Explicitly download dependent packages 
+
+# Install git.
+apt-get -y --force-yes install git
+
+
+# Log
+echo "${GV_LOG} * Install git."
