195cc617e7bc85f8da409e71564f10574c8f8c79
limelime <limelime@users.noreply.github.com>
2017-04-27 14:59:31 -0400

    update.

--- git-common-commits.sh
+++ git-common-commits.sh
@@ -2,6 +2,19 @@
 set -e
 # Description: Commit common files that are change constantly.
 
-find scripts/repository/ -type f -name '*.sh.log' -exec git add '{}' + -exec git commit -m 'update *.sh.log' {} +
+# binary/**
+  find binary/ -type f -name '*.log' -exec git add '{}' + -exec git commit -m '*.sh.log' {} +         || true
 
-find scripts/repository/ -type f -name '*.sh.log.md' -exec git add '{}' + -exec git commit -m 'update *.sh.log.md' {} +
+# scripts/repository/**
+  find scripts/repository/ -type f -name '*.sh.log' -exec git add '{}' + -exec git commit -m 'update *.sh.log' {} +         || true
+  find scripts/repository/ -type f -name '*.sh.log.md' -exec git add '{}' + -exec git commit -m 'update *.sh.log.md' {} +   || true
+
+# Commit individual files
+  files_to_commit=(build-elapsed.txt)
+  files_to_commit+=(install-size-history.txt)
+  files_to_commit+=(sizes.log)
+  
+  for file_2_commit in "${files_to_commit[@]}"
+  do
+    git add "${file_2_commit}" && git commit -m "${file_2_commit}" "${file_2_commit}" || true
+  done
