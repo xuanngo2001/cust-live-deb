7d20918058d766c0e877e05180053209b5eb4526
limelime <limelime@users.noreply.github.com>
2017-04-27 14:53:09 -0400

    add.

--- quick-build.sh
+++ quick-build.sh
@@ -1,1 +1,1 @@
-./mirrors-update.sh && ./master-update.sh && cd ../cust-live-deb/ && ./build-cld.sh work
+./master-release.sh tmp && ./mirrors-update.sh && ./master-update.sh && cd ../cust-live-deb/ && ./build-cld.sh work
