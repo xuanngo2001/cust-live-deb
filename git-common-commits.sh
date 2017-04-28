4f5ada4db29cee4d2e985a117fbe93e2b2fef179
limelime <limelime@users.noreply.github.com>
2017-04-28 16:38:29 -0400

    specify update folder.

--- git-common-commits.sh
+++ git-common-commits.sh
@@ -6,11 +6,11 @@ set -e
   git ls-files --deleted -z | xargs -0 git rm && git commit -m 'Deleted files manually' || true
   
 # binary/**
-  find binary/ -type f -name '*.log' -exec git add '{}' + -exec git commit -m '*.sh.log' {} +         || true
+  find binary/ -type f -name '*.log' -exec git add '{}' + -exec git commit -m 'update binary/*.log' {} +         || true
 
 # scripts/repository/**
-  find scripts/repository/ -type f -name '*.sh.log' -exec git add '{}' + -exec git commit -m 'update *.sh.log' {} +         || true
-  find scripts/repository/ -type f -name '*.sh.log.md' -exec git add '{}' + -exec git commit -m 'update *.sh.log.md' {} +   || true
+  find scripts/repository/ -type f -name '*.sh.log' -exec git add '{}' + -exec git commit -m 'update repository/*.sh.log' {} +         || true
+  find scripts/repository/ -type f -name '*.sh.log.md' -exec git add '{}' + -exec git commit -m 'update repository/*.sh.log.md' {} +   || true
 
 # Commit individual files
   files_to_commit=(build-elapsed.txt)
