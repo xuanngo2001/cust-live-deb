5999656675613b5d8cd44365fdd353fcd119ebfa
limelime <limelime@users.noreply.github.com>
2017-08-02 18:44:39 -0400

    Deleted files manually

--- scripts/repository/inst-std-internet-firefox-esr/to-dl.sh
+++
@@ -1,3 +0,0 @@
-# The mozilla.debian.net archive is signed. Therefore, need to install the key.
-# http://mozilla.debian.net/
-wget http://mozilla.debian.net/pkg-mozilla-archive-keyring_1.1_all.deb
--- scripts/repository/inst-xtra-zfs-bpo/test-zfs.sh
+++
@@ -1,3 +0,0 @@
-    # View file
-    echo "ZFS $(modinfo zfs | grep ^version | tr -s ' ')"
-    cat /zfs-dkms-bpo/file-in-zfs.txt
