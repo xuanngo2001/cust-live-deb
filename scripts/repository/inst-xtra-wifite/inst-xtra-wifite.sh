a5338cb9278a82e40d202c85a33f9ca51ffd8912
limelime <limelime@users.noreply.github.com>
2017-08-08 20:19:00 +0000

    Guarantee that wifite is executable.

--- scripts/repository/inst-xtra-wifite/inst-xtra-wifite.sh
+++ scripts/repository/inst-xtra-wifite/inst-xtra-wifite.sh
@@ -7,12 +7,14 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Install wifite
+# Install dependencies.
 debconf-set-selections -v wifite-wireshark.seed
-apt-get -y install wifite
+apt-get -y install tshark
+apt-get -y install aircrack-ng reaver 
 
 # Update wifite to revision 87.
 yes | cp wifite87.py /usr/bin/wifite
+chmod +x /usr/bin/wifite
 
 # Log
 echo "${GV_LOG} * Install wifite."
