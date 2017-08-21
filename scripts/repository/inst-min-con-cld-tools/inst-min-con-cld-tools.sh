463518fd8f22823e31a09371af69f3a675454850
limelime <limelime@users.noreply.github.com>
2017-08-21 11:22:38 +0000

    Force installation of require apps for cld-dd-usb.sh.

--- scripts/repository/inst-min-con-cld-tools/inst-min-con-cld-tools.sh
+++ scripts/repository/inst-min-con-cld-tools/inst-min-con-cld-tools.sh
@@ -6,6 +6,10 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install packages for cld-dd-usb:
+  apt-get -y install parted     # partprobe
+  apt-get -y install util-linux # partx
+  apt-get -y install udev       # udevadm
 
 chmod +x ./cld/bin/*.sh
 yes | cp -av ./cld/ /usr/local/
