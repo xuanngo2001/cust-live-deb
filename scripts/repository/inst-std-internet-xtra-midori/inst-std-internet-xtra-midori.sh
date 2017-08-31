5d6cd8d5abffe98a8125d80a0c3e78378331d901
limelime <limelime@users.noreply.github.com>
2017-08-31 17:07:59 +0000

    Add midori.

---
+++ scripts/repository/inst-std-internet-xtra-midori/inst-std-internet-xtra-midori.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Examples:
+#   apt-get install -d -y <your-package>  # Explicitly download dependent packages 
+
+# Install midori.
+apt-get -y install midori
+
+
+# Log
+echo "${GV_LOG} * Install midori."
