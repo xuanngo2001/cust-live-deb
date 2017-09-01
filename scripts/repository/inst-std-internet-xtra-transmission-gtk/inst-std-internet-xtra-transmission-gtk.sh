958cbc916415b103ac65ecfdf3d115eac4ae6613
limelime <limelime@users.noreply.github.com>
2017-09-01 19:33:46 +0000

    Remove useless comment.

--- scripts/repository/inst-std-internet-xtra-transmission-gtk/inst-std-internet-xtra-transmission-gtk.sh
+++ scripts/repository/inst-std-internet-xtra-transmission-gtk/inst-std-internet-xtra-transmission-gtk.sh
@@ -7,9 +7,6 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Examples:
-#   apt-get install -d -y <your-package>  # Explicitly download dependent packages 
-
 # Install transmission-gtk.
 apt-get -y install transmission-gtk
 
