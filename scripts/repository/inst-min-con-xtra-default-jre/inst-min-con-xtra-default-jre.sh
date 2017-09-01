d448fc6ce2a1a416d535a8a1af81bb1f2e7d7e3a
limelime <limelime@users.noreply.github.com>
2017-09-01 15:47:48 +0000

    removed unknown comment.

--- scripts/repository/inst-min-con-xtra-default-jre/inst-min-con-xtra-default-jre.sh
+++ scripts/repository/inst-min-con-xtra-default-jre/inst-min-con-xtra-default-jre.sh
@@ -17,12 +17,3 @@ apt-get -y install default-jre
 # Log
 echo "${GV_LOG} * Install JAVA JRE: default-jre."
 echo "${GV_LOG} * To install JAVA binary: https://wiki.debian.org/JavaPackage."
-
-
-#  tzdata version conflict with Debian update repository.
-# apt-get -y install default-jre
-# apt-get -y install default-jre-headless
-# apt-get -y install openjdk-7-jre-headless
-# apt-get -y install tzdata-java
-#   # Depends: tzdata (= 2016c-0+deb8u1) but 2016d-0+deb8u1 is to be installed
-# apt-get -y install tzdata=2016c-0+deb8u1
