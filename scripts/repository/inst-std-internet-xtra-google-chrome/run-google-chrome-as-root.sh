00d41f7c5ad47391eff0756c0f40bb7736c9a19f
limelime <limelime@users.noreply.github.com>
2017-09-07 16:00:19 +0000

    Make regex more readable: switching to ' and |.

--- scripts/repository/inst-std-internet-xtra-google-chrome/run-google-chrome-as-root.sh
+++ scripts/repository/inst-std-internet-xtra-google-chrome/run-google-chrome-as-root.sh
@@ -5,5 +5,6 @@ set -e
 
 # Run Google Chrome as root. Consider: x-www-browser. 
 GOOGLE_CHROME_BIN=/opt/google/chrome/google-chrome
-sed -i.original "s/exec -a \"\$0\" \"\$HERE\/chrome\" \"\$@\"$/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\" --no-sandbox --user-data-dir/" "${GOOGLE_CHROME_BIN}"
+sed -i.original 's|exec -a "$0" "$HERE/chrome" "$@"$|exec -a "$0" "$HERE/chrome" "$@" --no-sandbox --user-data-dir|' "${GOOGLE_CHROME_BIN}"
+#sed -i.original "s/exec -a \"\$0\" \"\$HERE\/chrome\" \"\$@\"$/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\" --no-sandbox --user-data-dir/" "${GOOGLE_CHROME_BIN}"
 #sed             "s/exec -a \"\$0\" \"\$HERE\/chrome\" \"\$@\"$/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\" --no-sandbox --user-data-dir/" /opt/google/chrome/google-chrome
