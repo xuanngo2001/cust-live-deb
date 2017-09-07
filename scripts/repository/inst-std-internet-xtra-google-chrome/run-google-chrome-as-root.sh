d718b6e02cde7e5f7f46f34fe91547c93799f377
limelime <limelime@users.noreply.github.com>
2017-09-07 15:47:17 +0000

    decouple.

---
+++ scripts/repository/inst-std-internet-xtra-google-chrome/run-google-chrome-as-root.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Description: Change configuration so that google-chrome can be ran as root.
+
+
+# Run Google Chrome as root. Consider: x-www-browser. 
+GOOGLE_CHROME_BIN=/opt/google/chrome/google-chrome
+sed -i.original "s/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\"$/exec -a \"\$0\" \"\$HERE\/chrome\"  \"\$@\" --no-sandbox --user-data-dir/" "${GOOGLE_CHROME_BIN}"
