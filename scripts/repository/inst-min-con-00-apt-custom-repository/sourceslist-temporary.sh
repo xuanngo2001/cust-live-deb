46ef76983fd4c6b5aa4fde3fda33c488399a8a21
limelime <limelime@users.noreply.github.com>
2017-09-12 13:19:01 +0000

    Add opera repository.

--- scripts/repository/inst-min-con-00-apt-custom-repository/sourceslist-temporary.sh
+++ scripts/repository/inst-min-con-00-apt-custom-repository/sourceslist-temporary.sh
@@ -2,6 +2,7 @@ GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_aptly-main squeez
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_deb-multimedia-main stretch main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_deb-multimedia-non-free stretch non-free"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_google-main stable main"
+GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_opera-non-free stable non-free"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_stretch-backports-contrib stretch-backports contrib"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_stretch-backports-main stretch-backports main"
 GF_ADD_SOURCE_LIST tmp "deb http://localhost/aptly-repo/master_stretch-backports-non-free stretch-backports non-free"
