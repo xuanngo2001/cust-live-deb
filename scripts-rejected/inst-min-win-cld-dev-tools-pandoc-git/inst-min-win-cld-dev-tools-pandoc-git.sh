b796a21b4571b91775f0be34319bc8ded81e9d77
limelime <limelime@users.noreply.github.com>
2017-09-12 18:14:41 +0000

    update pandoc.

--- scripts-rejected/inst-min-win-cld-dev-tools-pandoc-git/inst-min-win-cld-dev-tools-pandoc-git.sh
+++ scripts-rejected/inst-min-win-cld-dev-tools-pandoc-git/inst-min-win-cld-dev-tools-pandoc-git.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Ensure that dependent packages are downloaded.
-  apt-get install -d -y pandoc
+  #apt-get install -d -y pandoc
 
 # Install the latest version from Git
   PANDOC_DEB=$(find . -name 'pandoc*.deb' | sort -r | head -n1 )
@@ -19,3 +19,6 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Log
 echo "${GV_LOG} * Install ${PANDOC_VER} from git."
+
+# REJECTED:
+#   Bigger than standard package. Standard package=80MB, Git package=111MB.
