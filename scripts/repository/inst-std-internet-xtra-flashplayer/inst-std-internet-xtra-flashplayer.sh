7566ae82d52881676a536920ef1c3b673622bcd2
limelime <limelime@users.noreply.github.com>
2017-09-12 13:07:43 +0000

    Fix: Add archive file.

--- scripts/repository/inst-std-internet-xtra-flashplayer/inst-std-internet-xtra-flashplayer.sh
+++ scripts/repository/inst-std-internet-xtra-flashplayer/inst-std-internet-xtra-flashplayer.sh
@@ -7,7 +7,7 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-adobe_flashplayer_version=$(./adobe-flashplayer-install.sh)
+adobe_flashplayer_version=$(./adobe-flashplayer-install.sh flash_player_ppapi_linux.x86_64.tar.gz)
 
 # Log
 echo "${GV_LOG} * Install flashplayer: ${adobe_flashplayer_version}."
