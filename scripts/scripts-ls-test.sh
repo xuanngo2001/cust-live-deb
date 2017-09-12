bfbf3c973112548877c4d693d79ef064af53f4e3
limelime <limelime@users.noreply.github.com>
2017-09-12 16:59:52 +0000

    add test script.

---
+++ scripts/scripts-ls-test.sh
@@ -0,0 +1,10 @@
+./scripts-list.sh min;  yes | cp scripts-ls.lst b-min.txt
+./scripts-list.sh std;  yes | cp scripts-ls.lst b-std.txt
+./scripts-list.sh home; yes | cp scripts-ls.lst b-home.txt
+./scripts-list.sh work; yes | cp scripts-ls.lst b-work.txt
+
+
+./scripts-ls.sh min;  yes | cp scripts-ls.lst a-min.txt
+./scripts-ls.sh std;  yes | cp scripts-ls.lst a-std.txt
+./scripts-ls.sh home; yes | cp scripts-ls.lst a-home.txt
+./scripts-ls.sh work; yes | cp scripts-ls.lst a-work.txt
