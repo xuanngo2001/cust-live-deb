9585a0cc3975b6803e6bc77a2c162b72eec50a52
limelime <limelime@users.noreply.github.com>
2017-09-13 17:06:06 +0000

    Clean up.

--- scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
+++ scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
@@ -7,13 +7,6 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-# Install keyring.
-DEB_MULTIMEDIA_KEYRING=deb-multimedia-keyring_2015.6.1_all.deb
-DEB_MULTIMEDIA_KEYRING_SIZE=$(GF_SIZE_OF ${DEB_MULTIMEDIA_KEYRING})
-dpkg -i ${DEB_MULTIMEDIA_KEYRING}
-rm -f ${DEB_MULTIMEDIA_KEYRING}
-
-#apt-get -y install deb-multimedia-keyring
 
 # Add deb-multimedia sources list
 GF_ADD_SOURCE_LIST live "deb http://www.deb-multimedia.org stretch main non-free"
