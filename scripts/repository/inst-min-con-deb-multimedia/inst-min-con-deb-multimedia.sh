cb55430c2c8e78576e65ed452750f502e53ef088
limelime <limelime@users.noreply.github.com>
2017-09-13 20:16:25 +0000

    Add key.

--- scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
+++ scripts/repository/inst-min-con-deb-multimedia/inst-min-con-deb-multimedia.sh
@@ -7,14 +7,19 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Add deb-multimedia key.
+  keyring_file=deb-multimedia-keyring_2016.8.1_all.deb
+  #wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/${keyring_file}
+  dpkg -i ${keyring_file} && rm -f ${keyring_file}
 
 # Add deb-multimedia sources list
-GF_ADD_SOURCE_LIST live "deb http://www.deb-multimedia.org stretch main non-free"
+  GF_ADD_SOURCE_LIST live "deb http://www.deb-multimedia.org stretch main non-free"
 
 # Update packages.
-apt-get update
+  apt-get update
 
 # Log
+echo "${GV_LOG} * Add deb-multimedia key."
 echo "${GV_LOG} * Add deb-multimedia repository."
 echo "${GV_LOG} * apt-get update"
 
