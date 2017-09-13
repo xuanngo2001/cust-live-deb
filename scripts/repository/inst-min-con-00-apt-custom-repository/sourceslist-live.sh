474827d5b2024a9d8700c4ddcd1a75a2a43d7e7c
limelime <limelime@users.noreply.github.com>
2017-09-13 16:33:13 +0000

    distro upgrade to stretch

--- scripts/repository/inst-min-con-00-apt-custom-repository/sourceslist-live.sh
+++ scripts/repository/inst-min-con-00-apt-custom-repository/sourceslist-live.sh
@@ -1,4 +1,4 @@
 # Add default sources list
-GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian jessie main contrib non-free"
-GF_ADD_SOURCE_LIST live "deb http://security.debian.org/ jessie/updates main contrib non-free"
-GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian stretch main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://security.debian.org/ stretch/updates main contrib non-free"
+GF_ADD_SOURCE_LIST live "deb http://ftp.debian.org/debian/ stretch-updates main contrib non-free"
