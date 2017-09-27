c5b9d310c45d0205eaa27db410df3d6794aa8a5d
limelime <limelime@users.noreply.github.com>
2017-09-27 18:08:04 +0000

    Add missing php extension.

---
+++ scripts/repository/inst-std-xtra-opw-php-xml/inst-std-xtra-opw-php-xml.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install php-xml.
+apt-get -y install php-xml
+
+
+# Log
+echo "${GV_LOG} * Install php-xml."
