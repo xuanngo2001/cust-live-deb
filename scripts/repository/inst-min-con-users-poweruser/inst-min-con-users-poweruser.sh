8e5c76e861ede98792881c833342e0f762dadc12
limelime <limelime@users.noreply.github.com>
2017-10-30 15:44:16 +0000

    Elevate poweruser with root privilege.

--- scripts/repository/inst-min-con-users-poweruser/inst-min-con-users-poweruser.sh
+++ scripts/repository/inst-min-con-users-poweruser/inst-min-con-users-poweruser.sh
@@ -8,12 +8,17 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
-# Add power user.
+# Create power user.
 username='poweruser'
 adduser --disabled-password --gecos "" "${username}"
 (echo "${username}";echo "${username}") | passwd "${username}"
 
-
+# Add poweruser to sudo group(root).
+usermod -aG sudo "${username}"
 
 # Log
-echo "${GV_LOG} * Created user: ${username}/${username}."
+echo "${GV_LOG} * Created poweruser: Username/pwd = ${username}/${username}."
+
+# Test
+#   Log in as poweruser.
+#   touch /a.txt
