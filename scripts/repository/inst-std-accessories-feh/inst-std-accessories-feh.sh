743ddc01723dd4e8ede8b3fa21abede91b8a8ea8
limelime <limelime@users.noreply.github.com>
2017-11-22 21:06:28 +0000

    Add comment.

--- scripts/repository/inst-std-accessories-feh/inst-std-accessories-feh.sh
+++ scripts/repository/inst-std-accessories-feh/inst-std-accessories-feh.sh
@@ -6,6 +6,7 @@
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Install feh
 apt-get -y install feh
 
 # Add to CLD/
