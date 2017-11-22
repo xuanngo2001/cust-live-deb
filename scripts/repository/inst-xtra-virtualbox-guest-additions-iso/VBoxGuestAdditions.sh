5301b9cd4072331ea38c42be1afe38b3ada2c8a3
limelime <limelime@users.noreply.github.com>
2017-11-22 00:04:33 +0000

    update virtualbox iso version.

--- scripts/repository/inst-xtra-virtualbox-guest-additions-iso/VBoxGuestAdditions.sh
+++ scripts/repository/inst-xtra-virtualbox-guest-additions-iso/VBoxGuestAdditions.sh
@@ -7,9 +7,9 @@ set -e
 
 # BAD ISO, linux headers not found: VBoxGuestAdditions_4.3.4.iso
 
-URL=http://download.virtualbox.org/virtualbox/5.0.20/VBoxGuestAdditions_5.0.20.iso # Jessie
 URL=http://download.virtualbox.org/virtualbox/5.1.14/VBoxGuestAdditions_5.1.14.iso # Jessie
 URL=http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso # Stretch
+URL=http://download.virtualbox.org/virtualbox/5.2.0/VBoxGuestAdditions_5.2.0.iso
 
 # Download ISO if not already exists.
 ISO_FILE=$(basename $URL)
