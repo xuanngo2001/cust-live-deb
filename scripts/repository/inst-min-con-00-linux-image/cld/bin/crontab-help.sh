522ef68c01bde0abb7488c04e1e132ee4e08e955
limelime <limelime@users.noreply.github.com>
2018-01-02 19:32:30 +0000

    Add ehlpf for crontab.

--- scripts/repository/inst-min-con-00-linux-image/cld/bin/crontab-help.sh
+++ scripts/repository/inst-min-con-00-linux-image/cld/bin/crontab-help.sh
@@ -4,10 +4,12 @@ set -e
 #   Highlight comment: ./crontab-help.sh | grep -E '^|#.*'
 
 cat << EndOfCrontabHelp
+  # Crontab will NOT add new entry, only overwrite the main configuration file.
+  
   # Run script.sh hourly
   echo "@hourly script.sh" | crontab
   
   # List crontab job
-  crontab -l  
+  crontab -l
   
 EndOfCrontabHelp
