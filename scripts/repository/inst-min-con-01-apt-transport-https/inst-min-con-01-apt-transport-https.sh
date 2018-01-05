c2773ffb43a0301bba9a5cbbb9fc4de95dd68436
limelime <limelime@users.noreply.github.com>
2018-01-05 22:55:47 +0000

    Fix #271.

---
+++ scripts/repository/inst-min-con-01-apt-transport-https/inst-min-con-01-apt-transport-https.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+# ${GV_CLD_ROOT_DIR}: Hold settings and scripts files for Cust-Live-Deb.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install apt-transport-https.
+apt-get -y install apt-transport-https
+
+
+# Log
+echo "${GV_LOG} * Install apt-transport-https to support https debian repositories."
