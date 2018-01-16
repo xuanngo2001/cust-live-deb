a89045a617a3f40f13dc25067d443637d02a69ba
limelime <limelime@users.noreply.github.com>
2018-01-16 02:54:21 +0000

    Update mpv-find.sh

--- scripts/repository/inst-std-multimedia-mpv/cld/bin/mpv-find.sh
+++ scripts/repository/inst-std-multimedia-mpv/cld/bin/mpv-find.sh
@@ -2,7 +2,12 @@
 set -e
 # Description: Find all video files and play
 
-# Doesn't work yet.
-mpv -playlist <(find . -type f -name '*.avi' -o -name '*.mkv' -o -name '*.mp4' -o -name '*.mpg' -o -name '*.mpeg' -o -name '*.rmvb' -o -name '*.rm')
+# Error handling.
+  if [ -z "$1" ]; then
+    echo "Error: Input can't be empty. Aborted!"
+    exit 1;
+  fi
 
-find . -name "artist name" -exec mplayer '{}' \;
+# Play all videos.
+  dir_path=$(readlink -ev "$1")
+  mpv -playlist <(find "${dir_path}" -type f -name '*.avi' -o -name '*.mkv' -o -name '*.mp4' -o -name '*.mpg' -o -name '*.mpeg' -o -name '*.rmvb' -o -name '*.rm')
