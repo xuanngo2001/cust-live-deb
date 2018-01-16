c82fa00eeaa1cf0eb636b7d1a39ea21eab2185be
limelime <limelime@users.noreply.github.com>
2018-01-16 03:19:03 +0000

    Update mpv-find.sh

--- scripts/repository/inst-std-multimedia-mpv/cld/bin/mpv-find.sh
+++ scripts/repository/inst-std-multimedia-mpv/cld/bin/mpv-find.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 # Description: Find all video files and play from directory.
-#   Reference: https://github.com/limelime/cust-live-deb/issues/268
 
 # Error handling.
   if [ -z "$1" ]; then
@@ -11,4 +10,13 @@ set -e
 
 # Play all videos.
   dir_path=$(readlink -ev "$1")
-  mpv -playlist <(find "${dir_path}" -type f -iname '*.avi' -o -iname '*.mkv' -o -iname '*.mp4' -o -iname '*.mpg' -o -iname '*.mpeg' -o -iname '*.rmvb' -o -iname '*.rm' -o -iname '*.flv' -o -iname '*.ogg' -o -iname '*.vob')
+  mpv -playlist <(find "${dir_path}" -type f -iname '*.avi'\
+                                          -o -iname '*.mkv'\
+                                          -o -iname '*.mp4'\
+                                          -o -iname '*.mpg'\
+                                          -o -iname '*.mpeg'\
+                                          -o -iname '*.rmvb'\
+                                          -o -iname '*.rm'\
+                                          -o -iname '*.flv'\
+                                          -o -iname '*.ogg'\
+                                          -o -iname '*.vob')
