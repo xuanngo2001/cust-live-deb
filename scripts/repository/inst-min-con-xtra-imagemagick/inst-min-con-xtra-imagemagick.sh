d17d92e76e2bc7cc1875ec39859ad9d86fa3fef9
limelime <limelime@users.noreply.github.com>
2018-03-11 05:47:01 +0000

    ADD: Imagemagick help.

--- scripts/repository/inst-min-con-xtra-imagemagick/inst-min-con-xtra-imagemagick.sh
+++ scripts/repository/inst-min-con-xtra-imagemagick/inst-min-con-xtra-imagemagick.sh
@@ -10,6 +10,10 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Install imagemagick to process image from command line.
 apt-get -y install imagemagick
 
+# Add to CLD/
+ yes | cp -av ./cld  /usr/local
+
+
 # Log
 echo "${GV_LOG} * Install imagemagick to process image from command line."
 
