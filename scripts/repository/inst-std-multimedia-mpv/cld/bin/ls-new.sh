4f1df505b0be0ea1ab514bcd0c912ef25d5801a0
limelime <limelime@users.noreply.github.com>
2018-06-19 23:04:32 +0000

    Improve error handling.

--- scripts/repository/inst-std-multimedia-mpv/cld/bin/ls-new.sh
+++ scripts/repository/inst-std-multimedia-mpv/cld/bin/ls-new.sh
@@ -3,16 +3,22 @@ set -e
 # Description: List files that are new from input file.
 #   Usage: ./this-script.sh prev-list-file.txt
 #     mpv --playlist prev-list-file.txt
-#     For all files viewed, put # in front of filename from prev-list-file.txt. 
+#     For all files viewed, put # in front of filename from prev-list-file.txt.
+# Note: from ../inst-std-multimedia-mpv/cld/bin
 
 prev_ls_files=$1
 
 # Error handling.
   if [ -z "${prev_ls_files}" ]; then
-    echo "Error: $(basename "$0"): Input list file can't be empty. Aborted!"
+    echo "Error: $(basename "$0"): Input list filename can't be empty. Aborted!"
     exit 1;
   fi
-  
+
+  if [ -d "${prev_ls_files}" ]; then
+    echo "Error: $(basename "$0"): ${prev_ls_files} can't be a directory. Please supply filename for input list. Aborted!"
+    exit 1;
+  fi
+    
 # If ${prev_ls_files} not found, then create it.
   if [ ! -f "${prev_ls_files}" ]; then
     touch "${prev_ls_files}"
