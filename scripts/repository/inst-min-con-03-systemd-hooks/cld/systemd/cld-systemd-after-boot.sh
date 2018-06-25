a686b0bc33cef1c401f9e7acd457e143cfe0a262
limelime <limelime@users.noreply.github.com>
2018-06-25 22:36:07 +0000

    Use ../user/ folder.

--- scripts/repository/inst-min-con-03-systemd-hooks/cld/systemd/cld-systemd-after-boot.sh
+++ scripts/repository/inst-min-con-03-systemd-hooks/cld/systemd/cld-systemd-after-boot.sh
@@ -2,13 +2,19 @@
 set -e
 echo "$0: $(date)" >> /usr/local/cld/log/boot-sequence.log
 # Description: After boot, run the following scripts:
-#   Note: Internal use only. If you want to add you own custom scripts, add in
-#           /root/cld/systemd/user/cld-user-after-boot.sh
+#   Note: Internal use only. If you want to add you own custom scripts, add
+#           your scripts in /root/cld/systemd/user/after-boot/
 # Reference: 
 #   -http://unix.stackexchange.com/a/41756
 #   -https://wiki.archlinux.org/index.php/systemd
 
 log_file="/usr/local/cld/log/$(basename "$0").log"
 
+# Core use only.
 scripts_dir=/usr/local/cld/systemd/runlevel/after-boot
 /usr/local/cld/bin/cld-run-scripts.sh "${scripts_dir}" 2>&1 | tee -a "${log_file}"
+
+# Users custom scripts. To prevent overwrite on upgrade. 
+scripts_dir=/usr/local/cld/systemd/user/after-boot
+/usr/local/cld/bin/cld-run-scripts.sh "${scripts_dir}" 2>&1 | tee -a "${log_file}"
+
