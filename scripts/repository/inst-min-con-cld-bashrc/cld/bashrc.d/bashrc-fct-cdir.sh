d2120b170f070c5f62c750dda3609c6b2efe1c24
limelime <limelime@users.noreply.github.com>
2018-06-26 20:38:25 +0000

    added shell function to get the current directory.

---
+++ scripts/repository/inst-min-con-cld-bashrc/cld/bashrc.d/bashrc-fct-cdir.sh
@@ -0,0 +1,18 @@
+
+# Get current directory can only be done using shell function.
+#   Hence, the function below.
+function cdir()
+{ 
+  local current_directory=$(dirname "$0")                 # relative
+  current_directory=$( cd "${current_directory}" && pwd ) # absolutized and normalized
+  if [ -z "${current_directory}" ]; then
+    # error; for some reason, the path is not accessible
+    # to the script (e.g. permissions re-evaled after suid)
+    return 1  # fail
+  fi
+  
+  echo "${current_directory}"
+  
+}  
+export -f cdir
+
