b5ef25d67e9823594d5b2a51deb06d28adce2a4f
limelime <limelime@users.noreply.github.com>
2018-06-26 23:07:12 +0000

    Add move iso.

--- quick-build.sh
+++ quick-build.sh
@@ -1,1 +1,2 @@
 ./master-release.sh tmp && ./mirrors-update.sh && ./master-update.sh && ./aptly-update-cld.sh && cd ../cust-live-deb/ && ./build-cld.sh work
+mv cust-live-deb-64-*_????-??-??_*.iso /media/sf_shared/
