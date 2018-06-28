53e216315801c046512f19a3954f450c180dc046
limelime <limelime@users.noreply.github.com>
2018-06-28 20:45:14 +0000

    Add Cheese in Graphic menu.

--- scripts/repository/inst-std-graphic-xtra-cheese/inst-std-graphic-xtra-cheese.sh
+++ scripts/repository/inst-std-graphic-xtra-cheese/inst-std-graphic-xtra-cheese.sh
@@ -13,9 +13,12 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Install cheese.
 apt-get -y install cheese
 
+# Insert guvcview menu in Graphics
+sed -i "/Graphics\">/ r jwmrc-menus-graphics-cheese.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Log
 echo "${GV_LOG} * Install cheese."
+echo "${GV_LOG} * By default, cheese stores all files in ~/.gnome2/cheese/media . "
 
 # REJECTED because you can't choose where to store recorded file.
 #   Cheese will add 57,396 KB whereas Guvcview will add 4,340 KB.
