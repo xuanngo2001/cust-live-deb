662f901f14a73063d6782af0c37c3cb3dc0e841a
limelime <limelime@users.noreply.github.com>
2018-06-29 18:45:17 +0000

    Improve description.

--- scripts/repository/inst-min-con-cld-bin/cld/bin/cld-dirs-create.sh
+++ scripts/repository/inst-min-con-cld-bin/cld/bin/cld-dirs-create.sh
@@ -55,7 +55,7 @@ case "${action}" in
       echo "Error: Action: ${action} is unknown. Aborted!"
       printf "    %-15s %-43s [%s]\n" 'where-used'        '=> N/A'                                    'Show where paths below are used'
       printf "    %-15s %-43s [%s]\n" 'bin'               '=> ./cld/bin'                              'Put your executable here'
-      printf "    %-15s %-43s [%s]\n" 'doc'               '=> ./cld/doc'                              'Documentation'
+      printf "    %-15s %-43s [%s]\n" 'doc'               '=> ./cld/doc'                              'CLD documentation'
       printf "    %-15s %-43s [%s]\n" 'home'              '=> ./cld/home'                             '$HOME directory'
       printf "    %-15s %-43s [%s]\n" 'app'               '=> ./cld/app'
       printf "    %-15s %-43s [%s]\n" 'jwmrc'             '=> ./cld/app/jwm/jwmrc'                    'JWM configuration files'
