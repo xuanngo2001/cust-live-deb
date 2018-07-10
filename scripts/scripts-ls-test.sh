7316e0cc0ad840f2e5c611c3951ca92bcee3a4ca
limelime <limelime@users.noreply.github.com>
2018-07-10 20:56:34 +0000

    eod update.

--- scripts/scripts-ls-test.sh
+++ scripts/scripts-ls-test.sh
@@ -11,5 +11,17 @@ systems+=( work work_no_proxy )
 systems+=( all )
 for system in "${systems[@]}"
 do
-  ./scripts-ls.sh ${system};  yes | cp scripts-ls.lst test-scripts_${system}_${date_str}.txt
+  test_output_filename=test-scripts_${system}_${date_str}.txt
+  ./scripts-ls.sh ${system};  yes | cp scripts-ls.lst "${test_output_filename}"
+  
+  # Remove parent directories up to .../scripts/repository/
+    sed -i 's|.*/scripts/repository/||' "${test_output_filename}"
+  
+  # if diff than test-scripts_*_2000-01-01.00.00.00.txt, then create new scripts-ls.lst.
+    if cmp -s "test-scripts_${system}_2000-01-01.00.00.00.txt" "${test_output_filename}"; then
+      rm "${test_output_filename}"
+    fi
+  
 done
+
+
