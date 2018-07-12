44ad4207f27b8dd2b0c73c8b184c03595b47097c
limelime <limelime@users.noreply.github.com>
2018-07-12 15:50:03 +0000

    Add source list url where needed.

--- scripts/repository/inst-xtra-virtualbox/inst-xtra-virtualbox.sh
+++ scripts/repository/inst-xtra-virtualbox/inst-xtra-virtualbox.sh
@@ -7,12 +7,16 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Add source url.
+  GF_ADD_SOURCE_LIST live "deb https://download.virtualbox.org/virtualbox/debian stretch contrib"
+
 # Explicitly download dependent packages
 #   apt-get install -d -y <your-package> 
 
+
 # Install virtualbox.
-apt-get -y install virtualbox-5.2 libqt5opengl5 libqt5x11extras5
+  apt-get -y install virtualbox-5.2 libqt5opengl5 libqt5x11extras5
 
 
 # Log
-echo "${GV_LOG} * Install virtualbox."
+  echo "${GV_LOG} * Install virtualbox."
