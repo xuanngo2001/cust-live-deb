19c771ab6e51e5fdbefd3dc586ca59319e98eddc
limelime <limelime@users.noreply.github.com>
2018-07-12 19:48:14 +0000

    add build all.

--- quick-build.sh
+++ quick-build.sh
@@ -1,2 +1,3 @@
 ./master-release.sh tmp && ./mirrors-update.sh && ./master-update.sh && ./aptly-update-cld.sh && cd ../cust-live-deb/ && ./build-cld.sh work
-mv cust-live-deb-64-*_????-??-??_*.iso /media/sf_shared/
+
+./build-cld.sh home; ./build-cld.sh home_inspiron; mv cust-live-deb-64-*_????-??-??_*.iso /media/sf_shared/; ./build-cld.sh work_no_proxy
