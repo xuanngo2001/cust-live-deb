ccb13a9c082b75377322145608daf1551bd2f249
limelime <limelime@users.noreply.github.com>
2018-07-12 21:13:09 +0000

    update gpg key.

--- scripts/repository/inst-min-con-xtra-cld-dev-tools-aptly/inst-min-con-xtra-cld-dev-tools-aptly.sh
+++ scripts/repository/inst-min-con-xtra-cld-dev-tools-aptly/inst-min-con-xtra-cld-dev-tools-aptly.sh
@@ -8,24 +8,23 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Add nightly build repository.
-#GF_ADD_SOURCE_LIST live "deb http://repo.aptly.info/ nightly main"
-GF_ADD_SOURCE_LIST live "deb http://repo.aptly.info/ squeeze main"
+  GF_ADD_SOURCE_LIST live "deb http://repo.aptly.info/ squeeze main"
 
-# Add repository key.
-apt-key add pubkey.txt
-apt-get update
+# Add repository key: https://www.aptly.info/pubkey.txt
+  apt-key add pubkey.txt
+  apt-get update
 
 # aptly needs gnupg needs dirmngr
-apt-get -y install dirmngr
+  apt-get -y install dirmngr
 
 # Install aptly
-apt-get -y install xz-utils aptly
-apt-get -y install gpgv1 gnupg1   # Aptly still uses PGP v1.
+  apt-get -y install xz-utils aptly
+  apt-get -y install gpgv1 gnupg1   # Aptly still uses PGP v1.
 
 # Log
-echo "${GV_LOG} * Add aptly nightly build repository."
-echo "${GV_LOG} * Install $(aptly version) to create local debian repository."
-echo "${GV_LOG} * Add xz-utils package because aptly snapshot merge needs it." 
+  echo "${GV_LOG} * Add aptly nightly build repository."
+  echo "${GV_LOG} * Install $(aptly version) to create local debian repository."
+  echo "${GV_LOG} * Add xz-utils package because aptly snapshot merge needs it." 
 
 
 # Get key
