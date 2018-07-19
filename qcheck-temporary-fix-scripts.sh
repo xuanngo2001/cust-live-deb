6bbf3443a94bff1ccd70a06e82d846fc2b62c35e
limelime <limelime@users.noreply.github.com>
2018-07-19 21:44:36 +0000

    Remove parent directories.

--- qcheck-temporary-fix-scripts.sh
+++ qcheck-temporary-fix-scripts.sh
@@ -5,18 +5,19 @@ repository_dir=./scripts/repository/
 
 
 # Add newline as a separation.
-echo ""
-
-while IFS='' read -r filename || [[ -n "${filename}" ]]; do
- 
-  echo "Warning: Temporary fix found: ${filename}"
+  echo ""
   
-done < <( find "${repository_dir}" -type f -name "inst-*-fix.sh" )
+  while IFS='' read -r filename || [[ -n "${filename}" ]]; do
+    
+    script_name=$(echo "${filename}"| sed 's|./scripts/repository/|./|')
+    echo "   ${script_name}"
+    
+  done < <( find "${repository_dir}" -type f -name "inst-*-fix.sh" )
 
 
 # Display solution.
-echo 'All scripts above should be removed or merged in the official installation script.'
+  echo 'All scripts above should be removed or merged in the official installation script.'
 
 
 # Add newline as a separation.
-echo ""  
+  echo ""  
