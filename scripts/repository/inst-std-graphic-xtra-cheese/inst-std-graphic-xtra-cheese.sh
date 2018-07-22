feb16872638e4bb7c3b7933b946ed605099cec36
limelime <limelime@users.noreply.github.com>
2018-07-22 23:37:00 +0000

    add webcam icon.

--- scripts/repository/inst-std-graphic-xtra-cheese/inst-std-graphic-xtra-cheese.sh
+++ scripts/repository/inst-std-graphic-xtra-cheese/inst-std-graphic-xtra-cheese.sh
@@ -11,14 +11,17 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 #   apt-get install -d -y <your-package> 
 
 # Install cheese.
-apt-get -y install cheese
+  apt-get -y install cheese
 
 # Insert guvcview menu in Graphics
-sed -i "/Graphics\">/ r jwmrc-menus-graphics-cheese.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+  sed -i "/Graphics\">/ r jwmrc-menus-graphics-cheese.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+
+# Add to CLD/
+  yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install cheese."
-echo "${GV_LOG} * By default, cheese stores all files in ~/.gnome2/cheese/media . "
+  echo "${GV_LOG} * Install cheese."
+  echo "${GV_LOG} * By default, cheese stores all files in ~/.gnome2/cheese/media . "
 
 # REJECTED because you can't choose where to store recorded file.
 #   Cheese will add 57,396 KB whereas Guvcview will add 4,340 KB.
