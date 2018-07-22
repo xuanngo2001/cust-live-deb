a85c7d70b449081a45eec8836c01d61b12668891
limelime <limelime@users.noreply.github.com>
2018-07-22 19:20:11 +0000

    gnuplot with no X.

--- scripts/repository/inst-min-con-xtra-gnuplot-nox/inst-min-con-xtra-gnuplot-nox.sh
+++ scripts/repository/inst-min-con-xtra-gnuplot-nox/inst-min-con-xtra-gnuplot-nox.sh
@@ -8,9 +8,9 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install gnuplot.
-apt-get -y install gnuplot
+apt-get -y install gnuplot-nox
 
 # Log
-echo "${GV_LOG} * Install gnuplot."
+echo "${GV_LOG} * Install gnuplot-nox."
 
 
