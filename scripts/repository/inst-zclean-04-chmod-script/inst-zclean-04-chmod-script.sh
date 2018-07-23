20e8b373b0511aef07c9ab5c9309e330c365793a
limelime <limelime@users.noreply.github.com>
2018-07-23 20:42:38 +0000

    list all *.sh.

--- scripts/repository/inst-zclean-04-chmod-script/inst-zclean-04-chmod-script.sh
+++ scripts/repository/inst-zclean-04-chmod-script/inst-zclean-04-chmod-script.sh
@@ -8,7 +8,8 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Make *.sh under /usr/local/cld/ executable.
-  find /usr/local/cld -name '*.sh' -exec chmod +x {} +
+  find /usr/local/cld -name '*.sh' -type f -execdir chmod +x {} \; -execdir echo "chmod {}" \;
+  
 
 
 # Log
