6bfa30ccc72f124aad6746009ee8161f6749c866
limelime <limelime@users.noreply.github.com>
2018-07-23 04:08:12 +0000

    use absolute wiki directory.

--- cld-manuals-create.sh
+++ cld-manuals-create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 # Description: Generate new version of manuals.
+script_name=$(basename "$0")
 
-WIKI_DIR=./scripts/settings/doc/cust-live-deb.wiki/
+WIKI_DIR=/media/master/github/cust-live-deb.wiki/
 
 # Error handling
-  if [ ! -d "${WIKI_DIR}" ]; then echo "Error: Wiki directory: ${WIKI_DIR}: no such directory. Aborted!"; exit 1; fi;
+  if [ ! -d "${WIKI_DIR}" ]; then echo "Error: ${script_name}: Wiki directory: ${WIKI_DIR}: no such directory. Aborted!"; exit 1; fi;
 
 WIKI_DIR=$(readlink -ev "${WIKI_DIR}")
 
