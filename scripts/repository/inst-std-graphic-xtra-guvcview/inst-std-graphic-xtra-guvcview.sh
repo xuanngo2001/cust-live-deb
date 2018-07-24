1d9b8c125aa44b03ad81826dbe69390b299a0bd6
limelime <limelime@users.noreply.github.com>
2018-07-24 02:44:25 +0000

    Guvcview 2.0.4+debian-1+b1 cashes after 5 mins of recording.

--- scripts/repository/inst-std-graphic-xtra-guvcview/inst-std-graphic-xtra-guvcview.sh
+++ scripts/repository/inst-std-graphic-xtra-guvcview/inst-std-graphic-xtra-guvcview.sh
@@ -20,6 +20,6 @@ sed -i "/Graphics\">/ r jwmrc-menus-graphics-guvcview.xml" "${CLD_JWMRC_DIR}/jwm
 echo "${GV_LOG} * Install guvcview."
 echo "${GV_LOG} * Insert guvcview in Graphics menu."
 
-# REJECTED because it crashes X server after 5 minutes of recording.
+# REJECTED because Guvcview 2.0.4+debian-1+b1 cashes after 5 mins of recording.
 #   Space Used = 6504K
 
