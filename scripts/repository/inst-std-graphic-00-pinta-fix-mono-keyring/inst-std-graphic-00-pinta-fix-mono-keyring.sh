54734480226b4c8fa8fa9e15eb05fc8bf5d374b0
limelime <limelime@users.noreply.github.com>
2018-08-08 22:31:16 +0000

    add local keyring file.

--- scripts/repository/inst-std-graphic-00-pinta-fix-mono-keyring/inst-std-graphic-00-pinta-fix-mono-keyring.sh
+++ scripts/repository/inst-std-graphic-00-pinta-fix-mono-keyring/inst-std-graphic-00-pinta-fix-mono-keyring.sh
@@ -9,12 +9,19 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Add mono-official-stable.list
   GF_ADD_SOURCE_LIST live "deb https://download.mono-project.com/repo/debian stable-jessie main"
   
-# Add keyring.  
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver pool.sks-keyservers.net --recv-keys A6A19B38D3D831EF
-
+# Add mono-project repository key.
+  key_pub_file=xamarin.gpg
+  if [ -f "${key_pub_file}" ]; then
+    cat "${key_pub_file}" | apt-key add -
+    echo "${GV_LOG} * Add mono-project keyring."
+  else
+    echo "${GV_LOG} * ERROR: ${SCRIPT_NAME} failed: ${key_pub_file} doesn't exist!"
+  fi
 
 # Log
   echo "${GV_LOG} * Install mono-keyring."
 
 # Note: http://www.mono-project.com/download/stable/#download-lin-debian
+#       https://github.com/mono/mono/issues/9891
+#         wget https://download.mono-project.com/repo/xamarin.gpg
+#         wget https://packages.microsoft.com/keys/microsoft.asc 
