a70c9635dc2de8c7fe70a434c6327cc46a061aaa
limelime <limelime@users.noreply.github.com>
2018-08-19 18:26:51 +0000

    add doc.

--- scripts/repository/inst-min-con-xtra-iso-iat/inst-min-con-xtra-iso-iat.sh
+++ scripts/repository/inst-min-con-xtra-iso-iat/inst-min-con-xtra-iso-iat.sh
@@ -8,9 +8,9 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install iat
-apt-get -y install iat
+  apt-get -y install iat
 
 
 # Log
-echo "${GV_LOG} * Install iat to convert BIN, MDF, PDI, CDI, NRG, and B5I into ISO-9660."
+  echo "${GV_LOG} * Install iat to convert BIN, MDF, PDI, CDI, NRG, and B5I into ISO-9660."
 
