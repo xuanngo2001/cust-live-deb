74d6dab3c96c299dfa6df731d2e9793f8ec33a11
limelime <limelime@users.noreply.github.com>
2018-08-31 20:10:25 +0000

    add dependent packages.

--- scripts/repository/inst-xtra-games-freeciv-client-gtk/inst-xtra-games-freeciv-client-gtk.sh
+++ scripts/repository/inst-xtra-games-freeciv-client-gtk/inst-xtra-games-freeciv-client-gtk.sh
@@ -6,13 +6,16 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Explicitly download dependent packages
+  apt-get install -d -y libsdl2-mixer-2.0-0 
+
 # Install dependencies.
-apt-get -y install freeciv-server
+  apt-get -y install freeciv-server
 
 # Install freeciv-client-gtk only, other clients are available.
-apt-get -y install freeciv-client-gtk freeciv-data freeciv-sound-standard
+  apt-get -y install freeciv-client-gtk freeciv-data freeciv-sound-standard
 
 # Log
-echo "${GV_LOG} * Install freeciv-server: You need the server to run freeciv."
-echo "${GV_LOG} * Install freeciv-client-gtk."
-echo "${GV_LOG} * How to run freeciv: http://freeciv.wikia.com/wiki/Install#Start_the_game"
+  echo "${GV_LOG} * Install freeciv-server: You need the server to run freeciv."
+  echo "${GV_LOG} * Install freeciv-client-gtk."
+  echo "${GV_LOG} * How to run freeciv: http://freeciv.wikia.com/wiki/Install#Start_the_game"
