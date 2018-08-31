9b80db2305518e236efc304ce05fab4edd23fbd2
limelime <limelime@users.noreply.github.com>
2018-08-31 22:18:41 +0000

    update.

--- scripts/repository/zinst-xtra-wine/inst-xtra-wine.sh
+++ scripts/repository/zinst-xtra-wine/inst-xtra-wine.sh
@@ -7,12 +7,23 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+apt-get -y install wine
+
+# Explicitly download dependent packages
+  apt-get install -d -y libwine:i386
+
+# Install wine32.
+  dpkg --add-architecture i386
+  apt-get update
+  apt-get -y install wine32
+
+
 # Explicitly download dependent packages
 #apt-get install -d -y libwine ocl-icd-libopencl1 wine64
 
 # Install wine so that you can run Windows applications.
 #apt-get install -d -y nvidia-libopencl1 amd-libopencl1
-apt-get -y install wine
+#apt-get -y install wine
 #apt-get -y install mono-vbnc
 #dpkg --add-architecture i386 && apt-get update && apt-get install wine32
 
