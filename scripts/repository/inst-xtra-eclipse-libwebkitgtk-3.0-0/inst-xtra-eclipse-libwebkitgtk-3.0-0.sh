a5e88c93338e4e9083095dd10352c82f411e9f53
limelime <limelime@users.noreply.github.com>
2018-09-02 20:37:08 +0000

    Fix #334.

---
+++ scripts/repository/inst-xtra-eclipse-libwebkitgtk-3.0-0/inst-xtra-eclipse-libwebkitgtk-3.0-0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly download dependent packages
+#   apt-get install -d -y <your-package> 
+
+# Install libwebkitgtk-3.0-0.
+  apt-get -y install libwebkitgtk-3.0-0
+
+
+# Log
+  echo "${GV_LOG} * Install libwebkitgtk-3.0-0."
+
+
