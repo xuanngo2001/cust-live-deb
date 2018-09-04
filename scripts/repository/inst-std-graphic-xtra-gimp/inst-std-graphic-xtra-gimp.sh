c24e51e761eefdb2e181aa44987063fa2e021d54
limelime <limelime@users.noreply.github.com>
2018-09-04 20:21:35 +0000

    add gimp.

---
+++ scripts/repository/inst-std-graphic-xtra-gimp/inst-std-graphic-xtra-gimp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly download dependent packages
+#   apt-get install -d -y <your-package> 
+
+# Install gimp.
+  apt-get -y install gimp
+
+# Insert Gimp menu in Graphics
+  sed -i "/Graphics\">/ r jwmrc-menus-graphics-gimp.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+
+# Log
+  echo "${GV_LOG} * Install gimp."
+
+
