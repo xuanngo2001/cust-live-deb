82e7916d1b98c939463e4e21da5709058a77a521
limelime <limelime@users.noreply.github.com>
2018-09-05 20:21:12 +0000

    indent.

--- scripts/repository/inst-std-00-jwm-menu/inst-std-00-jwm-menu.sh
+++ scripts/repository/inst-std-00-jwm-menu/inst-std-00-jwm-menu.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Add default standard menus in JWM.
-sed -i "/<JWM>/ r jwmrc-menus-standard.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+	sed -i "/<JWM>/ r jwmrc-menus-standard.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Log
-echo "${GV_LOG} * Add default standard menus in JWM."
+	echo "${GV_LOG} * Add default standard menus in JWM."
