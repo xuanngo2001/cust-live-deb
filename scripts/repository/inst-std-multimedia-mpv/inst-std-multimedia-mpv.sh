518ffbde282e390dcaba277d1d6633275d634a86
limelime <limelime@users.noreply.github.com>
2018-09-07 20:17:49 +0000

    refactor to use home/.

--- scripts/repository/inst-std-multimedia-mpv/inst-std-multimedia-mpv.sh
+++ scripts/repository/inst-std-multimedia-mpv/inst-std-multimedia-mpv.sh
@@ -7,42 +7,40 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly download dependent packages
-apt-get install -d -y libavc1394-0
-#apt-get install -d -y libavcodec56 
-#apt-get install -d -y libavdevice56
-#apt-get install -d -y libavfilter5 
-#apt-get install -d -y libavformat56
-apt-get install -d -y libcrystalhd3
-apt-get install -d -y libiec61883-0
-apt-get install -d -y libopencore-amrnb0
-apt-get install -d -y libopencore-amrwb0
-apt-get install -d -y libsmbclient 
-apt-get install -d -y libvo-aacenc0
-apt-get install -d -y libvo-amrwbenc0
-apt-get install -d -y libzvbi0
-
-apt-get install -d -y libuchardet0
+	apt-get install -d -y libavc1394-0
+	#apt-get install -d -y libavcodec56 
+	#apt-get install -d -y libavdevice56
+	#apt-get install -d -y libavfilter5 
+	#apt-get install -d -y libavformat56
+	apt-get install -d -y libcrystalhd3
+	apt-get install -d -y libiec61883-0
+	apt-get install -d -y libopencore-amrnb0
+	apt-get install -d -y libopencore-amrwb0
+	apt-get install -d -y libsmbclient 
+	apt-get install -d -y libvo-aacenc0
+	apt-get install -d -y libvo-amrwbenc0
+	apt-get install -d -y libzvbi0
+	apt-get install -d -y libuchardet0
 
 # Install dependency.
-  apt-get -y install liblept5 # Allow to take screenshot with option: --vo=image
-  
+	apt-get -y install liblept5 # Allow to take screenshot with option: --vo=image
   
 # Install MPV.
-  apt-get -y install mpv
+	apt-get -y install mpv
 
 # Add to CLD/
-  yes | cp -av ./cld  /usr/local
+	yes | cp -av ./cld  /usr/local
 
 # Add useful mpv keybindings settings.
-  MPV_CONFIG_DIR=${HOME}/.config/mpv/
-  mkdir -p ${MPV_CONFIG_DIR} 
-  cat mpv.conf >> ${MPV_CONFIG_DIR}/mpv.conf
-  cat input.conf >> ${MPV_CONFIG_DIR}/input.conf
+#	MPV_CONFIG_DIR=${HOME}/.config/mpv/
+#	mkdir -p ${MPV_CONFIG_DIR} 
+#	cat mpv.conf >> ${MPV_CONFIG_DIR}/mpv.conf
+#	cat input.conf >> ${MPV_CONFIG_DIR}/input.conf
 
 # Log
-  echo "${GV_LOG} * Install Media Player: $(mpv --version | head -n 1 | sed 's/(.*//')."
-  echo "${GV_LOG} * Add multiple aliases for MPV."
-  echo "${GV_LOG} * Add useful mpv keybindings settings."
+	echo "${GV_LOG} * Install Media Player: $(mpv --version | head -n 1 | sed 's/(.*//')."
+	echo "${GV_LOG} * Add multiple aliases for MPV."
+	echo "${GV_LOG} * Add useful mpv keybindings settings."
 
 
 # Dependencies:
