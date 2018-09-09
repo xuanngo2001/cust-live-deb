74dcfa6619444be2f66b40baf4e0b1d7a3d02729
limelime <limelime@users.noreply.github.com>
2018-09-09 17:47:02 +0000

    don't ignore error.

--- cld.sh
+++ cld.sh
@@ -13,33 +13,33 @@ DEB_REPO_URL=$2
                         " e.g. $0 min http://localhost/aptly-repo/master_jessie-main"\
                 )
   if [ "$#" -ne "${ARGS_REQUIRED}" ]; then
-    echo "Error: ${ARGS_REQUIRED} arguments are required. Aborted!"
-    echo "${CMD_EXAMPLES}"
-    exit 1;
+		echo "Error: ${ARGS_REQUIRED} arguments are required. Aborted!"
+		echo "${CMD_EXAMPLES}"
+		exit 1;
   fi
 
 # Update manuals.
-  ./cld-manuals-create.sh
+	./cld-manuals-create.sh
 
 # Update cld-tools scripts on the host. Ensure the latest scripts and versions.
-  ./cld-tools-deploy.sh
+	./cld-tools-deploy.sh
 
 # Build ISO
 	CHROOT_DIR=/media/imdb/chroot/
 	mkdir -p "${CHROOT_DIR}"
 	CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
 
-  ./cld-umount.sh "${CHROOT_DIR}" || true
+	./cld-umount.sh "${CHROOT_DIR}" || true
 	./cld-debootstrap.sh "${CHROOT_DIR}" "${DEB_REPO_URL}"
 	./cld-mount.sh "${CHROOT_DIR}"
 	./cld-chroot.sh "${SYSTEM}" "${CHROOT_DIR}"
-  ./cld-umount.sh "${CHROOT_DIR}" || true
-  ./cld-chroot-log-backup.sh "${CHROOT_DIR}/root/scripts/logs" "${PWD}"
-	./cld-mkiso.sh "${SYSTEM}" "${CHROOT_DIR}" ignore
+	./cld-umount.sh "${CHROOT_DIR}" || true
+	./cld-chroot-log-backup.sh "${CHROOT_DIR}/root/scripts/logs" "${PWD}"
+	./cld-mkiso.sh "${SYSTEM}" "${CHROOT_DIR}"
 
   
 # Logs.
-  LOG_DIR=./logs/
+	LOG_DIR=./logs/
 	./cld-shlog.sh "${LOG_DIR}"
 	./cld-logs-extract-package-sizes.sh "${LOG_DIR}"
-  ./cld-logs-backup.sh "${LOG_DIR}"
+	./cld-logs-backup.sh "${LOG_DIR}"
