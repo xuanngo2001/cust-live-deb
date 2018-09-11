12ecdb33a6fcfd2e8f306862c065522084627735
limelime <limelime@users.noreply.github.com>
2018-09-11 22:10:18 +0000

    reformat indentation.

--- scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
+++ scripts/repository/inst-std-xtra-blender/inst-std-xtra-blender.sh
@@ -8,20 +8,20 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install required packages.
-apt-get -y install libsensors4
-apt-get -y install libgl1-mesa-dri libjemalloc1
+	apt-get -y install libsensors4
+	apt-get -y install libgl1-mesa-dri libjemalloc1
 
 # Install blender.
-apt-get -y install blender
+	apt-get -y install blender
 
 # Insert Blender 3D menu in Graphics
-sed -i "/Graphics\">/ r jwmrc-menus-graphics-blender.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+	sed -i "/Graphics\">/ r jwmrc-menus-graphics-blender.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Log
-echo "${GV_LOG} * Install required packages for blender."
-echo "${GV_LOG} * Install blender3d."
-echo "${GV_LOG} * Insert Blender 3D menu in Graphics."
-echo "${GV_LOG} * http://download.blender.org/release/"
+	echo "${GV_LOG} * Install required packages for blender."
+	echo "${GV_LOG} * Install blender3d."
+	echo "${GV_LOG} * Insert Blender 3D menu in Graphics."
+	echo "${GV_LOG} * http://download.blender.org/release/"
 
 # Note:
 # Worked on Intel graphic: apt-get -y install libgl1-mesa-dri libgl1-mesa-glx
