497a228fb11cff3b94e2cd6e1771a2595d6d5b69
limelime <limelime@users.noreply.github.com>
2018-09-11 22:06:12 +0000

    reformat indentation.

--- scripts/repository/inst-min-win-systray-volumeicon-alsa/inst-min-win-systray-volumeicon-alsa.sh
+++ scripts/repository/inst-min-win-systray-volumeicon-alsa/inst-min-win-systray-volumeicon-alsa.sh
@@ -7,15 +7,13 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-
 # Install volume icon.
-apt-get -y install volumeicon-alsa
-
+	apt-get -y install volumeicon-alsa
 
 # Set volumeicon to run on JWN startup and set left-click to display volume slider
-yes | cp -av ./cld/ /usr/local
+	yes | cp -av ./cld/ /usr/local
 
 # Log
-echo "${GV_LOG} * Install volume controller in system tray."
-echo "${GV_LOG} * Set left-click to display volume slider: lmb_slider=true."
-echo "${GV_LOG} * Set volumeicon to run on JWN startup."
+	echo "${GV_LOG} * Install volume controller in system tray."
+	echo "${GV_LOG} * Set left-click to display volume slider: lmb_slider=true."
+	echo "${GV_LOG} * Set volumeicon to run on JWN startup."
