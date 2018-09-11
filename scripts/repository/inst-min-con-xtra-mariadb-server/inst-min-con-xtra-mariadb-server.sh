4277c873434e3354a008098578ffbe12c460c9c1
limelime <limelime@users.noreply.github.com>
2018-09-11 22:36:20 +0000

    reformat indentation.

--- scripts/repository/inst-min-con-xtra-mariadb-server/inst-min-con-xtra-mariadb-server.sh
+++ scripts/repository/inst-min-con-xtra-mariadb-server/inst-min-con-xtra-mariadb-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 
@@ -8,22 +7,22 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Preseed password
-ROOT_PASSWORD=password
-echo "mariadb-server-10.0 mysql-server/root_password password ${ROOT_PASSWORD}" | debconf-set-selections
-echo "mariadb-server-10.0 mysql-server/root_password_again password ${ROOT_PASSWORD}" | debconf-set-selections
+	ROOT_PASSWORD=password
+	echo "mariadb-server-10.0 mysql-server/root_password password ${ROOT_PASSWORD}" | debconf-set-selections
+	echo "mariadb-server-10.0 mysql-server/root_password_again password ${ROOT_PASSWORD}" | debconf-set-selections
   
 # Install mariadb-server.
-apt-get -y install mariadb-server
+	apt-get -y install mariadb-server
 
 # Shutdown mariadb-server
-mysqladmin -u root -p${ROOT_PASSWORD} shutdown
+	mysqladmin -u root -p${ROOT_PASSWORD} shutdown
 
 # Add to CLD/
- yes | cp -av ./cld  /usr/local
+	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install mariadb-server."
-echo "${GV_LOG} * Set root password to ${ROOT_PASSWORD}."
+	echo "${GV_LOG} * Install mariadb-server."
+	echo "${GV_LOG} * Set root password to ${ROOT_PASSWORD}."
 
 # If /var/run/mysqld/mysqld.sock doesn't exist, then mysqld is not running.
 # mysqladmin -u root -p shutdown
