a6393fb8c06a2a869e998678c931bc6f345fa7f1
limelime <limelime@users.noreply.github.com>
2018-09-11 22:15:42 +0000

    reformat indentation.

--- scripts/repository/inst-std-system-monitor-switcher-lxrandr/inst-std-system-monitor-switcher-lxrandr.sh
+++ scripts/repository/inst-std-system-monitor-switcher-lxrandr/inst-std-system-monitor-switcher-lxrandr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 # Global variables:
 # ${GV_LOG}: Prefix this variable in echo to log echoed string.
 
@@ -9,20 +8,23 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # This is required. Otherwise, when opening lxrandr, it will say "Unable to get monitor information".
 #   Probably, xrandr is needed.
-apt-get -y install x11-xserver-utils
+	apt-get -y install x11-xserver-utils
 
 # Install lxrandr.
-apt-get -y install lxrandr
+	apt-get -y install lxrandr
 
 # Insert lxrandr menu in Administration
-sed -i "/Administration\">/ r jwmrc-menus-adm-lxrandr.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+	sed -i "/Administration\">/ r jwmrc-menus-adm-lxrandr.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Add key binding
-sed -i "/NEW KEYS HERE -->/ r jwmrc-key-lxrandr.xml" "${CLD_JWMRC_DIR}/jwmrc-key.xml"
+	sed -i "/NEW KEYS HERE -->/ r jwmrc-key-lxrandr.xml" "${CLD_JWMRC_DIR}/jwmrc-key.xml"
+
+# Add to CLD/
+	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install Monitor Switcher: lxrandr."
-echo "${GV_LOG} * Add lxrandr key binding."
+	echo "${GV_LOG} * Install Monitor Switcher: lxrandr."
+	echo "${GV_LOG} * Add lxrandr key binding."
 
 
 # Reference
