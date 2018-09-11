e158d891c63a6d9303f8b064c6b0c49d7e7157f6
limelime <limelime@users.noreply.github.com>
2018-09-11 22:09:48 +0000

    reformat indentation.

--- scripts/repository/inst-std-xtra-opw-drush8-phar/inst-std-xtra-opw-drush8-phar.sh
+++ scripts/repository/inst-std-xtra-opw-drush8-phar/inst-std-xtra-opw-drush8-phar.sh
@@ -9,20 +9,19 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Install Drush using phar binary
-DRUSH_PHAR=$(ls -1 drush-*.phar | sort -r | head -n1)
-chmod +x ${DRUSH_PHAR}
-
-LOCAL_BIN=/usr/local/bin
-yes | cp -av "${DRUSH_PHAR}" ${LOCAL_BIN}/drush 
+	drush_phar=$(ls -1 drush-*.phar | sort -r | head -n1)
+	chmod +x ${drush_phar}
+	
+	local_bin_dir=/usr/local/bin
+	yes | cp -av "${drush_phar}" ${local_bin_dir}/drush 
 
 # Add to CLD/
-  yes | cp -av ./cld  /usr/local
+	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install $(drush --version | tr -s ' ') to manage Drupal."
-echo "${GV_LOG} * List of commands at http://drushcommands.com/."
+	echo "${GV_LOG} * Install $(drush --version | tr -s ' ') to manage Drupal."
+	echo "${GV_LOG} * List of commands at http://drushcommands.com/."
 
 # Reference:
 #   -Download at https://github.com/drush-ops/drush/releases
 #   -https://pantheon.io/blog/easily-install-drush-phar
-
