13d92872cdc858d037cc23488d398992487cff7a
limelime <limelime@users.noreply.github.com>
2018-09-13 18:11:34 +0000

    indent.

--- scripts/repository/inst-min-con-xtra-locate/inst-min-con-xtra-locate.sh
+++ scripts/repository/inst-min-con-xtra-locate/inst-min-con-xtra-locate.sh
@@ -8,12 +8,12 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install locate
-apt-get -y install locate
+	apt-get -y install locate
 
-# Add locate alias.
-yes | cp -av ./cld/  /usr/local
+# Add to CLD/
+	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install locate to generate an index of files and directories. Searching for files will be much faster."
-echo "${GV_LOG} * Add locate alias."
-echo "${GV_LOG} * Run updatedb to update the database."
+	echo "${GV_LOG} * Install locate to generate an index of files and directories. Searching for files will be much faster."
+	echo "${GV_LOG} * Add locate alias."
+	echo "${GV_LOG} * Run updatedb to update the database."
