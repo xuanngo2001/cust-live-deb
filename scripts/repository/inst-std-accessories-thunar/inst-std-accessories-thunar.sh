763f343ee363db789069ea9a5c47db0c318689bb
limelime <limelime@users.noreply.github.com>
2018-09-27 00:11:58 +0000

    Ensure icons are shown in Thunar.

--- scripts/repository/inst-std-accessories-thunar/inst-std-accessories-thunar.sh
+++ scripts/repository/inst-std-accessories-thunar/inst-std-accessories-thunar.sh
@@ -7,18 +7,19 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
-apt-get -y install thunar
-apt-get -y install thunar-media-tags-plugin
-apt-get -y install thunar-archive-plugin
-apt-get -y install thunar-volman
+# Install thunar.
+	apt-get -y install thunar
+	apt-get -y install thunar-media-tags-plugin
+	apt-get -y install thunar-archive-plugin
+	apt-get -y install thunar-volman
+	apt-get -y install gnome-icon-theme   # Ensure icons are shown in Thunar.
 
 # Insert Thunar in Accessories menu.
-sed -i "/Accessories\">/ r jwmrc-menus-acc-thunar.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
-
+	sed -i "/Accessories\">/ r jwmrc-menus-acc-thunar.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Log
-echo "${GV_LOG} * Install Thunar file manager."
-echo "${GV_LOG} * Insert Thunar in Accessories menu."
+	echo "${GV_LOG} * Install Thunar file manager."
+	echo "${GV_LOG} * Insert Thunar in Accessories menu."
 
 # Feature: 
 #   -Can't compress by its own. Need other plugins: xarchiver(crap), try file-roller(?).
