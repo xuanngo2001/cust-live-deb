2286b3389f875115606bb2e80ac33e73b56d4774
limelime <limelime@users.noreply.github.com>
2018-09-28 21:01:51 +0000

    indent.

--- scripts/repository/inst-std-graphic-xtra-dia/inst-std-graphic-xtra-dia.sh
+++ scripts/repository/inst-std-graphic-xtra-dia/inst-std-graphic-xtra-dia.sh
@@ -8,13 +8,13 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install dia to draw diagrams.
-apt-get -y install dia dia-shapes
+	apt-get -y install dia dia-shapes
 
 # Insert Dia menu in Graphics
-sed -i "/Graphics\">/ r jwmrc-menus-graphics-dia.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+	sed -i "/Graphics\">/ r jwmrc-menus-graphics-dia.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
 # Log
-echo "${GV_LOG} * Install Dia to draw diagrams."
-echo "${GV_LOG} * Insert Dia menu in Graphics."
-echo "${GV_LOG} * How to add a new shape to Dia: http://dia-installer.de/howto/create_shape/index.html.en"
-echo "${GV_LOG} * Dia Shape Repository: http://dia-installer.de/shapes/index.html.en"
+	echo "${GV_LOG} * Install Dia to draw diagrams."
+	echo "${GV_LOG} * Insert Dia menu in Graphics."
+	echo "${GV_LOG} * How to add a new shape to Dia: http://dia-installer.de/howto/create_shape/index.html.en"
+	echo "${GV_LOG} * Dia Shape Repository: http://dia-installer.de/shapes/index.html.en"
