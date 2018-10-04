1de7d6649eca547904b7bcff97df0122a9ffacd6
limelime <limelime@users.noreply.github.com>
2018-10-04 22:56:58 +0000

    Add JDK.

---
+++ scripts/repository/inst-min-win-xtra-default-jdk/inst-min-win-xtra-default-jdk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly download dependent packages
+#	apt-get install -d -y <your-package> 
+
+# Install default-jdk.
+	apt-get -y install default-jdk
+
+
+# Log
+	echo "${GV_LOG} * Install default-jdk."
+
+# JDK: Space Used = 52,784K
