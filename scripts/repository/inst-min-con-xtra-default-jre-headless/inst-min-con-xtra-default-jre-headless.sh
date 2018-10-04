2be1e2802c7060aee51f742c1be673f77cd75c59
limelime <limelime@users.noreply.github.com>
2018-10-04 22:56:37 +0000

    Add space used.

--- scripts/repository/inst-min-con-xtra-default-jre-headless/inst-min-con-xtra-default-jre-headless.sh
+++ scripts/repository/inst-min-con-xtra-default-jre-headless/inst-min-con-xtra-default-jre-headless.sh
@@ -7,11 +7,13 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install default-jre.
-apt-get -y install default-jre-headless
+	apt-get -y install default-jre-headless
 
 # Get Java version
-JAVA_VER=$(java -version 2>&1 > /dev/null  | head -n1)
+	JAVA_VER=$(java -version 2>&1 > /dev/null  | head -n1)
 
 # Log
-echo "${GV_LOG} * Install ${JAVA_VER}."
-echo "${GV_LOG} * To install JAVA binary: https://wiki.debian.org/JavaPackage."
+	echo "${GV_LOG} * Install ${JAVA_VER}."
+	echo "${GV_LOG} * To install JAVA binary: https://wiki.debian.org/JavaPackage."
+
+# JRE: Space Used = 160728K
