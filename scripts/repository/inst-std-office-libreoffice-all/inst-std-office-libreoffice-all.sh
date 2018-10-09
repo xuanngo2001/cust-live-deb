af9f9e552b7ddc0458d5fab3bc687fd2a253f36c
limelime <limelime@users.noreply.github.com>
2018-10-09 16:53:05 +0000

    Add Size variations.

--- scripts/repository/inst-std-office-libreoffice-all/inst-std-office-libreoffice-all.sh
+++ scripts/repository/inst-std-office-libreoffice-all/inst-std-office-libreoffice-all.sh
@@ -22,3 +22,19 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 	echo "${GV_LOG} * Install Office Suite: Database."
 	echo "${GV_LOG} * Install Office Suite: Equation."
 	echo "${GV_LOG} * https://wiki.documentfoundation.org/ReleasePlan"
+
+# Size variations:
+#inst-std-office-libreoffice-impress.sh;     4692
+#inst-std-office-libreoffice-draw.sh;       17732
+#inst-std-office-libreoffice-writer.sh;     41060
+#inst-std-office-libreoffice-calc.sh;      398468
+#										  461,952 KB
+
+#inst-std-office-libreoffice-math.sh;     1828
+#inst-std-office-libreoffice-impress.sh;  4700
+#inst-std-office-libreoffice-draw.sh;    17732
+#inst-std-office-libreoffice-writer.sh;  41068
+#inst-std-office-libreoffice-calc.sh;    47228
+#inst-std-office-libreoffice-base.sh;   364924
+#									   477,480 KB
+									   
