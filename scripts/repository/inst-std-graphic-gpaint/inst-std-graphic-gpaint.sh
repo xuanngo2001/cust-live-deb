4230a9ba782ed7eb96da57aaa0730983e5451160
limelime <limelime@users.noreply.github.com>
2018-10-10 22:33:17 +0000

    Add gpaint.

---
+++ scripts/repository/inst-std-graphic-gpaint/inst-std-graphic-gpaint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly download dependent packages
+#	apt-get install -d -y <your-package> 
+
+# Install gpaint.
+	apt-get -y install gpaint
+
+
+# Log
+	echo "${GV_LOG} * Install gpaint."
