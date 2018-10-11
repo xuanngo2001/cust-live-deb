8bdbdf90fbd00077999d07e71495cc1a7c9cfaf1
limelime <limelime@users.noreply.github.com>
2018-10-11 18:56:47 +0000

    Update executable and icons paths.

--- scripts/repository/inst-std-office-libreoffice-deb/inst-std-office-libreoffice-deb.sh
+++ scripts/repository/inst-std-office-libreoffice-deb/inst-std-office-libreoffice-deb.sh
@@ -13,10 +13,17 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 	./libreoffice-deb-install.sh
 
 # Insert LibreOffice menu in Office
+	libreoffice_bin_path=$(whereis libreoffice | cut -d' ' -f2)
+	sed -i "s|/usr/bin/libreoffice.* --|${libreoffice_bin_path} --|" jwmrc-menus-office-libreoffice.xml	# Update bin paths.
+
+	libreoffice_bin_filename=$(basename "${libreoffice_bin_path}")
+	sed -i "s|/libreoffice-|/${libreoffice_bin_filename}-|" jwmrc-menus-office-libreoffice.xml			# Update icon paths.
+	
 	sed -i "/Office\">/ r jwmrc-menus-office-libreoffice.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
  
 # Log
-	lo_version=$(libreoffice --version | head -n1 | sed 's/.*Office //')
+	libreoffice_bin_path=$(whereis libreoffice | cut -d' ' -f2)
+	lo_version=$("${libreoffice_bin_path}" --version | head -n1 | sed 's/.*Office //')
 	echo "${GV_LOG} * Install libreoffice-deb: ${lo_version}."
 
 	echo "${GV_LOG} * Install Office Suite: Word processor."
