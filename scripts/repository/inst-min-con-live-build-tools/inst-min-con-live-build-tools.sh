770eb7c306286a6d7afc2e3e31114033843235ac
limelime <limelime@users.noreply.github.com>
2018-10-14 17:07:08 +0000

    tab indent.

--- scripts/repository/inst-min-con-live-build-tools/inst-min-con-live-build-tools.sh
+++ scripts/repository/inst-min-con-live-build-tools/inst-min-con-live-build-tools.sh
@@ -10,23 +10,24 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # To get isohdpfx.bin and isolinux.bin in order to create isohybrid.
 # Note: boot.cat is automatically created
-apt-get -y install isolinux
+	apt-get -y install isolinux
 
 # To create isohybrid.
-apt-get -y install xorriso
+	apt-get -y install xorriso
 
 # To copy directories.
-apt-get -y install rsync
+	apt-get -y install rsync
 
 # To create new squashfs.
-apt-get -y install squashfs-tools
+	apt-get -y install squashfs-tools
 
 # Add to CLD/
 	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install xorriso to create isohybrid."
-echo "${GV_LOG} * Install isolinux to get isohdpfx.bin and isolinux.bin in order to create isohybrid."
-echo "${GV_LOG} * Note: boot.cat is automatically created."
-echo "${GV_LOG} * Install rsync to copy directories."
-echo "${GV_LOG} * Install squashfs-tools to create new squashfs."
+	echo "${GV_LOG} * Install xorriso to create isohybrid."
+	echo "${GV_LOG} * Install isolinux to get isohdpfx.bin and isolinux.bin in order to create isohybrid."
+	echo "${GV_LOG} * Note: boot.cat is automatically created."
+	echo "${GV_LOG} * Install rsync to copy directories."
+	echo "${GV_LOG} * Install squashfs-tools to create new squashfs."
+
