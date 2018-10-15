ddbdef74a7af563be668fc7b5cc3deb46ea6eb63
limelime <limelime@users.noreply.github.com>
2018-10-15 22:00:18 +0000

    TAB indent.

--- scripts/repository/inst-min-win-00-xorg/inst-min-win-00-xorg.sh
+++ scripts/repository/inst-min-win-00-xorg/inst-min-win-00-xorg.sh
@@ -5,17 +5,17 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Install X-Windows=xorg, Window Manager=openbox
-debconf-set-selections -v keyboard.seed
-apt-get -y install xinit xserver-xorg
+	debconf-set-selections -v keyboard.seed
+	apt-get -y install xinit xserver-xorg
 
 # Auto startx
-cat bash_profile-auto-startx >> ${HOME}/.bash_profile
+	cat bash_profile-auto-startx >> ${HOME}/.bash_profile
 
 # Log
-echo "${GV_LOG} * Set keyboard to use US international."
-echo "${GV_LOG} * Install minimum Xorg X server."
-echo "${GV_LOG} * Install xinit so you can run startx."
-echo "${GV_LOG} * Add auto start window manager(i.e startx)."
+	echo "${GV_LOG} * Set keyboard to use US international."
+	echo "${GV_LOG} * Install minimum Xorg X server."
+	echo "${GV_LOG} * Install xinit so you can run startx."
+	echo "${GV_LOG} * Add auto start window manager(i.e startx)."
 
 # Note:
 # "apt-get -y install xorg" uses too much space(~200MB).
