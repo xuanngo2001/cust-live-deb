f8d7fd898df1bd6608152fbc986476c46f73286c
limelime <limelime@users.noreply.github.com>
2018-10-23 17:50:33 +0000

    TAB indent.

--- scripts/repository/inst-xtra-games-openttd/inst-xtra-games-openttd.sh
+++ scripts/repository/inst-xtra-games-openttd/inst-xtra-games-openttd.sh
@@ -10,13 +10,13 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 #   apt-get install -d -y <your-package>  # Explicitly download dependent packages 
 
 # Install openttd.
-apt-get -y install openttd-opengfx openttd-opensfx openttd-openmsx
-apt-get -y install openttd
+	apt-get -y install openttd-opengfx openttd-opensfx openttd-openmsx
+	apt-get -y install openttd
 
 
 # Log
-openttd_version=$(/usr/games/openttd -v | head -n1)
-echo "${GV_LOG} * Install ${openttd_version}."
+	openttd_version=$(/usr/games/openttd -v | head -n1)
+	echo "${GV_LOG} * Install ${openttd_version}."
 
 # dpkg -i $(ls -1 openttd-*linux*-amd64.deb | sort -r | head -n1)
 
