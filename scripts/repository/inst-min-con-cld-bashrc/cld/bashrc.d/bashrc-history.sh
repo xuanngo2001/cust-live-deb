47f420f39d40891e16dbd998b885234a5894bfc4
limelime <limelime@users.noreply.github.com>
2018-11-03 19:37:33 +0000

    move to firstboot.

--- scripts/repository/inst-min-con-cld-bashrc/cld/bashrc.d/bashrc-history.sh
+++
@@ -1,6 +0,0 @@
-
-# Keep history of all your commands executed in ~/.bash_eternal_history
-export HISTTIMEFORMAT="%s "
-PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo $$ $USER \
-               "$(history 1)" >> ~/.bash_eternal_history'
-
