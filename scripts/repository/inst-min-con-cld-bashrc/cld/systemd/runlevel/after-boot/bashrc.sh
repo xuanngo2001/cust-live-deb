1010d0dd46f1b79e81e7eb1b919b625e4bf49265
limelime <limelime@users.noreply.github.com>
2018-11-06 19:23:52 +0000

    Double quotes.

--- scripts/repository/inst-min-con-cld-bashrc/cld/systemd/runlevel/after-boot/bashrc.sh
+++ scripts/repository/inst-min-con-cld-bashrc/cld/systemd/runlevel/after-boot/bashrc.sh
@@ -3,7 +3,7 @@
 if [ -d /usr/local/cld/bashrc.d ]; then
   for i in /usr/local/cld/bashrc.d/*.sh; do
     if [ -r $i ]; then
-      echo ". $i" >> /usr/local/cld/home/.bashrc
+      echo ". \"$i\"" >> /usr/local/cld/home/.bashrc
     fi
   done
   unset i
