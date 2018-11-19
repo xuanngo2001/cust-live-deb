a0e7321290474c3a5ffd148e6786cb6d52bfb722
limelime <limelime@users.noreply.github.com>
2018-11-19 17:33:16 +0000

    List all available options.

--- scripts/repository/inst-min-con-cld-bin/cld/bin/cld-help.sh
+++ scripts/repository/inst-min-con-cld-bin/cld/bin/cld-help.sh
@@ -5,15 +5,23 @@ set -e
 #     If you want different variation, add symbolic link: cd .../cld/doc && ln -s smartphone-help.txt phone-help.txt 
 #   Usage: cld-helph.sh smartphone
 #   Note: from ../inst-min-con-cld-bin/cld/bin
+script_name=$(basename "${0}")
 
-topic=$1-help.txt
-doc_dir=$(readlink -ev /usr/local/cld/doc/)
+# Define variables.
+	topic=$1-help.txt
+	doc_dir=$(readlink -ev /usr/local/cld/doc/)
 
-help_file="${doc_dir}/${topic}"
+	help_file="${doc_dir}/${topic}"
 
-if [ ! -f "${help_file}" ]; then
-  echo "$0: Error: ${help_file} no such file. Aborted!"
-  exit 1;
-fi
+# Error handling.
+	if [ ! -f "${help_file}" ]; then
+		echo "${script_name}: Error: ${help_file} no such file. Aborted!"
+		echo "   e.g. ./${script_name} [option]"
+		echo "   option: " 
+		options_list=$(ls /usr/local/cld/doc/*-help.txt | sed 's|.*/||' | sed 's/-help.txt//' | sort | pr -t -6 -o4)
+		echo "${options_list}"
+		exit 1;
+	fi
 
-cat "${help_file}"
+# Display document content.
+	cat "${help_file}"
