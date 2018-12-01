5fbca883e927e7992459dfdd03a4a8d3c75131f1
limelime <limelime@users.noreply.github.com>
2018-12-01 05:19:10 +0000

    Add doc for wmctrl.

--- scripts/repository/inst-min-win-cld-win-split/inst-min-win-cld-win-split.sh
+++ scripts/repository/inst-min-win-cld-win-split/inst-min-win-cld-win-split.sh
@@ -9,18 +9,20 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Install required applications for cld-win-split.sh
-apt-get -y install wmctrl
-apt-get -y install x11-utils
-
-chmod +x ./xwinsplitter/*.sh
-chmod +x *.sh
-yes | cp -av ./xwinsplitter/*.sh /usr/local/bin/
-yes | cp -av *.sh /usr/local/bin/
+	apt-get -y install wmctrl
+	apt-get -y install x11-utils
+	
+	chmod +x ./xwinsplitter/*.sh
+	chmod +x *.sh
+	yes | cp -av ./xwinsplitter/*.sh /usr/local/bin/
+	yes | cp -av *.sh /usr/local/bin/
 
 # Add key binding
-sed -i "/NEW KEYS HERE -->/ r jwmrc-key-win-split.xml" "${CLD_JWMRC_DIR}/jwmrc-key.xml"
+	sed -i "/NEW KEYS HERE -->/ r jwmrc-key-win-split.xml" "${CLD_JWMRC_DIR}/jwmrc-key.xml"
 
+# Add to CLD/
+	yes | cp -av ./cld  /usr/local
 
 # Log
-echo "${GV_LOG} * Install required applications for cld-win-split.sh: wmctrl and x11-utils for xprop."
-echo "${GV_LOG} * Add key bindings: Ctrl+Alt+Left, Right, Up, Down, 1, 2, 3, 4, 5, 6, 7, 8, 9."
+	echo "${GV_LOG} * Install required applications for cld-win-split.sh: wmctrl and x11-utils for xprop."
+	echo "${GV_LOG} * Add key bindings: Ctrl+Alt+Left, Right, Up, Down, 1, 2, 3, 4, 5, 6, 7, 8, 9."
