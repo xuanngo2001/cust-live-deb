7f6b6db644938da0f96acdd2263b79bc6dddefc5
limelime <limelime@users.noreply.github.com>
2019-04-29 19:06:00 +0000

    Add php-curl.

---
+++ scripts/repository/inst-std-xtra-opw-php-curl/inst-std-xtra-opw-php-curl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install php-curl.
+	apt-get -y install php-curl
+
+
+# Log
+	echo "${GV_LOG} * Install php-curl: Required for DrupalConsole"
