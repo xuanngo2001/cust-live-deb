5b765336a059012fbab79a53cd543140bc8191a0
limelime <limelime@users.noreply.github.com>
2019-04-29 19:04:33 +0000

    Add java.

---
+++ scripts/repository/inst-min-win-xtra-default-jdk/cld/bashrc.d/bashrc-java.sh
@@ -0,0 +1,2 @@
+# From 
+JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
---
+++ scripts/repository/inst-min-win-xtra-default-jdk/java-set-home.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Description: Set JAVA_HOME.
+
+# Get java home directory path.
+    java_dir=$(update-alternatives --list java | sed 's|/jre.*||')
+    java_home_value="JAVA_HOME=\"${java_dir}\""
+
+# Set java home directory path in /etc/environment.
+    #~ java_home_pattern="JAVA_HOME="
+    #~ environment_file=/etc/environment
+    #~ if grep -q "${java_home_pattern}" "${environment_file}"; then
+        #~ sed -i "s|${java_home_patterh}.*|${java_home_value}|" "${environment_file}"
+    #~ else
+        #~ echo "${java_home_value}" >> "${environment_file}"
+    #~ fi
+
+# Echo what is set.
+    echo "${java_home_value}" 
