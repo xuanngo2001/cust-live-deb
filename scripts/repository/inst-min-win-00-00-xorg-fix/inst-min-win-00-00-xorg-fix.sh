423da1fc63e2d4e3924dc9fde120af673e60c5a9
limelime <limelime@users.noreply.github.com>
2019-05-10 18:43:11 +0000

    indent.

--- scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
+++ scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
@@ -4,4 +4,4 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Dependency needed from K4.14 -> K4.16.
-apt-get -y install libglapi-mesa=13.0.6-1+b2
+	apt-get -y install libglapi-mesa=13.0.6-1+b2
