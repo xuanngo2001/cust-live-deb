3f81b890b5199c54e202684ba5faa3a65d50ecd1
limelime <limelime@users.noreply.github.com>
2019-05-12 19:34:18 +0000

    remove fix version number.

--- scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
+++ scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
@@ -5,6 +5,10 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Dependency needed from K4.14 -> K4.16.
 	# Still valid on 2019-05-10
-	apt-get -y install libglapi-mesa=13.0.6-1+b2
+	#apt-get -y install libglapi-mesa=13.0.6-1+b2
+	#apt-get -y install libglapi-mesa libgl1-mesa-glx
 	
+		
+# Explicitly download dependent packages
+	apt-get install -s -y libglapi-mesa libgl1-mesa-glx
 		
