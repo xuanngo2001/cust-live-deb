c9d3ef9958248cab1aa7259652bb2e8b66020813
limelime <limelime@users.noreply.github.com>
2019-05-16 19:50:37 +0000

    update old.

--- scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
+++ scripts/repository/inst-min-win-00-00-xorg-fix/inst-min-win-00-00-xorg-fix.sh
@@ -4,11 +4,4 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Dependency needed from K4.14 -> K4.16.
-	# Still valid on 2019-05-10
-	#apt-get -y install libglapi-mesa=13.0.6-1+b2
-	#apt-get -y install libglapi-mesa libgl1-mesa-glx
-	
-		
-# Explicitly download dependent packages
-	apt-get install -s -y libglapi-mesa libgl1-mesa-glx
-		
+	apt-get -y install libglapi-mesa=13.0.6-1+b2
