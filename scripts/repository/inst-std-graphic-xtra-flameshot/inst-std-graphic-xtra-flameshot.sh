dd9bc1a00ef0755990373a7ad5283c9ce9eef6a2
limelime <limelime@users.noreply.github.com>
2019-08-21 19:33:34 -0400

    Add flameshot.

---
+++ scripts/repository/inst-std-graphic-xtra-flameshot/inst-std-graphic-xtra-flameshot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install flameshot.
+	apt-get -y install flameshot
+
+
+# Log
+	echo "${GV_LOG} * Install flameshot."
