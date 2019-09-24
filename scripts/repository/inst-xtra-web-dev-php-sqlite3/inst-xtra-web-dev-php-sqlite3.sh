bb51b0330bde832ce22bfc330a77ac136ed09847
limelime <limelime@users.noreply.github.com>
2019-09-24 15:32:59 -0400

    add php-sqlite3.

---
+++ scripts/repository/inst-xtra-web-dev-php-sqlite3/inst-xtra-web-dev-php-sqlite3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install php-sqlite3.
+	apt-get -y install php-sqlite3
+
+
+# Log
+	echo "${GV_LOG} * Install php-sqlite3."
