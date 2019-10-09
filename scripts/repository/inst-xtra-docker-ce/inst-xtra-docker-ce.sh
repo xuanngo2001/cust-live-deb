6ea837386b770436ab3b93e6c5f350a399c309ac
limelime <limelime@users.noreply.github.com>
2019-10-09 14:55:35 -0400

    Docker working.

--- scripts/repository/inst-xtra-docker-ce/inst-xtra-docker-ce.sh
+++ scripts/repository/inst-xtra-docker-ce/inst-xtra-docker-ce.sh
@@ -6,6 +6,13 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+# Add deb-multimedia sources list
+  GF_ADD_SOURCE_LIST live "deb https://download.docker.com/linux/debian buster stable"
+  GF_ADD_SOURCE_LIST tmp "deb https://download.docker.com/linux/debian buster stable"
+  
+  
 # Explicitly install dependent packages
 	apt-get -y install apt-transport-https
 	apt-get -y install ca-certificates
@@ -14,8 +21,8 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 	apt-get -y install software-properties-common
 
 # Install docker-ce.
-	#apt-get -y install docker-ce
-
+	apt-get update
+	apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Log
 	echo "${GV_LOG} * Install docker-ce."
