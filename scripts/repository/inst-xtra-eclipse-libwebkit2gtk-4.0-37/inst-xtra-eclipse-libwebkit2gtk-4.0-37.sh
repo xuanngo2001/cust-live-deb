e90cc602bf681d9832dd4fb41b1d5b047099032b
limelime <limelime@users.noreply.github.com>
2019-10-16 11:30:24 -0400

    Add dependencies.

--- scripts/repository/inst-xtra-eclipse-libwebkit2gtk-4.0-37/inst-xtra-eclipse-libwebkit2gtk-4.0-37.sh
+++ scripts/repository/inst-xtra-eclipse-libwebkit2gtk-4.0-37/inst-xtra-eclipse-libwebkit2gtk-4.0-37.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly list dependent packages
-#	apt-get install -s -y <your-package> 
+	apt-get install -s -y xdg-dbus-proxy 
 
 # Install libwebkit2gtk-4.0-37.
 	apt-get -y install libwebkit2gtk-4.0-37
