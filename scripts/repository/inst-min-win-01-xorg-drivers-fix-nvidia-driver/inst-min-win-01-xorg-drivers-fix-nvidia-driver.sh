637040122b2477acc7f3a6f93e258ffb37eabdc3
limelime <limelime@users.noreply.github.com>
2020-02-18 15:59:26 -0500

    add nvidia-driver.

--- scripts/repository/inst-min-win-01-xorg-drivers-fix-nvidia-driver/inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh
+++ scripts/repository/inst-min-win-01-xorg-drivers-fix-nvidia-driver/inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh
@@ -8,6 +8,46 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly list dependent packages
 #	apt-get install -s -y <your-package> 
+apt-get install -s -y nvidia-installer-cleanup
+apt-get install -s -y nvidia-legacy-check
+apt-get install -s -y nvidia-driver-libs
+apt-get install -s -y nvidia-driver-bin
+apt-get install -s -y xserver-xorg-video-nvidia
+apt-get install -s -y nvidia-vdpau-driver
+apt-get install -s -y nvidia-alternative
+apt-get install -s -y nvidia-kernel-dkms
+apt-get install -s -y nvidia-kernel-430.64
+apt-get install -s -y nvidia-support
+apt-get install -s -y glx-alternative-mesa
+apt-get install -s -y glx-alternative-nvidia
+apt-get install -s -y glx-diversions
+apt-get install -s -y libegl-nvidia0
+apt-get install -s -y libgl1-nvidia-glvnd-glx
+
+apt-get install -s -y libglx-nvidia0
+apt-get install -s -y libnvidia-eglcore
+apt-get install -s -y libnvidia-glcore
+apt-get install -s -y libnvidia-ml1
+apt-get install -s -y nvidia-alternative
+apt-get install -s -y nvidia-driver
+
+apt-get install -s -y nvidia-driver-bin
+apt-get install -s -y nvidia-driver-libs
+apt-get install -s -y nvidia-egl-common
+apt-get install -s -y nvidia-egl-icd
+apt-get install -s -y nvidia-installer-cleanup
+
+apt-get install -s -y nvidia-kernel-common
+apt-get install -s -y nvidia-kernel-dkms
+apt-get install -s -y nvidia-kernel-support
+apt-get install -s -y nvidia-legacy-check
+apt-get install -s -y nvidia-modprobe
+
+apt-get install -s -y nvidia-support
+apt-get install -s -y nvidia-vdpau-driver
+apt-get install -s -y update-glx
+apt-get install -s -y xserver-xorg-video-nvidia
+
 
 # Install nvidia-driver.
 	apt-get -y install nvidia-driver
---
+++ scripts/repository/inst-min-win-01-xorg-drivers-fix-nvidia-driver/inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh.log.md
@@ -0,0 +1,18 @@
+* >>>>>>>>> Running inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh ...
+  * After this operation, 228 MB of additional disk space will be used.
+  * Install nvidia-driver to fix freeze of nouveau driver: Nouveau system freeze fifo: SCHED_ERROR 0a [CTXSW_TIMEOUT]
+  * Disk size = 3345612K. Space Used = 273144K. Runtime = 00:00:00:34.
+  * The following NEW packages will be installed:
+  ```bash
+binutils binutils-common binutils-x86-64-linux-gnu cpp cpp-8
+dkms dpkg-dev gcc gcc-8 glx-alternative-mesa
+glx-alternative-nvidia glx-diversions libasan5 libatomic1 libbinutils
+libcc1-0 libdpkg-perl libegl-nvidia0 libgcc-8-dev libgl1-nvidia-glvnd-glx
+libglx-nvidia0 libisl19 libitm1 liblsan0 libmpc3
+libmpx2 libnvidia-eglcore libnvidia-glcore libnvidia-ml1 libquadmath0
+libtsan0 libubsan1 libvdpau1 make nvidia-alternative
+nvidia-driver nvidia-driver-bin nvidia-driver-libs nvidia-egl-common nvidia-egl-icd
+nvidia-installer-cleanup nvidia-kernel-common nvidia-kernel-dkms nvidia-kernel-support nvidia-legacy-check
+nvidia-modprobe nvidia-support nvidia-vdpau-driver patch update-glx
+xserver-xorg-video-nvidia
+  ```
