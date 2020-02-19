e823779f554e9bbf7dc99528d79908c5efa2d237
limelime <limelime@users.noreply.github.com>
2020-02-19 16:58:35 -0500

    remove nvidia.

--- scripts/repository/inst-min-win-01-xorg-drivers-fix-nvidia-driver/inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh
+++ scripts/repository/inst-min-win-01-xorg-drivers-fix-nvidia-driver/inst-min-win-01-xorg-drivers-fix-nvidia-driver.sh
@@ -6,47 +6,50 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Remove nouveau driver first.
+	apt-get -y --purge remove xserver-xorg-video-nouveau
+
 # Explicitly list dependent packages
 #	apt-get install -s -y <your-package> 
-apt-get install -s -y nvidia-installer-cleanup
-apt-get install -s -y nvidia-legacy-check
-apt-get install -s -y nvidia-driver-libs
-apt-get install -s -y nvidia-driver-bin
-apt-get install -s -y xserver-xorg-video-nvidia
-apt-get install -s -y nvidia-vdpau-driver
-apt-get install -s -y nvidia-alternative
-apt-get install -s -y nvidia-kernel-dkms
-apt-get install -s -y nvidia-kernel-430.64
-apt-get install -s -y nvidia-support
-apt-get install -s -y glx-alternative-mesa
-apt-get install -s -y glx-alternative-nvidia
-apt-get install -s -y glx-diversions
-apt-get install -s -y libegl-nvidia0
-apt-get install -s -y libgl1-nvidia-glvnd-glx
-
-apt-get install -s -y libglx-nvidia0
-apt-get install -s -y libnvidia-eglcore
-apt-get install -s -y libnvidia-glcore
-apt-get install -s -y libnvidia-ml1
-apt-get install -s -y nvidia-alternative
-apt-get install -s -y nvidia-driver
-
-apt-get install -s -y nvidia-driver-bin
-apt-get install -s -y nvidia-driver-libs
-apt-get install -s -y nvidia-egl-common
-apt-get install -s -y nvidia-egl-icd
-apt-get install -s -y nvidia-installer-cleanup
-
-apt-get install -s -y nvidia-kernel-common
-apt-get install -s -y nvidia-kernel-dkms
-apt-get install -s -y nvidia-kernel-support
-apt-get install -s -y nvidia-legacy-check
-apt-get install -s -y nvidia-modprobe
-
-apt-get install -s -y nvidia-support
-apt-get install -s -y nvidia-vdpau-driver
-apt-get install -s -y update-glx
-apt-get install -s -y xserver-xorg-video-nvidia
+	apt-get install -s -y nvidia-installer-cleanup
+	apt-get install -s -y nvidia-legacy-check
+	apt-get install -s -y nvidia-driver-libs
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y xserver-xorg-video-nvidia
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-kernel-dkms
+	apt-get install -s -y nvidia-kernel-430.64
+	apt-get install -s -y nvidia-support
+	apt-get install -s -y glx-alternative-mesa
+	apt-get install -s -y glx-alternative-nvidia
+	apt-get install -s -y glx-diversions
+	apt-get install -s -y libegl-nvidia0
+	apt-get install -s -y libgl1-nvidia-glvnd-glx
+	
+	apt-get install -s -y libglx-nvidia0
+	apt-get install -s -y libnvidia-eglcore
+	apt-get install -s -y libnvidia-glcore
+	apt-get install -s -y libnvidia-ml1
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-driver
+	
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y nvidia-driver-libs
+	apt-get install -s -y nvidia-egl-common
+	apt-get install -s -y nvidia-egl-icd
+	apt-get install -s -y nvidia-installer-cleanup
+	
+	apt-get install -s -y nvidia-kernel-common
+	apt-get install -s -y nvidia-kernel-dkms
+	apt-get install -s -y nvidia-kernel-support
+	apt-get install -s -y nvidia-legacy-check
+	apt-get install -s -y nvidia-modprobe
+	
+	apt-get install -s -y nvidia-support
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y update-glx
+	apt-get install -s -y xserver-xorg-video-nvidia
 
 
 # Install nvidia-driver.
