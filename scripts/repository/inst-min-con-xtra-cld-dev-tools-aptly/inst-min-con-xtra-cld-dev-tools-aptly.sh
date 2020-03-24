40bdcb228d1f9d5bb64c2422f3aae4c597c7c686
limelime <limelime@users.noreply.github.com>
2020-03-24 17:31:03 -0400

    B: Add key.

--- scripts/repository/inst-min-con-xtra-cld-dev-tools-aptly/inst-min-con-xtra-cld-dev-tools-aptly.sh
+++ scripts/repository/inst-min-con-xtra-cld-dev-tools-aptly/inst-min-con-xtra-cld-dev-tools-aptly.sh
@@ -6,6 +6,12 @@ set -e
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
+# Add keyring: https://www.aptly.info/download/
+    url_key="https://www.aptly.info/pubkey.txt"
+    deb_repo_key_file="${SCRIPT_NAME}.gpg"
+    wget -q "${url_key}" -O "${deb_repo_key_file}"
+    cat "${deb_repo_key_file}" | apt-key add -
+    
 # Add nightly build repository.
   GF_ADD_SOURCE_LIST live "deb http://repo.aptly.info/ squeeze main"
 
