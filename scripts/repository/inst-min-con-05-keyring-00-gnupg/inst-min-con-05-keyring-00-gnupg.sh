cd51bf2167e08e4019be7a3e650bd599f181fe00
limelime <limelime@users.noreply.github.com>
2020-04-09 21:41:57 -0400

    add new way to manage keys.

---
+++ scripts/repository/inst-min-con-05-keyring-00-gnupg/inst-min-con-05-keyring-00-gnupg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install gnupg.
+	apt-get -y install gnupg wget
+
+# Log
+	echo "${GV_LOG} * Install gnupg to manage and create keys."
