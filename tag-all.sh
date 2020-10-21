954fb3f672020a8ff0fe05f7b3c399bdf9a728c1
Xuan Ngo <xuan.ngo@example.com>
2020-10-21 17:41:23 -0400

    u

--- tag-all.sh
+++ tag-all.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 # Description: Tag good build.
+date_string=$(date +"%Y-%m-%d.%0k.%M.%S")
 
 ## Tag cld
-    ./tag-git.sh cld
+    ./tag-git.sh "${date_string}" cld
 
 ## Tag aptly
     (
         cd ../aptly/;
-        ./master-release-ok.sh > /dev/null
-        ./tag-git.sh aptly
+        ./master-release.sh "tag-${date_string}" > /dev/null
+        ./tag-git.sh "${date_string}" aptly
     )
