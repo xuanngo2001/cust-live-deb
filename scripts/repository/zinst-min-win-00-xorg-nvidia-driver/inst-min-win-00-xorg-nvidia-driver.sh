5766b1ce9e4f47ea07f3151e1e3b4da4fb589636
Xuan Ngo <xuan.ngo@example.com>
2020-11-10 12:04:49 -0500

    u

---
+++ scripts/repository/zinst-min-win-00-xorg-nvidia-driver/inst-min-win-00-xorg-nvidia-driver.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+	apt-get install -s -y nvidia-installer-cleanup
+	apt-get install -s -y nvidia-support
+
+	apt-get install -s -y nvidia-installer-cleanup
+	apt-get install -s -y nvidia-legacy-check
+	apt-get install -s -y nvidia-driver-libs
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y xserver-xorg-video-nvidia
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-kernel-dkms
+	apt-get install -s -y nvidia-support
+	apt-get install -s -y glx-alternative-mesa
+	apt-get install -s -y glx-alternative-nvidia
+	apt-get install -s -y glx-diversions
+	apt-get install -s -y libegl-nvidia0
+	apt-get install -s -y libgl1-nvidia-glvnd-glx
+	
+	apt-get install -s -y libglx-nvidia0
+	apt-get install -s -y libnvidia-eglcore
+	apt-get install -s -y libnvidia-glcore
+	apt-get install -s -y libnvidia-ml1
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-driver
+	
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y nvidia-driver-libs
+	apt-get install -s -y nvidia-egl-common
+	apt-get install -s -y nvidia-egl-icd
+	apt-get install -s -y nvidia-installer-cleanup
+	
+	apt-get install -s -y nvidia-kernel-common
+	apt-get install -s -y nvidia-kernel-dkms
+	apt-get install -s -y nvidia-kernel-support
+	apt-get install -s -y nvidia-legacy-check
+	apt-get install -s -y nvidia-modprobe
+	
+	apt-get install -s -y nvidia-support
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y update-glx
+	apt-get install -s -y xserver-xorg-video-nvidia
+	
+	apt-get install -s -y nvidia-installer-cleanup
+
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y xserver-xorg-video-nvidia
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-kernel-dkms
+	
+	
+	apt-get install -s -y nvidia-legacy-check
+	apt-get install -s -y nvidia-driver-libs
+	
+	apt-get install -s -y nvidia-driver-libs
+	apt-get install -s -y nvidia-driver-bin
+	apt-get install -s -y xserver-xorg-video-nvidia
+	apt-get install -s -y nvidia-vdpau-driver
+	apt-get install -s -y nvidia-alternative
+	apt-get install -s -y nvidia-kernel-dkms
+	apt-get install -s -y nvidia-kernel-450.66
+	apt-get install -s -y nvidia-support
+	
+
+# Install nvidia-driver.
+	apt-get -y install nvidia-driver
+
+
+# Log
+	echo "${GV_LOG} * Install nvidia-driver."
