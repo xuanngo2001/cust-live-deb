09ccb227f3566c82ee3c414fb95bb195c0c6aa01
Xuan Ngo <xuan.ngo@example.com>
2020-11-14 11:12:35 -0500

    u

--- scripts/repository/inst-min-con-cld-bashrc/inst-min-con-cld-bashrc.sh
+++ scripts/repository/inst-min-con-cld-bashrc/inst-min-con-cld-bashrc.sh
@@ -9,9 +9,9 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 
 # Add common bashrc environment.
-yes | cp -av ./cld/ /usr/local/
+	yes | cp -av ./cld/ /usr/local/
 
 
 # Log
-echo "${GV_LOG} * Set common settings for ~/.bashrc."
-# find cld/ -type f -name '*.sh' -execdir chmod +x {} \;
+	echo "${GV_LOG} * Set common settings for ~/.bashrc."
+	# find cld/ -type f -name '*.sh' -execdir chmod +x {} \;
