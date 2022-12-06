2281be05f173cac7487728089e01b94e6e620a83
xuanngo2001 <xuanngo2001@users.noreply.github.com>
2022-12-06 15:28:22 -0500

    include image files from /usr/share/doc/

--- scripts/repository/inst-min-con-cld-tools/cld/bin/cld-list-icons.sh
+++ scripts/repository/inst-min-con-cld-tools/cld/bin/cld-list-icons.sh
@@ -4,8 +4,10 @@ set -e
 #   Test case: files & symbolic links
 #  Note: from ../inst-min-con-cld-tools/cld/bin
 
-find /usr/share/icons/    -name '*.*'
-find /usr/share/pixmaps/  -name '*.*'
+find /usr/share/icons/		-name '*.*'
+find /usr/share/pixmaps/	-name '*.*'
+find /usr/share/doc 	 \( -name "*.svg" -or -name "*.png" -or -name "*.jpg" \)
+
 
 # TODO: Display unique filename only. 
 #   sed 's/^.*\///' | sort | uniq
