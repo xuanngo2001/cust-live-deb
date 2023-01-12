5f1e30a1ec5286478830f4f7f583e0f04f6f74cf
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2023-01-11 21:51:01 -0500

    update docs from firstboot/.../help-doc/update-cld-doc.sh: deleted files.

--- scripts/repository/inst-min-con-cld-bashrc/cld/systemd/runlevel/after-boot/bashrc.sh
+++ scripts/repository/inst-min-con-cld-bashrc/cld/systemd/runlevel/after-boot/bashrc.sh
@@ -1,5 +1,8 @@
-# Source all *.sh in bashrc.d/
+#!/bin/bash
+set -e
+# Description: Source all *.sh in bashrc.d/
 #   Code taken from /etc/profile
+
 if [ -d /usr/local/cld/bashrc.d ]; then
   for i in /usr/local/cld/bashrc.d/*.sh; do
     if [ -r $i ]; then
