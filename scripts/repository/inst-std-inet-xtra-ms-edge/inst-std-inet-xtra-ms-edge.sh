406ab35fd6acfbfe325e669f4313b45014e99d8a
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2023-04-18 19:01:08 -0400

    u

---
+++ scripts/repository/inst-std-inet-xtra-ms-edge/inst-std-inet-xtra-ms-edge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install edge.
+	#apt-get -y install edge
+	
+	dpkg -i microsoft-edge-stable_112.0.1722.48-1_amd64.deb
+
+# Insert Google Chrome in Internet menu.
+	sed -i "/Internet\">/ r jwmrc-menus-inet-ms-edge.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
+
+
+# Log
+	echo "${GV_LOG} * Install MS Edge using *.deb."
