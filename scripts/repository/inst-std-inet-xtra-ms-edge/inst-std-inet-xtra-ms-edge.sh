d799f575f0471ef1a69334aaf475dc77cb49cbc7
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2023-04-18 19:05:14 -0400

    u

--- scripts/repository/inst-std-inet-xtra-ms-edge/inst-std-inet-xtra-ms-edge.sh
+++ scripts/repository/inst-std-inet-xtra-ms-edge/inst-std-inet-xtra-ms-edge.sh
@@ -17,6 +17,10 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 # Insert Google Chrome in Internet menu.
 	sed -i "/Internet\">/ r jwmrc-menus-inet-ms-edge.xml" "${CLD_JWMRC_DIR}/jwmrc-menus.xml"
 
+# Change config to run MS Edge as root.
+	chmod +x  ./run-ms-edge-as-root.sh
+	./run-ms-edge-as-root.sh
 
 # Log
 	echo "${GV_LOG} * Install MS Edge using *.deb."
+	echo "${GV_LOG} * Change config to run MS Edge as root."
