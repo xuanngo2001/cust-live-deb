df3f6e0cde8a79c71c07324e65ec122c103bf891
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2024-10-14 16:42:47 +0000

    uu

---
+++ scripts/repository/inst-min-con-xtra-ansible/inst-min-con-xtra-ansible.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#	apt-get install -s -y <your-package> 
+
+# Install ansible.
+	apt-get -y install ansible
+	apt-get -y install -s python3-cffi-backend
+
+
+# Log
+	echo "${GV_LOG} * Install ansible."
