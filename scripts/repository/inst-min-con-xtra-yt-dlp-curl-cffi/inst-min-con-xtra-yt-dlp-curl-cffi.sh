92c9fbdc987472d179237d79e806da3f1bcf2718
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-03-26 10:52:15 -0400

    99999

--- scripts/repository/inst-min-con-xtra-yt-dlp-curl-cffi/inst-min-con-xtra-yt-dlp-curl-cffi.sh
+++ scripts/repository/inst-min-con-xtra-yt-dlp-curl-cffi/inst-min-con-xtra-yt-dlp-curl-cffi.sh
@@ -8,6 +8,9 @@ echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Install curl-cffi.
     pip install "yt-dlp[default,curl-cffi]" --break-system-packages
+    #pip install curl_cffi --upgrade --break-system-packages
+
+    yt-dlp --list-impersonate-targets
 
 # Log
     echo "${GV_LOG} * Install curl-cffi for yt-dlp to impersonate: yt-dlp --impersonate Chrome-124"
