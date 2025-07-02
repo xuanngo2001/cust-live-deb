1ce3e962b8c997133f2f523826ca773cca187f3a
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-07-02 15:42:15 -0400

    mmm

--- scripts/repository/inst-std-multimedia-mpv/cld/bashrc.d/bashrc-alias-mpv.sh
+++ scripts/repository/inst-std-multimedia-mpv/cld/bashrc.d/bashrc-alias-mpv.sh
@@ -4,3 +4,6 @@ alias m='mpv --volume=20 --autofit=50%'
 alias mu='mpv --volume=20 --autofit=50% --no-audio-display'
 alias mf='mpv --volume=20 --autofit=50% -fs'
 
+alias m1='mpv --volume=20 --autofit=50% --start=00:01:00'
+alias m2='mpv --volume=20 --autofit=50% --start=00:02:00'
+alias m3='mpv --volume=20 --autofit=50% --start=00:03:00'
