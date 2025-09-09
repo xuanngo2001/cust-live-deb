c44167b719cc4899ec78fedb0d61ab3cd84898d4
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-09-09 13:40:01 -0400

    mmm

---
+++ cld-mkiso.0.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -e
+
+# Description: Create an iso-hybrid using chroot/.
+# Requirements:
+
+SYSTEM=$1
+CHROOT_DIR=$2
+iso_output_dir=$3
+IGNORE_ERROR=$4
+
+# Error Handling
+  CMD_EXAMPLES=$(printf "%s\n%s\n%s\n" \
+                        "  e.g. $0 <SYSTEM> <CHROOT_DIR> <iso_output_dir> <IGNORE_ERROR_IS_OPTIONAL>"\
+                        "  e.g. $0 std /path/to/chroot/ /media/iso/"\
+                        "  e.g. $0 min /path/to/chroot/ /media/iso/ ignore"\
+                )
+  if [ -z "${SYSTEM}" ]; then
+    echo "Error: Please provide the system name. Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  
+  if [ ! -d "${CHROOT_DIR}" ]; then
+    echo "Error: Chroot directory: ${CHROOT_DIR}: no such directory. Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
+  
+  if [ ! -d "${iso_output_dir}" ]; then
+    echo "Error: ISO output directory: ${iso_output_dir}: no such directory. Aborted!"
+    echo "${CMD_EXAMPLES}"
+    exit 1;
+  fi
+  iso_output_dir=$(readlink -ev "${iso_output_dir}") 
+  
+
+IGNORE_ERROR=$(echo "${IGNORE_ERROR}" | tr '[:upper:]' '[:lower:]')
+
+# Stop if there is error in main.log
+##################################################################
+MAIN_LOG=./logs/main.log
+if [ "${IGNORE_ERROR}" != "ignore" ];
+then
+	if grep -E '^E:|ERROR:' ${MAIN_LOG} > /dev/null
+	then
+	  echo "Process stopped. There is error in ${MAIN_LOG}."
+	  grep -E '^E:|ERROR:' ${MAIN_LOG} | sed 's/^/  /'
+	  exit 1;
+	fi
+fi
+
+# If system is not empty, do the followings.
+##################################################################
+if [ ! -z "${SYSTEM}" ]; then
+  # Prefix main.size with system name.
+  sed -i "/^${SYSTEM}:/b; s/^/${SYSTEM}: /" ./logs/main.size
+fi
+
+# Copy vmlinuz & initrd in binary/live/.
+##################################################################
+mkdir -p ./binary/live/
+yes | cp ${CHROOT_DIR}/boot/vmlinuz-* ./binary/live/vmlinuz
+yes | cp ${CHROOT_DIR}/boot/initrd.img-* ./binary/live/initrd
+#yes | cp /lib/live/mount/medium/live/initrd ./binary/live/initrd
+# Check if vmlinuz & initrd exist in binary/live/.
+if [ ! -e ./binary/live/initrd ]; then
+  echo "Error: ./binary/live/initrd is missing. ISO will not boot."
+  exit 1;
+fi
+if [ ! -e ./binary/live/vmlinuz ]; then
+  echo "Error: ./binary/live/vmlinuz is missing. ISO will not boot."
+  exit 1;
+fi
+
+# Global variables.
+##################################################################
+YYYY_MM_DD=$(date +"%Y-%m-%d")
+HH_MM_SS=$(date +"%0k.%M.%S")
+DATE_STRING="${YYYY_MM_DD}_${HH_MM_SS}"
+
+
+# Copy main.log & dpkg*.log in ISOHYBRID
+##################################################################
+yes | cp -av ${MAIN_LOG} binary/
+yes | cp -av ./logs/dpkg*.log binary/
+yes | cp -av ./logs/scripts-ls.lst.log binary/
+
+# Backup log files in ./binary/ to ./install-log/
+##################################################################
+KERNEL_VERSION=$(cat binary/dpkg-no-version.log| grep 'linux-image-' | head -n1 | sed 's/linux-image-//')
+VERSION_CODENAME=$(cat /etc/os-release| grep CODENAME| cut -d'=' -f2)
+OUTPUT_NAME="cld_${SYSTEM}_${VERSION_CODENAME}_K${KERNEL_VERSION}_${DATE_STRING}"
+CLD_LOGS_DIR="./install-log/${OUTPUT_NAME}"
+mkdir -p "${CLD_LOGS_DIR}"
+CLD_LOGS_DIR=$(readlink -ev "${CLD_LOGS_DIR}")
+yes | cp -av ./logs "${CLD_LOGS_DIR}"
+
+# Create squashfs.
+##################################################################
+# Delete chroot/root/scripts/.
+rm -rf ${CHROOT_DIR}/root/scripts
+rm -f ./binary/live/filesystem.squashfs
+mkdir -p binary/live/
+mksquashfs "${CHROOT_DIR}" ./binary/live/filesystem.squashfs -comp xz
+# Note: Don't exclude /boot/. initrd.img & vmlinuz are needed for update-initramfs
+
+
+
+# Create ISOHYBRID.
+##################################################################
+# Note: boot.cat is automatically created
+architecture=$(dpkg --print-architecture)
+APP_ID=cld-${architecture}
+iso_filename="${OUTPUT_NAME}.iso"
+iso_filepath="${iso_output_dir}/${iso_filename}"
+rm -f ./binary/cld*.iso; touch ./binary/"${iso_filename}"
+xorriso -as mkisofs -r -J -joliet-long -l \
+				-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16 \
+				-A "${APP_ID}"  \
+				-V "${APP_ID}" \
+				-b isolinux/isolinux.bin \
+				-c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
+				-boot-info-table \
+				-o ${iso_filepath} \
+				./binary
+
+md5sum "${iso_filepath}" > "${iso_filepath}.md5"
+
+# Log directories size.
+##################################################################
+SIZE_LOG=sizes.log
+echo "${iso_filepath}" >> ${SIZE_LOG}
+du -h -c ./binary | sed 's/^/   /' >> ${SIZE_LOG}
+du -h -c -d 1 "${CHROOT_DIR}" | sed 's/^/   /' >> ${SIZE_LOG}
+echo "" >> ${SIZE_LOG}
+
+#TODO:
+# Auto architecture.
+# Add distribution name.
+# Create Hybrid ISO. https://wiki.archlinux.org/index.php/Remastering_the_Install_ISO
