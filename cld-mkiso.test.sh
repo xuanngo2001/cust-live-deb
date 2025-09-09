2e003c22e35e7812762a2fcf19285e1b00d18b74
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-09-09 15:00:47 -0400

    aaa

---
+++ cld-mkiso.test.sh
@@ -0,0 +1,6 @@
+# Build ISO
+	CHROOT_DIR=/media/sql/chroot/
+	CHROOT_DIR=$(readlink -ev "${CHROOT_DIR}")
+	iso_output_dir=$(readlink -ev "/media/sql")
+
+	./cld-mkiso.sh "${SYSTEM}" "${CHROOT_DIR}" "${iso_output_dir}" ignore
