559a35caa9831aafffeda5d37fe510c3d2282605
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-09-13 12:08:55 -0400

    mmm

--- bootloader/debian-live-iso/deb-live.sh
+++ bootloader/debian-live-iso/deb-live.sh
@@ -25,6 +25,7 @@ deb_live_iso="/media/sf_shared/dump/debian-live/debian-live-13.1.0-amd64-standar
 
 # # Use custom grub.cfg
     \cp -v ./config/boot/grub/grub.cfg "$working/boot/grub/"
+    \cp -v ./config/isolinux/live.cfg "$working/isolinux/"
 
 # # Copy live/
 #     \cp -av ../live/ "$working"
