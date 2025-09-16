9076f4451697d2582e0c7268428e644129e0536e
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-09-16 13:25:37 -0400

    aaa

---
+++ scripts/repository/inst-min-con-xtra-vm-hyperv-daemons/_readme.sh
@@ -0,0 +1,29 @@
+# AI
+    - how to setup hyper-v to copy and paste text between windows and linux windows manager
+    - how to setup hyperv-daemons in debian
+
+# Reference:
+    - https://techlabs.blog/categories/debian-linux/install-linux-integration-services-and-hyper-v-daemons-on-debian
+
+# For Debian 10 (Buster) and earlier:
+
+    # cat this text to /etc/initramfs-tools/modules
+    cat << 'EOF' >> /etc/initramfs-tools/modules
+    # Hyper-V modules
+    hv_vmbus
+    hv_storvsc
+    hv_blkvsc
+    hv_netvsc
+    EOF
+
+    # Update initramfs
+        update-initramfs -u
+
+# For Debian 11 (Bullseye) and later:
+    systemctl status hyperv-daemons.hv-*
+    lsmod | grep hv_utils
+
+# Generic 
+    Ensure the hv_sock kernel module is enabled. You can do this by creating a file:
+    sudo sh -c 'echo "hv_sock" > /etc/modules-load.d/hv_sock.conf'
+    Reboot the Linux VM: sudo reboot.
