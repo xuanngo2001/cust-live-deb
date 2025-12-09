a627a230e0d0c6a33f9fa356b924e3d2dbfe74e3
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-12-08 22:24:56 -0500

    mmm

---
+++ scripts/repository/inst-min-con-xtra-yt-dlp-ejs/inst-min-con-xtra-yt-dlp-ejs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Install yt-dlp-ejs.
+    pip install -U yt-dlp-ejs --break-system-packages
+
+# Log
+    echo "${GV_LOG} * Install yt-dlp-ejs."
