a9812e3435b5bb9c547b44cfbbf3ee66ea7fed2a
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-12-08 23:46:17 -0500

    Update repository references from 'bookworm' to 'trixie' in build scripts

--- build-cld.sh
+++ build-cld.sh
@@ -15,7 +15,7 @@ set -e
     T="$(date +%s)"
 
 # Run command.
-    ./cld.sh ${system_type} http://localhost/aptly-repo/master_bookworm-main
+    ./cld.sh ${system_type} http://localhost/aptly-repo/master_trixie-main
 
 # Log total elapsed time if success.
     if [ $? -eq 0 ]; then
