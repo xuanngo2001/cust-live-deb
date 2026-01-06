3e04f1aff3ccd07b0a4ce9450bbac03527aa954e
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2026-01-06 16:31:52 -0500

    aaa

---
+++ scripts/repository/inst-min-win-00-wm-jwm-tmpfix-libatk-bridge2.0-0t64/inst-min-win-00-wm-jwm-tmpfix-libatk-bridge2.0-0t64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+# Global variables:
+# ${GV_LOG}: Prefix this variable in echo to log echoed string.
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
+
+# Explicitly list dependent packages
+#   apt-get install -s -y <your-package> 
+
+# Install libatk-bridge2.0-0t64.
+    apt-get -y install libatk-bridge2.0-0t64=2.58.2-1~bpo13+1
+
+
+# Log
+    echo "${GV_LOG} * Install libatk-bridge2.0-0t64."
+    echo "${GV_LOG} * Trixie: 2026-01-06: Force to install backport version instead of stable version."
