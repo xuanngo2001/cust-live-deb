5c2b96f3de01033a6659cb7a5f92f757fd9bc8f1
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2026-01-12 07:43:22 -0500

    mmm

--- scripts/repository/inst-xtra-zfs-dkms/inst-xtra-zfs-dkms.sh
+++ scripts/repository/inst-xtra-zfs-dkms/inst-xtra-zfs-dkms.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly download dependent packages
-  apt-get install -s -y libelf-dev 
+  apt-get install -s -y libelf-dev pahole libelf1t64 libdw1t64 file
 
 # Install required packages.
 apt-get install -y linux-headers-amd64 lsb-release
