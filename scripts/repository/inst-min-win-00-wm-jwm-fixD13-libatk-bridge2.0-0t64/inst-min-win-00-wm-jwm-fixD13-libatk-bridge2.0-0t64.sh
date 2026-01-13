39a780af00f7f6ad48b77e3b19dedde76b9f4292
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2026-01-13 10:41:23 -0500

    mmm

--- scripts/repository/inst-min-win-00-wm-jwm-fixD13-libatk-bridge2.0-0t64/inst-min-win-00-wm-jwm-fixD13-libatk-bridge2.0-0t64.sh
+++ scripts/repository/inst-min-win-00-wm-jwm-fixD13-libatk-bridge2.0-0t64/inst-min-win-00-wm-jwm-fixD13-libatk-bridge2.0-0t64.sh
@@ -7,10 +7,10 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly list dependent packages
-#   apt-get install -s -y <your-package> 
+    apt-get install -s -y libatk-bridge2.0-0t64 
 
 # Install libatk-bridge2.0-0t64.
-    apt-get -y install libatk-bridge2.0-0t64=2.58.2-1~bpo13+1
+    # apt-get -y install libatk-bridge2.0-0t64=2.58.2-1~bpo13+1
 
 
 # Log
