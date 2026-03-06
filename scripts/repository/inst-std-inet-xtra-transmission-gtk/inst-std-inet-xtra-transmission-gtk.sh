d9616b5661f7384d09b399a769367ac0f1585357
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2026-03-06 13:19:39 -0500

    aaa

--- scripts/repository/inst-std-inet-xtra-transmission-gtk/inst-std-inet-xtra-transmission-gtk.sh
+++ scripts/repository/inst-std-inet-xtra-transmission-gtk/inst-std-inet-xtra-transmission-gtk.sh
@@ -7,8 +7,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly list dependent packages
-    # 2026-03-05: Added for Trixie.
-    apt-get install -s -y libevent-extra-2.1-7t64
+    #apt-get install -s -y libevent-extra-2.1-7t64
 
 # Install transmission-gtk.
     apt-get -y install transmission-gtk
